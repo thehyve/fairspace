c1613fec791cb0212174db41010fd1f3638510b2
Robert Horlings <robert@isdat.nl>
2018-06-13 09:43:20 +0200

    Update readme

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {userLogin, ViewTitle} from 'react-admin';
+import {ViewTitle} from 'react-admin';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
