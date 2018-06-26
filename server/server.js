1f7bce5d3e26dbb6a33145bf79cd53a546d15607
Robert Horlings <robert@isdat.nl>
2018-06-26 14:54:29 +0200

    Add simple express.js server for mock backend calls

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.get('/account/name', (req, res) => res.send({username: 'John Butler'}));
+
+app.listen(port, () => console.log('Example app listening on port ' + port ))
