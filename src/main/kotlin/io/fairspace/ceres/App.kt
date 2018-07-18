ec250376cc30f849911e3a3cfc3afe7e77dd0923
strangepleasures <pavel@thehyve.nl>
2018-07-18 14:25:48 +0200

    Apply command line configuration to injected beans

--- src/main/kotlin/io/fairspace/ceres/App.kt
+++ src/main/kotlin/io/fairspace/ceres/App.kt
@@ -1,24 +1,23 @@
 package io.fairspace.ceres
 
 import com.auth0.jwk.JwkProviderBuilder
-import com.typesafe.config.Config
-import com.typesafe.config.ConfigFactory
 import io.fairspace.ceres.repository.ModelRepository
-import io.ktor.server.engine.commandLineEnvironment
-import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.*
 import io.ktor.server.netty.Netty
 import org.apache.jena.tdb2.TDB2Factory
 import org.koin.dsl.module.applicationContext
 import org.koin.standalone.StandAloneContext.startKoin
 
-private val runtimeContext = applicationContext {
-    bean { ConfigFactory.load()}
-    bean { TDB2Factory.connectDataset(get<Config>().getString("jena.dataset.path")) }
-    bean { ModelRepository(get()) }
-    bean { JwkProviderBuilder(get<Config>().getString("authentication.jwt.issuer")).build() }
-}
 
 fun main(args: Array<String>) {
-    startKoin(listOf(runtimeContext))
-    embeddedServer(Netty, commandLineEnvironment(args)).start(wait = true)
+    val env = commandLineEnvironment(args)
+    val cfg = env.config
+
+    startKoin(listOf(applicationContext {
+        bean { TDB2Factory.connectDataset(cfg.property("jena.dataset.path").getString()) }
+        bean { ModelRepository(get()) }
+        bean { JwkProviderBuilder(cfg.property("authentication.jwt.issuer").getString()).build() }
+    }))
+
+    embeddedServer(Netty, env).start(wait = true)
 }
