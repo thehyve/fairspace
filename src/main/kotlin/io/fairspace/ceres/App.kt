a34683d96c3904f88e4a67d480b4bb77c1ee2133
Robert Horlings <robert@isdat.nl>
2018-07-23 09:44:06 +0200

    Retrieve jwk set on proper url in keycloak

--- src/main/kotlin/io/fairspace/ceres/App.kt
+++ src/main/kotlin/io/fairspace/ceres/App.kt
@@ -2,11 +2,13 @@ package io.fairspace.ceres
 
 import com.auth0.jwk.JwkProviderBuilder
 import io.fairspace.ceres.repository.ModelRepository
-import io.ktor.server.engine.*
+import io.ktor.server.engine.commandLineEnvironment
+import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import org.apache.jena.tdb2.TDB2Factory
 import org.koin.dsl.module.applicationContext
 import org.koin.standalone.StandAloneContext.startKoin
+import java.net.URL
 
 
 fun main(args: Array<String>) {
@@ -15,7 +17,14 @@ fun main(args: Array<String>) {
     startKoin(listOf(applicationContext {
         bean { TDB2Factory.connectDataset(env["jena.dataset.path"]) }
         bean { ModelRepository(get()) }
-        bean { JwkProviderBuilder(env["authentication.jwt.issuer"]).build() }
+        bean {
+            val url = String.format("%s/auth/realms/%s/protocol/openid-connect/certs",
+                    env["authentication.jwt.issuer"],
+                    env["authentication.jwt.realm"]);
+
+            JwkProviderBuilder(URL(url))
+                    .build()
+        }
     }))
 
     embeddedServer(Netty, env).start(wait = true)
