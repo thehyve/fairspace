4f10b1479665a2baaf4c2bafe8146aa613704655
strangepleasures <pavel@thehyve.nl>
2018-07-27 17:58:26 +0200

    Fix after merge

--- src/test/kotlin/io/fairspace/ceres/ModuleTest.kt
+++ src/test/kotlin/io/fairspace/ceres/ModuleTest.kt
@@ -5,7 +5,11 @@ import io.fairspace.ceres.repository.toString
 import io.ktor.config.MapApplicationConfig
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import org.apache.jena.rdf.model.ModelFactory.createDefaultModel
+import org.apache.jena.rdf.model.ResourceFactory.createResource
+import org.apache.jena.rdf.model.ResourceFactory.createStringLiteral
 import org.apache.jena.riot.RDFFormat
+import org.apache.jena.vocabulary.VCARD
 import kotlin.test.*
 
 class ModuleTest : BaseCeresTest() {
@@ -78,41 +82,41 @@ class ModuleTest : BaseCeresTest() {
     fun `Test Patch`() {
         test {
             with(handleRequest(HttpMethod.Post, "/model/test/statements") {
-                addHeader(HttpHeaders.ContentType, RDF_JSON)
-                setBody(model.toString(RDFFormat.RDFJSON))
+                addHeader(HttpHeaders.ContentType, JSONLD)
+                setBody(model.toString(RDFFormat.JSONLD))
             }) {
                 assertEquals(HttpStatusCode.NoContent, response.status())
             }
 
             with(handleRequest(HttpMethod.Get, "/model/test/statements") {
-                addHeader(HttpHeaders.Accept, RDF_JSON)
+                addHeader(HttpHeaders.Accept, JSONLD)
             }) {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(RDF_JSON, response.headers[HttpHeaders.ContentType])
-                val model = RDFFormat.RDFJSON.parse(response.content!!)
-                assertTrue(model.contains(StatementImpl(createResource(personURI), VCARD.FN, createStringLiteral("John Smith"))))
+                assertEquals(JSONLD, response.headers[HttpHeaders.ContentType])
+                val model = RDFFormat.JSONLD.parse(response.content!!)
+                assertTrue(model.contains(createResource(personURI), VCARD.FN, createStringLiteral("John Smith")))
             }
 
-            val delta = ModelFactory.createDefaultModel().apply {
+            val delta = createDefaultModel().apply {
                 createResource(personURI)
                         .addProperty(VCARD.FN, "William Shakespeare")
             }
 
             with(handleRequest(HttpMethod.Patch, "/model/test/statements") {
-                addHeader(HttpHeaders.ContentType, RDF_JSON)
-                setBody(delta.toString(RDFFormat.RDFJSON))
+                addHeader(HttpHeaders.ContentType, JSONLD)
+                setBody(delta.toString(RDFFormat.JSONLD))
             }) {
                 assertEquals(HttpStatusCode.NoContent, response.status())
             }
 
             with(handleRequest(HttpMethod.Get, "/model/test/statements") {
-                addHeader(HttpHeaders.Accept, RDF_JSON)
+                addHeader(HttpHeaders.Accept, JSONLD)
             }) {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(RDF_JSON, response.headers[HttpHeaders.ContentType])
-                val model = RDFFormat.RDFJSON.parse(response.content!!)
-                assertFalse(model.contains(StatementImpl(createResource(personURI), VCARD.FN, createStringLiteral("John Smith"))))
-                assertTrue(model.contains(StatementImpl(createResource(personURI), VCARD.FN, createStringLiteral("William Shakespeare"))))
+                assertEquals(JSONLD, response.headers[HttpHeaders.ContentType])
+                val model = RDFFormat.JSONLD.parse(response.content!!)
+                assertFalse(model.contains(createResource(personURI), VCARD.FN, createStringLiteral("John Smith")))
+                assertTrue(model.contains(createResource(personURI), VCARD.FN, createStringLiteral("William Shakespeare")))
             }
         }
     }
