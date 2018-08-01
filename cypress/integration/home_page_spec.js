196c093f3e9f95f003fd54de1eec58fabe431c27
Jarvanerp <jarno@thehyve.nl>
2018-08-01 13:59:13 +0200

    Add e2e tests for fairspace

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,43 @@
+describe('The Home Page', function() {
+    beforeEach(() => {
+        cy.visit('https://workspace.ci.test.fairdev.app/')
+        cy.url().should('include', '/auth/realms')
+        cy.get('input[name=username]').type('test-workspace-ci')
+        cy.get('input[name=password').type('fairspace123{enter}')
+    })
+
+    it('successfully log in', function() {
+        cy.url().should('include', 'https://workspace.ci.test.fairdev.app/')
+        cy.getCookie('JSESSIONID').should('exist')
+    })
+
+    it('successfully see list of collections', function() {
+        cy.contains("Collections").click()
+        cy.wait(500)
+        cy.get('ul').find('li').should('length.above', 0)
+    })
+
+    it('successfully add collection', function() {
+        cy.contains("Collections").click()
+        cy.wait(500)
+        let number = 0
+        cy.get('ul>li').each(() => {
+            number += 1
+            return number
+        }).then(() =>{
+            cy.get('button').contains("add").click()
+            cy.get('ul>li').should('length.above', number)
+        })
+    })
+
+
+    it('successfully change name of collection', function() {
+        cy.contains("Collections").click()
+        cy.wait(500)
+        cy.get('ul').find('li').eq(0).click()
+        cy.get('h2').contains("Test workspace-ci's collection").click()
+        cy.get('input[name=name]').clear().type('test change name')
+        cy.get('button').contains('Save').click()
+        cy.get('ul>li').should('contain', 'test change name')
+    })
+});
