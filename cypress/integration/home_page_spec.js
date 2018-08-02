b15f4cfbba2647a5ab303f3193991f5c9a3fbcc8
Jarvanerp <jarno@thehyve.nl>
2018-08-02 15:36:09 +0200

    Move user to cypress config

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,43 +1,42 @@
-describe('The Home Page', function() {
+describe('e2e tests for Fairspace', function() {
     beforeEach(() => {
-        cy.visit('https://workspace.ci.test.fairdev.app/')
-        cy.url().should('include', '/auth/realms')
-        cy.get('input[name=username]').type('test-workspace-ci')
-        cy.get('input[name=password').type('fairspace123{enter}')
-    })
+        cy.visit('');
+        cy.url().should('include', '/auth/realms');
+        cy.get('input[name=username]').type(Cypress.config("user_name"));
+        cy.get('input[name=password').type(Cypress.config("password")+'{enter}');
+    });
 
     it('successfully log in', function() {
-        cy.url().should('include', 'https://workspace.ci.test.fairdev.app/')
-        cy.getCookie('JSESSIONID').should('exist')
-    })
+        cy.url().should('include', 'https://workspace.ci.test.fairdev.app/');
+        cy.getCookie('JSESSIONID').should('exist');
+    });
 
     it('successfully see list of collections', function() {
-        cy.contains("Collections").click()
-        cy.get('h3')
-        cy.get('ul').find('li').should('length.above', 0)
-    })
+        cy.contains("Collections").click();
+        cy.get('h3');
+        cy.get('ul').find('li').should('length.above', 0);
+    });
 
     it('successfully add collection', function() {
-        cy.contains("Collections").click()
-        cy.get('h3')
-        let number = 0
+        cy.contains("Collections").click();
+        cy.get('h3');
+        let number = 0;
         cy.get('ul>li').each(() => {
-            number += 1
-            return number
+            number += 1;
+            return number;
         }).then(() =>{
-            cy.get('button').contains("add").click()
-            cy.get('ul>li').should('length.above', number)
+            cy.get('button').contains("add").click();
+            cy.get('ul>li').should('length.above', number);
         })
-    })
-
+    });
 
     it('successfully change name of collection', function() {
-        cy.contains("Collections").click()
-        cy.get('h3')
-        cy.get('ul').find('li').contains("Test workspace-ci's collection").click()
-        cy.get('h2').contains("Test workspace-ci's collection").click()
-        cy.get('input[name=name]').clear().type('test change name')
-        cy.get('button').contains('Save').click()
-        cy.get('ul>li').should('contain', 'test change name')
-    })
+        cy.contains("Collections").click();
+        cy.get('h3');
+        cy.get('ul').find('li').contains("Test workspace-ci's collection").click();
+        cy.get('h2').contains("Test workspace-ci's collection").click();
+        cy.get('input[name=name]').clear().type('test change name');
+        cy.get('button').contains('Save').click();
+        cy.get('ul>li').should('contain', 'test change name');
+    });
 });
