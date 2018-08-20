156bf49005f51bfe839cf0199cb505ff827b8280
Jarvanerp <jarno@thehyve.nl>
2018-08-20 15:34:21 +0200

    Refactor for improved readability

--- src/components/metadata/MetadataViewer.js
+++ src/components/metadata/MetadataViewer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { JsonToTable } from "react-json-to-table";
+import * as jsonld from 'jsonld/dist/jsonld';
+
 
 /**
  * This compp
@@ -9,67 +10,136 @@ class MetadataViewer extends React.Component {
     constructor(props) {
         super(props);
         this.props = props;
-        this.idsMap = {};
         this.contentMap = {};
-        this.metadata = props.metadata;
-        this.vocab =  props.vocab;
+        // this.vocab = null;
+        this.metadata = this.testData;
+        this.state = {
+            metadata: null,
+            vocab: null,
+            contentMap: null
+        };
     }
 
-    addtoIdsMap(key, metadata) {
-        if (Object.keys(this.idsMap).includes(key)){
-            let itemList = [];
-            if (typeof  this.idsMap[key] === "object") {
-                itemList = itemList.concat(this.idsMap[key]);
+    testData = {
+        "@id": "http://fairspace.com/iri/collections/1",
+        "@type": "http://fairspace.io/ontology#Collection",
+        "description": "My first collection",
+        "name": "Collection 5",
+        "@context": {
+            "name": {
+                "@id": "http://fairspace.io/ontology#name"
+            },
+            "description": {
+                "@id": "http://fairspace.io/ontology#description"
+            }
+        }
+    };
+
+    vocab = {
+        "@context": {
+            "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+            "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            "dc": "http://purl.org/dc/elements/1.1/",
+            "schema": "http://schema.org/",
+            "fairspace": "http://fairspace.io/ontology#"
+        },
+        "@graph": [
+            {
+                "@id": "fairspace:name",
+                "@type": "rdf:Property",
+                "rdfs:label": "Name"
+            },
+            {
+                "@id": "fairspace:description",
+                "@type": "rdf:Property",
+                "rdfs:label": "Description"
+            },
+            {
+                "@id": "fairspace:Collection",
+                "@type": "rdf:Class",
+                "rdfs:label": "Collection"
+            }
+        ]
+    };
+
+    updateMetadataState = (value) => {
+        this.setState({metadata: value});
+    };
+
+    updateVocabState = (value) => {
+        this.setState({vocab: value});
+    };
+
+    createVocabMapping = () => {
+        const labelsById = {};
+        this.state.vocab.forEach(propertyDefinition => {
+            let id = propertyDefinition["@id"];
+            let label = propertyDefinition['http://www.w3.org/2000/01/rdf-schema#label'][0];
+            labelsById[id] = label["@value"];
+        });
+        return labelsById;
+    };
+
+    addToContentMap = (label, value) => {
+        console.log(this.state.metadata);
+        console.log(this.state.vocab);
+        let itemList;
+        if (label in this.contentMap) {
+            if (typeof this.contentMap[label] === "object") {
+                itemList = this.contentMap[label];
             }
             else {
-                itemList.push(this.idsMap[key]);
+                itemList = [this.contentMap[label]];
             }
-            itemList.push(metadata[key]);
-            this.idsMap[key] = itemList;
+            itemList.push(value);
+            this.contentMap[label] = itemList;
         }
         else {
-            this.idsMap[key] = metadata[key];
+            this.contentMap[label] = value;
         }
-        delete metadata[key];
-        return metadata;
-    }
+        // if (itemList.length === 0 ) {
+        //     this.setState({})
+        // }
+        // else {
+        //     this.setState({})
+        // }
+    };
 
-    getIds(metadata) {
-        for (let key in metadata) {
-            if (key.includes(":")) {
-                metadata = this.addtoIdsMap(key, metadata);
+    createMetadataWithLabels = (labelsById) => {
+        Object.keys(this.state.metadata).forEach((key) => {
+            if (key in labelsById) {
+                this.state.metadata[key].forEach((valObj) => {
+                    this.addToContentMap(labelsById[key], valObj["@value"]);
+                });
             }
-            else if (typeof metadata[key] === 'object' || typeof key === 'number') {
-                this.getIds(metadata[key]);
-            }
-        }
-    }
+        });
 
-    getLabels() {
-        for (let label in this.idsMap) {
-            let found = false;
-            for (const labelObject of this.vocab["@graph"]) {
-                if (labelObject["@id"] === label) {
-                    this.contentMap[labelObject["rdfs:label"]] = this.idsMap[label];
-                    found = true;
-                }
-            }
-            // TODO what if label is not found?
-            if (!found) {
-                this.contentMap[label] = this.idsMap[label];
-            }
-        }
-    }
+    };
 
     componentWillMount() {
-        this.getIds(this.metadata);
-        this.getLabels();
+        jsonld.expand(this.vocab)
+            .then(this.updateVocabState);
+
+        jsonld.expand(this.metadata)
+            .then(metadataItems => this.updateMetadataState(metadataItems[0]))
+            .then(this.createVocabMapping)
+            .then(this.createMetadataWithLabels);
+    }
+
+    renderContentMap() {
+        console.log(this.contentMap);
+        return Object.keys(this.contentMap).map((contentLabel) =>
+            <li><b>{contentLabel}</b>: {this.contentMap[contentLabel]}</li>
+        );
     }
 
     render() {
         return (
             <div>
-                <JsonToTable json={this.contentMap} />
+                <ul>
+                    {this.renderContentMap()}
+                </ul>
+
             </div>
         )
 
