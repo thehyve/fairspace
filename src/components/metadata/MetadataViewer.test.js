9ed0f8f2ed91dc6237d10ea583ad8f8f8e9dd818
Jarvanerp <jarno@thehyve.nl>
2018-08-21 14:46:02 +0200

    Add tests for MetadataViewer component

--- src/components/metadata/MetadataViewer.test.js
+++ src/components/metadata/MetadataViewer.test.js
@@ -3,33 +3,64 @@ import React from 'react';
 import {mount} from "enzyme";
 
 function flushPromises() {
-    return new Promise((resolve, reject) => resolve());
+    return new Promise(resolve => setImmediate(resolve));
 }
 
-it('combines vocabulary and metadata', () => {
+it('Combines vocabulary and metadata', () => {
     const wrapper = mount(<MetadataViewer vocabulary={vocabulary} metadata={metadata1} />);
-
     return flushPromises().then(() => {
+        wrapper.update();
+    }).then(() => {
         const result = wrapper.find("li");
-        console.log(wrapper);
-        console.log(result);
-        expect(result.length).toEqual(2);
+        expect(result.length).toEqual(4);
+        expect(wrapper.text()).toEqual("Description:My first collectionName:Collection 5");
     });
 });
 
-// it('adds non fairspace identifiers, no label', () => {
-//     const check = new MetadataViewer({"vocabulary": vocabulary, "metadata": metadata2});
-//     check.getIds(check.metadata);
-//     check.getLabels();
-//     expect(check.contentMap).toEqual({"Collection": "this url", "Description": "What", "dats:name": "John's quotes"});
-// });
-//
-// it('adds multiple nested', () => {
-//     const check = new MetadataViewer({"vocabulary": vocabulary, "metadata": metadata3});
-//     check.getIds(check.metadata);
-//     check.getLabels();
-//     expect(check.contentMap).toEqual({"Collection": "this url", "Description": "What", "Name": "test123456789"});
-// });
+it('Does not show metadata with missing label', () => {
+    const wrapper = mount(<MetadataViewer vocabulary={vocabulary} metadata={metadata2} />);
+    return flushPromises().then(() => {
+        wrapper.update();
+    }).then(() => {
+        const result = wrapper.find("li");
+        expect(result.length).toEqual(4);
+        expect(wrapper.text()).toEqual("Description:My first collectionName:Collection 5");
+    });
+});
+
+it('No vocabulary provided', () => {
+    const wrapper = mount(<MetadataViewer vocabulary={empty_vocabulary} metadata={metadata1} />);
+    return flushPromises().then(() => {
+        wrapper.update();
+    }).then(() => {
+        const result = wrapper.find("li");
+        expect(result.length).toEqual(0);
+        expect(wrapper.text()).toEqual("");
+    });
+});
+
+it('No metadata provided', () => {
+    const wrapper = mount(<MetadataViewer vocabulary={vocabulary} metadata={empty_metadata} />);
+    return flushPromises().then(() => {
+        wrapper.update();
+    }).then(() => {
+        const result = wrapper.find("li");
+        expect(result.length).toEqual(0);
+        expect(wrapper.text()).toEqual("");
+    });
+});
+
+it('Does not show extra label', () => {
+    const wrapper = mount(<MetadataViewer vocabulary={vocabulary2} metadata={metadata2} />);
+    return flushPromises().then(() => {
+        wrapper.update();
+    }).then(() => {
+        const result = wrapper.find("li");
+        expect(result.length).toEqual(4);
+        expect(wrapper.text()).toEqual("Description:My first collectionName:Collection 5");
+    });
+});
+
 
 const metadata1 = {
     "@id": "http://fairspace.com/iri/collections/1",
@@ -47,15 +78,19 @@ const metadata1 = {
 };
 
 const metadata2 = {
-    "dats:name" : "John's quotes",
-    "fairspace:description" : "What",
-    "fairspace:Collection" : "this url",
-};
-
-const metadata3 = {
-    "test1" : {"test2" : [{"fairspace:name" : "test123456789"}]},
-    "fairspace:description" : "What",
-    "fairspace:Collection" : "this url",
+    "@id": "http://fairspace.com/iri/collections/1",
+    "@type": "http://fairspace.io/ontology#Collection",
+    "description": "My first collection",
+    "name": "Collection 5",
+    "what": "test",
+    "@context": {
+        "name": {
+            "@id": "http://fairspace.io/ontology#name"
+        },
+        "description": {
+            "@id": "http://fairspace.io/ontology#description"
+        }
+    }
 };
 
 const vocabulary = {
@@ -84,3 +119,38 @@ const vocabulary = {
         }
     ]
 };
+
+const vocabulary2 = {
+    "@context": {
+        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+        "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+        "dc": "http://purl.org/dc/elements/1.1/",
+        "schema": "http://schema.org/",
+        "fairspace": "http://fairspace.io/ontology#"
+    },
+    "@graph": [
+        {
+            "@id": "fairspace:name",
+            "@type": "rdf:Property",
+            "rdfs:label": "Name"
+        },
+        {
+            "@id": "fairspace:description",
+            "@type": "rdf:Property",
+            "rdfs:label": "Description"
+        },
+        {
+            "@id": "fairspace:Collection",
+            "@type": "rdf:Class",
+            "rdfs:label": "Collection"
+        },
+        {
+            "@id": "fairspace:Patient",
+            "@type": "rdf:Property",
+            "rdfs:label": "Patient"
+        }
+    ]
+};
+
+const empty_vocabulary = {};
+const empty_metadata = {};
