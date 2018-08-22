232d34c73649b2b6eaa608460e0a8eb3c1e978ce
strangepleasures <pavel@thehyve.nl>
2018-08-22 16:24:17 +0200

    Add /api/ to the root path

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ const server = new webdav.WebDAVServer({
 
 app.get('/', (req, res) => res.send('Hi, I\'m Titan!').end());
 
-app.use(webdav.extensions.express('/storage/webdav/', server));
+app.use(webdav.extensions.express('/api/storage/webdav/', server));
 
 module.exports = app;
