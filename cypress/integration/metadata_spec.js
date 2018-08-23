868fcba7472d67cdc6fe0aadcfc1b5ba2fec3b01
Jarvanerp <jarno@thehyve.nl>
2018-08-23 14:30:31 +0200

    Traverse linked metadata

---
+++ cypress/integration/metadata_spec.js
@@ -0,0 +1,22 @@
+describe('e2e tests checking metadata for Fairspace', function () {
+    beforeEach(() => {
+        cy.visit('');
+        cy.url().should('include', '/auth/realms');
+        cy.get('input[name=username]').type(Cypress.config("user_name"));
+        cy.get('input[name=password').type(Cypress.config("password") + '{enter}');
+    });
+
+    it('successfully see metadata about patient that is linked to a collection', function () {
+        cy.contains("Collections").click();
+        cy.request("/metadata/collections", "GET").as("getCollections")
+            .then(() => {
+                cy.get('button').contains("add").click();
+                cy.wait(500);
+                cy.get('tr').find('th').contains("GSE8581").click();
+                cy.wait(500);
+                cy.get('a').contains("https://workspace.ci.test.fairdev.app/iri/patients/GSM210004").click();
+                cy.get("ul>li").contains("Type: patients");
+            });
+    });
+
+});
