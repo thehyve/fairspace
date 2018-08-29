3d46657b1a17e5d849d6699433f43b126806fb64
Robert Horlings <robert@isdat.nl>
2018-08-29 14:19:44 +0200

    Add route instead of path to span name.
    This way, the number of different span names will be limited
    See https://github.com/openzipkin/brave/blob/master/instrumentation/http/README.md#http-route-cardinality
    for an explanation about why that is useful.

--- app/src/main/java/nl/fairspace/pluto/app/config/TracingConfiguration.java
+++ app/src/main/java/nl/fairspace/pluto/app/config/TracingConfiguration.java
@@ -1,43 +1,10 @@
 package nl.fairspace.pluto.app.config;
 
-import nl.fairspace.pluto.app.config.ApiTokenAccessFilter;
-import nl.fairspace.pluto.app.config.OAuth2AuthenticationWithLoginUrlEntryPoint;
-import nl.fairspace.pluto.app.config.dto.AppConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
+import com.netflix.zuul.context.RequestContext;
 import org.springframework.cloud.sleuth.SpanAdjuster;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.AccessTokenProviderChain;
-import org.springframework.security.oauth2.client.token.AccessTokenRequest;
-import org.springframework.security.oauth2.client.token.RequestEnhancer;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
-import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.accept.ContentNegotiationStrategy;
-import org.springframework.web.accept.HeaderContentNegotiationStrategy;
-import zipkin2.Span;
 
-import java.net.URLEncoder;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Map;
 
 @Configuration
@@ -48,13 +15,18 @@ public class TracingConfiguration {
 
     @Bean
     SpanAdjuster spanAdjuster() {
+        // The zuul filter does not add the route to the span name
+        // where it would be useful
         return span -> {
             Map<String, String> tags = span.tags();
+            RequestContext currentContext = RequestContext.getCurrentContext();
 
-            if(tags.containsKey(HTTP_METHOD_TAG) &&
-                    tags.containsKey(HTTP_PATH_TAG) &&
+            if(currentContext != null &&
+                    currentContext.getBoolean("zuulEngineRan", false) &&
+                    tags.containsKey(HTTP_METHOD_TAG) &&
                     span.name().equalsIgnoreCase(tags.get(HTTP_METHOD_TAG))) {
-                return span.toBuilder().name(tags.get(HTTP_METHOD_TAG) + " " + tags.get(HTTP_PATH_TAG)).build();
+
+                return span.toBuilder().name(String.format("%s %s", tags.get(HTTP_METHOD_TAG), currentContext.get("proxy"))).build();
             } else {
                 return span;
             }
