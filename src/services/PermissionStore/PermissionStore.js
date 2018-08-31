e5db42f4d5368f7b32dfdf3de8da66125ac68e62
strangepleasures <pavel@thehyve.nl>
2018-08-31 12:13:57 +0200

    Refactoring of PermissionStore

--- src/services/PermissionStore/PermissionStore.js
+++ src/services/PermissionStore/PermissionStore.js
@@ -1,18 +1,38 @@
 import Config from '../../components/generic/Config/Config';
 import {failOnHttpError} from "../../utils/httputils";
+import {format} from 'util';
 
 class PermissionStore {
-    static getHeaders = new Headers({'Accept': 'application/json'});
+    static getConfig = {
+        method: 'GET',
+        headers: new Headers({'Accept': 'application/json'}),
+        credentials: 'same-origin'
+    };
 
-    getCollectionPermissions(collectionId, user) {
-        return fetch(Config.get().urls.collections + '/' + collectionId + '/permissions' + (user ? '?user=' + user : ''),
-            {
-                method: 'GET',
-                headers: PermissionStore.getHeaders,
-                credentials: 'same-origin'
-            })
-            .then(failOnHttpError('Error while loading collection permissions'))
+    /**
+     * Retrieves a list of permissions for a specific collection.
+     * @param collectionId The id of the collection.
+     * @returns A Promise returning an array of user permissions, not including users with None permissions.
+     */
+    getCollectionPermissions(collectionId) {
+        let url = format(Config.get().urls.collectionPermissions, collectionId);
+        return fetch(url, PermissionStore.getConfig)
+            .then(failOnHttpError('Error while loading permissions'))
             .then(response => response.json())
+    }
+
+    /**
+     * Retrieves an access permission a user has to a specific collection.
+     * @param collectionId The id of the collection.
+     * @param user The user.
+     * @returns {Promise<'Manage' | 'Write' | 'Read' | 'None'>}
+     */
+    getPermission(collectionId, user) {
+        let url = format(Config.get().urls.collectionPermissions, collectionId) + '?user=' + user;
+        return fetch(url, PermissionStore.getConfig)
+            .then(failOnHttpError('Error while loading permissions'))
+            .then(response => response.json())
+            .then(json => json.permission)
     }
 }
 
