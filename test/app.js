b9ef24acfbcc306fa5fa919f1e92f1c85ba68c2d
Pavel Mikhailovskii <pavel@thehyve.nl>
2018-09-04 21:22:44 +0200

    Fix tests

--- test/app.js
+++ test/app.js
@@ -69,28 +69,29 @@ describe('Webdav', () => {
         server
             .mkcol('/api/storage/webdav/tmp')
             .set('Authorization', 'Bearer ' + token)
+            .expect(201)
     );
 
-    it('can move directories properly within the /api/storage/webdav/ root', () => {
+    it('can move directories properly within the /api/storage/webdav/ root', () =>
         server
             .move('/api/storage/webdav/tmp')
             .set('Authorization', 'Bearer ' + token)
             .set('Destination', '/api/storage/webdav/tmp2')
             .expect(201)
+            .then(() => server
+                .move('/api/storage/webdav/tmp2')
+                .set('Authorization', 'Bearer ' + token)
+                .set('Destination', '/api/storage/webdav/tmp')
+                .expect(201))
 
         // Move back again to allow cleanup
-        server
-            .move('/api/storage/webdav/tmp2')
-            .set('Authorization', 'Bearer ' + token)
-            .set('Destination', '/api/storage/webdav/tmp')
-            .expect(201)
-
-    });
+    );
 
     afterEach(() =>
         server
             .delete('/api/storage/webdav/tmp')
             .set('Authorization', 'Bearer ' + token)
+            .expect(200)
     );
 
 });
