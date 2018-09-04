f3cd0f4b7d2aaabbadbde2a09751257f12a50562
Robert Horlings <robert@isdat.nl>
2018-09-04 15:57:47 +0200

    Add keys for list of components. See https://reactjs.org/docs/lists-and-keys.html#keys

--- src/components/metadata/MetadataUtils.js
+++ src/components/metadata/MetadataUtils.js
@@ -7,25 +7,44 @@ import {compareBy, comparing} from "../../utils/comparators";
  * combines them to make a list of objects with the label and value as keys.
  * @param vocabulary json-ld format where labels are specified.
  * @param metadata json-ld format with actual metadata.
- * @returns {*}
+ * @returns A promise resolving in an array with metadata. Each element will look like this:
+ * {
+ *      key: "fairspace:description",
+ *      label: "Description",
+ *      values: [
+ *          { "@id": "http://fairspace.com/collections/1", "rdfs:label": "My collection" },
+ *          { "@value": "Literal value"}
+ *      ]
+ *  }
  */
 function combine(vocabulary, metadata) {
     return Promise.all([extractLabelsByIdMap(vocabulary), jsonld.expand(metadata)])
         .then(([labelsById, expandedMetadata]) => {
             const root = expandedMetadata[0];
-            const result = {};
+            const result = [];
+
             for (const key in root) {
                 const label = labelsById[key];
                 if (label) {
-                    result[label] = root[key].sort(comparing(compareBy('@id'), compareBy('@value')));
-                    // @type is not a label. Therefore, this needs to be a separate check. Where the label needs  to be retrieved.
+                    result.push({
+                        key: key,
+                        label: label,
+                        values: root[key].sort(comparing(compareBy('@id'), compareBy('@value')))
+                    });
                 } else if (key === "@type") {
-                    result["Type"] = root[key]
-                        .map(type => ({"@id": type, "rdfs:label": labelsById[type]}))
-                        .sort(comparing(compareBy('rdfs:label'), compareBy('@id')));
+                    // @type is not a label. Therefore, this needs to be a separate check.
+                    // Where the label needs  to be retrieved.
+                    result.push({
+                        key: key,
+                        label: "Type",
+                        values: root[key]
+                            .map(type => ({"@id": type, "rdfs:label": labelsById[type]}))
+                            .sort(comparing(compareBy('rdfs:label'), compareBy('@id')))
+                    });
                 }
             }
-            return Object.keys(result).sort().map(label => ({label: label, values: result[label]}));
+
+            return result.sort(comparing(compareBy('label')));
         })
         .catch(err => console.error('Error combining metadata and vocabulary', err));
 }
