ddf02dcdf954d2e6916c7f5f3851cb59f4b54176
strangepleasures <pavel@thehyve.nl>
2018-09-12 12:52:36 +0200

    Try eager fetch

--- src/main/java/io/fairspace/neptune/model/Permission.java
+++ src/main/java/io/fairspace/neptune/model/Permission.java
@@ -30,7 +30,7 @@ public class Permission {
 
     @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
     @JsonIdentityReference(alwaysAsId = true)
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     Collection collection;
 
     @NotNull
