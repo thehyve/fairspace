aca7c8ee68b304b4e1f204ec51821b32534cb41c
Riza Nugraha <rnugraha@gmail.com>
2018-09-14 10:50:58 +0200

    Add ui components to remove, change and add access rights

--- src/components/permissions/Permissions.js
+++ src/components/permissions/Permissions.js
@@ -1,68 +1,129 @@
 import React from 'react';
-import Loader from '../generic/Loader/Loader';
-import permissionStore from '../../services/PermissionAPI/PermissionAPI'
-import List from "@material-ui/core/List/List";
-import ListItem from "@material-ui/core/ListItem/ListItem";
+import permissionStore from '../../services/PermissionStore/PermissionStore'
+import IconButton from '@material-ui/core/IconButton';
 import {compareBy, comparing} from "../../utils/comparators";
 import Typography from "@material-ui/core/Typography";
-import Icon from "@material-ui/core/Icon";
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import ShareWithDialog from './ShareWithDialog';
 
-class Permissions extends React.Component{
+class Permissions extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            collectionId: props.collectionId,
-            showEditButton: false
+            permissions: [],
+            showPermissionDialog: false
         };
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-
-        if (!this.props.collectionId) {
-            this.props.collectionId = false;
-        }
-
-        if (this.props.collectionId !== prevProps.collectionId) {
-            this.setState({
-                collectionId: this.props.collectionId
-            });
+    componentDidMount() {
+        if (this.props.collectionId) {
+            this.loadPermissions();
         }
     }
 
-    toggleEditButton = () => {
-        return this.setState({showEditButton: !this.state.showEditButton})
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.collectionId !== prevProps.collectionId) {
+            this.loadPermissions();
+        }
+    }
+
+    resetPermissions = () => {
+        this.setState({permissions: []});
     };
 
     loadPermissions = () => {
-        return permissionStore.getCollectionPermissions(this.state.collectionId)
+        permissionStore
+            .getCollectionPermissions(this.props.collectionId)
+            .then(result => this.setState({permissions: result}))
+            .catch(e => {
+                this.resetPermissions();
+                // console.error('Error loading permissions', e);
+            });
     };
 
-    renderPermissions = (permissions) => {
+    handleOpenPermissionDialog = () => {
+        this.setState({
+            showPermissionDialog: true,
+        })
+    };
+
+    handleClosePermissionDialog = () => {
+        this.setState({
+            showPermissionDialog: false
+        })
+    };
+
+    static permissionLevel(p) {
+        return {Manage: 0, Write: 1, Read: 2}[p.access]
+    }
+
+    renderAccessRight = (p) => {
         return (
-            <div>
-                <Typography variant="subheading"
-                            onMouseEnter={this.toggleEditButton}
-                            onMouseLeave={this.toggleEditButton}>
-                    Shared with:&nbsp;
-                    {this.state.showEditButton ? <Icon style={{fontSize: '.9em'}}>edit</Icon> : ''}
-                </Typography>
-                <List>
-                    {
-                        permissions
-                            .sort(comparing(compareBy(Permissions.permissionLevel), compareBy('subject')))
-                            .map(p => (<ListItem>{`${p.subject} (${p.access})`}</ListItem>))
-                    }
-                </List>
-            </div>
-        )
+            <Select value={p.access} disabled>
+                <MenuItem value={'Manage'}><Typography variant={'body1'}>Manage</Typography></MenuItem>
+                <MenuItem value={'Read'}><Typography variant={'body1'}>Read</Typography></MenuItem>
+                <MenuItem value={'Write'}><Typography variant={'body1'}>Write</Typography></MenuItem>
+            </Select>
+        );
+    };
+
+    renderUserList = () => {
+        const permissions = this.state.permissions
+            .sort(comparing(compareBy(Permissions.permissionLevel), compareBy('subject')))
+            .map((p, idx) => {
+                return (
+                    <TableRow key={idx}>
+                        <TableCell component="th" scope="row">{p.subject}</TableCell>
+                        <TableCell>
+                            {this.renderAccessRight(p)}
+                        </TableCell>
+                        <TableCell>
+                            <IconButton aria-label="Delete" disabled>
+                                <DeleteIcon />
+                            </IconButton>
+                        </TableCell>
+                    </TableRow>
+                )
+            });
+        return (
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell numeric>User</TableCell>
+                            <TableCell numeric>Access</TableCell>
+                            <TableCell numeric>
+                                <IconButton aria-label="Add" onClick={this.handleOpenPermissionDialog}>
+                                    <AddIcon />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {permissions}
+                    </TableBody>
+                </Table>
+            );
     };
 
     render() {
-        return (<Loader what={'permissions'}
-                        onLoad={this.loadPermissions}
-                        onRender={this.renderPermissions}/>)
-    }
+        return (
+            <div>
+                <Typography variant="subheading">Shared with:</Typography>
+                <ShareWithDialog open={this.state.showPermissionDialog}
+                                 onClose={this.handleClosePermissionDialog}/>
+                {this.renderUserList()}
+            </div>
+        )
+    };
 }
 
 export default Permissions
---
+++ src/components/permissions/ShareWithDialog.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import {withStyles} from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1
+    },
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+    },
+});
+
+class ShareWithDialog extends React.Component {
+    state = {
+        single: null,
+        multi: null,
+    };
+
+    handleChange = name => value => {
+        this.setState({
+            [name]: value,
+        });
+    };
+
+    handleClose = () => {
+        this.props.onClose();
+    };
+
+    render() {
+        const {classes} = this.props;
+
+        return (
+            <Dialog
+                contentStyle={{width: 1250, maxWidth: "none"}}
+                open={this.props.open}
+                onClose={this.handleClose}>
+                <DialogTitle id="scroll-dialog-title">Share with</DialogTitle>
+                <DialogContent>
+                    <div className={classes.container}>
+                        <FormControl className={classes.formControl}>
+                            <TextField
+                                id="user-name"
+                                required
+                                label='User'
+                            />
+                            <FormHelperText>Select a user</FormHelperText>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel htmlFor="access-right">Access Right</InputLabel>
+                            <Select
+                                value={''}
+                                onChange={this.handleChange}
+                                input={<Input name="access-right" id="access-right"/>}
+                                required
+                            >
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                                <MenuItem value={10}>Manage</MenuItem>
+                                <MenuItem value={20}>Read</MenuItem>
+                                <MenuItem value={30}>Write</MenuItem>
+                            </Select>
+                            <FormHelperText>Select access right for the user.</FormHelperText>
+                        </FormControl>
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={this.handleClose} color="primary">
+                        Submit
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+}
+
+ShareWithDialog.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, {withTheme: true})(ShareWithDialog);
