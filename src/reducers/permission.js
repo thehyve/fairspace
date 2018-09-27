f5820b30916d294763cb9a24c65b99ab66d96e3b
Riza Nugraha <rnugraha@gmail.com>
2018-09-27 17:19:51 +0200

    Refactor permission API to use redux

--- src/reducers/permission.js
+++ src/reducers/permission.js
@@ -1,39 +1,32 @@
-import {SHOW_CONFIRM_DELETE_DIALOG, SHOW_PERMISSION_DIALOG} from "../actions/actionTypes";
+import {
+    PERMISSIONS_FULFILLED,
+    PERMISSIONS_PENDING,
+    PERMISSIONS_REJECTED
+} from "../actions/actionTypes";
 
 const defaultState = {
     pending: false,
     error: false,
     items: [],
-    showPermissionDialog:false,
-    showConfirmDeleteDialog:false
+    showPermissionDialog: false,
+    showConfirmDeleteDialog: false
 };
 
 const permissions = (state = defaultState, action) => {
-    console.log('action', action);
     switch (action.type) {
-        case SHOW_PERMISSION_DIALOG:
-            return {
-                ...state,
-                showPermissionDialog: !state.showPermissionDialog
-            };
-        case SHOW_CONFIRM_DELETE_DIALOG:
-            return {
-                ...state,
-                showConfirmDeleteDialog: !state.showConfirmDeleteDialog
-            };
-        case "PERMISSIONS_PENDING":
+        case PERMISSIONS_PENDING:
             return {
                 ...state,
                 pending: true,
                 error: false
             };
-        case "PERMISSIONS_FULFILLED":
+        case PERMISSIONS_FULFILLED:
             return {
                 ...state,
                 pending: false,
                 items: action.payload
             };
-        case "PERMISSIONS_REJECTED":
+        case PERMISSIONS_REJECTED:
             return {
                 ...state,
                 pending: false,
