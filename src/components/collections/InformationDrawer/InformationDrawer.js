4937939ee622ba0266646d9402e88fbfa1af255f
bo <bo@bos-MacBook-Pro-13.local>
2018-10-02 12:44:28 +0200

    move permision check up to information drawer

--- src/components/collections/InformationDrawer/InformationDrawer.js
+++ src/components/collections/InformationDrawer/InformationDrawer.js
@@ -14,6 +14,7 @@ import Metadata from "../../metadata/Metadata";
 import Permissions from '../../permissions/Permissions'
 import * as metadataActions from "../../../actions/metadata";
 import {connect} from 'react-redux';
+import permissionChecker from '../permissions/PermissionChecker';
 
 function InformationDrawer(props) {
     function handleDetailsChange(collection) {
@@ -59,7 +60,7 @@ function InformationDrawer(props) {
                     <ExpansionPanelDetails>
                         <Metadata
                             subject={collection.uri}
-                            collection={collection}
+                            editable={permissionChecker.canManage(collection)}
                             style={{width: '100%'}}
                         />
                     </ExpansionPanelDetails>
