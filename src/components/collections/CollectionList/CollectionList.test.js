88bae923c0a2bafcb419b722b8b643a6b8ee0abe
Robert Horlings <robert@isdat.nl>
2018-10-08 14:59:42 +0200

    Add unit test for S3 specific icon

--- src/components/collections/CollectionList/CollectionList.test.js
+++ src/components/collections/CollectionList/CollectionList.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CollectionList from "./CollectionList";
+import {shallow} from "enzyme";
+import Icon from "@material-ui/core/Icon";
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -12,4 +14,20 @@ it('renders without crashing with elements', () => {
     const div = document.createElement('div');
     ReactDOM.render(<CollectionList collections={[]}/>, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders separate icon for s3 buckets', () => {
+    const collections = [
+        {type: 'LOCAL_STORAGE', name: 'Test1', id: '1'},
+        {type: 'S3_BUCKET', name: 'Test2', id: '2'},
+        {name: 'Test3', id: '3'}
+    ]
+
+    const wrapper = shallow(<CollectionList collections={collections}/>);
+
+    const icons = wrapper.find(Icon);
+    expect(icons.length).toEqual(3);
+    expect(icons.get(0).props.children).toEqual('folder_open');
+    expect(icons.get(1).props.children).toEqual('cloud_open');
+    expect(icons.get(2).props.children).toEqual('folder_open');
 });
