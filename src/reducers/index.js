e7d9d8f3e5cab28e81670e4e97ae337346e89259
bo <bo@bos-MacBook-Pro-13.local>
2018-10-09 18:41:45 +0200

    resolve conflicts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,7 @@ import cache from './cache'
 import metadataBySubject from "./metadataBySubject";
 import collectionBrowser from "./collectionBrowser";
 import clipboard from "./clipboard";
-<<<<<<< HEAD
-=======
-import users from "./users";
 import workspace from './workspace';
->>>>>>> add footer
 
 export default combineReducers({
     account,
@@ -17,10 +13,6 @@ export default combineReducers({
     cache,
     metadataBySubject,
     collectionBrowser,
-<<<<<<< HEAD
-=======
-    users,
     workspace,
->>>>>>> add footer
     clipboard
 })
