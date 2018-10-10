8778fb5f6c3b07fd6aa59e373337d1c4c68230d4
strangepleasures <pavel@thehyve.nl>
2018-10-10 17:41:09 +0200

    VRE-245 Fix list of permissions is not updated when alter permissions

--- src/actions/permissions.js
+++ src/actions/permissions.js
@@ -2,9 +2,9 @@ import PermissionAPI from '../services/PermissionAPI/PermissionAPI'
 import {createErrorHandlingPromiseAction} from "../utils/redux";
 import {PERMISSIONS, ALTER_PERMISSION} from "./actionTypes";
 
-export const fetchPermissions = createErrorHandlingPromiseAction((collectionId) =>  ({
+export const fetchPermissions = createErrorHandlingPromiseAction((collectionId, noCache = false) =>  ({
         type: PERMISSIONS,
-        payload: PermissionAPI.getCollectionPermissions(collectionId)
+        payload: PermissionAPI.getCollectionPermissions(collectionId, noCache)
 }));
 
 export const alterPermission = createErrorHandlingPromiseAction((userId, collectionId, access) => ({
