464d8cac0124a74be62c46df55fa27d9f355d146
strangepleasures <pavel@thehyve.nl>
2018-10-10 12:22:17 +0200

    Add more tests

--- src/components/collections/CollectionList/CollectionEditor.js
+++ src/components/collections/CollectionList/CollectionEditor.js
@@ -11,7 +11,7 @@ class CollectionEditor extends React.Component {
     constructor(props) {
         super(props);
 
-            this.state = {};
+        this.state = {};
     }
 
     componentDidUpdate(prevProps, prevState) {
