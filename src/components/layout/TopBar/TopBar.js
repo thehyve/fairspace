dd1b62af5db512fc792a0c9b54309f03b770a249
bo <bo@bos-MacBook-Pro-13.local>
2018-10-10 15:09:34 +0200

    remove redundant lines

--- src/components/layout/TopBar/TopBar.js
+++ src/components/layout/TopBar/TopBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Config from "../../generic/Config/Config";
 import UserMenu from "../UserMenu/UserMenu";
 import {logout} from "../../App/logout";
 
