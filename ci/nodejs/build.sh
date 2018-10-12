6df81cfc8eed04cb3b7be5ae0f8736885945863c
Robert Horlings <robert@isdat.nl>
2018-10-12 14:05:57 +0200

    Upgrade gcc when building
    For building rdf-canonize (dependency for jsonld), we need at least
    g++ 4.9 and gcc 4.9. We have to do that manually for Ubuntu 14.04

--- ci/nodejs/build.sh
+++ ci/nodejs/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-npm install
-npm build
-CI=true DISPLAY=:99 npm test
+# The environment variables are set are due to the version of gcc
+# needing to be at least 4.9 to properly install the
+# jsonld dependency rdf-canonize.
+env CC=/usr/bin/gcc-4.9 CXX=/usr/bin/g++-4.9 yarn install
+CI=true DISPLAY=:99 yarn test
+yarn build
