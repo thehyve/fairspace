0faf16c86e47a79923d956c0ada5bcadf133eea6
Robert Horlings <robert@isdat.nl>
2018-10-15 12:16:54 +0200

    Use correct configuration prefix

--- app/src/main/java/nl/fairspace/pluto/app/config/dto/WorkspaceDetails.java
+++ app/src/main/java/nl/fairspace/pluto/app/config/dto/WorkspaceDetails.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "frontend")
+@ConfigurationProperties(prefix = "workspace")
 @Component
 @Data
 @JsonIgnoreProperties("$$beanFactory")
