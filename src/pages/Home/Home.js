713c0e861cee0f431a0d228398c635d47ac9ae59
Robert Horlings <robert@isdat.nl>
2018-10-15 15:31:32 +0200

    Add mock component for recent activity

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Typography from "@material-ui/core/Typography";
+import WithRightDrawer from "../../components/generic/WithRightDrawer/WithRightDrawer";
+import RecentActivity from "../../components/generic/RecentActivity/RecentActivity";
 
 function Home(props) {
     return (
-        <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+        <WithRightDrawer
+            collapsible={false}
+            mainContents={<Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>}
+            drawerContents={<RecentActivity />} />
     );
 }
 
