547f7cfff7cf0d8093d8be4b570bc0836279c05b
Riza Nugraha <rnugraha@gmail.com>
2018-10-15 09:46:41 +0200

    Revert back remove desc

--- src/components/collections/CollectionList/CollectionItem.js
+++ src/components/collections/CollectionList/CollectionItem.js
@@ -3,7 +3,17 @@ import Typography from "@material-ui/core/Typography";
 
 function Collection(props) {
     const {collection} = props;
-    return (<Typography>{collection.name}</Typography>);
+
+    return (
+        <div>
+            <Typography variant="subtitle1">{collection.name}</Typography>
+            {collection.description}
+        </div>
+    );
 }
 
 export default Collection;
+
+
+
+
