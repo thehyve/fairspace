f5d7720fc255575dcc6ba36f2f38276dfe48216e
Robert Horlings <robert@isdat.nl>
2018-10-16 15:22:02 +0200

    Avoid console logging for tests

--- ci/nodejs/build.sh
+++ ci/nodejs/build.sh
@@ -4,5 +4,5 @@
 # needing to be at least 4.9 to properly install the
 # jsonld dependency rdf-canonize.
 env CC=/usr/bin/gcc-4.9 CXX=/usr/bin/g++-4.9 yarn install
-CI=true DISPLAY=:99 yarn test
+CI=true DISPLAY=:99 yarn test --silent
 yarn build
