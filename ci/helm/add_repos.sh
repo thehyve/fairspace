fdce886a1bdd8225fdbdc1f4060682479f3d5833
Robert Horlings <robert@isdat.nl>
2018-10-16 13:27:20 +0200

    Fix repo url

--- ci/helm/add_repos.sh
+++ ci/helm/add_repos.sh
@@ -2,5 +2,5 @@
 
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo add chartmuseum https://chartmuseum.jx.test.fairdev.app/
-helm repo add fairspace https://fairspace.github.io/help-repo/
+helm repo add fairspace https://fairspace.github.io/helm-repo/
 helm repo update
