68e10108850ea36993b37123bda6326196a31d91
Sietse Snel <sietse@thehyve.nl>
2018-10-16 13:59:02 +0200

    add Travis variable for build.gradle filename

--- ci/gradle/tag.sh
+++ ci/gradle/tag.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #
 # Required env variables:
-#   $APPNAME
+#   $BUILD_GRADLE_FILE
 #   $VERSION
-#
-#   $GITHUB_USERNAME
-#   $GITHUB_PASSWORD
-#
 
-# Update version number and repository
+# Update version number in build.gradle
 #
-# This script has been adapted to use a nonstandard location for the build.gradle file location.
-sed -i -e "s/version = 'RELEASEVERSION'/version = '$VERSION'/" app/build.gradle
+sed -i -e "s/version = 'RELEASEVERSION'/version = '$VERSION'/" $BUILD_GRADLE_FILE
