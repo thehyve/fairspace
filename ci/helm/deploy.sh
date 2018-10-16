97de544b0e86438e629693a03bdf716cd0f41106
Robert Horlings <robert@isdat.nl>
2018-10-16 13:00:52 +0200

    Deploy workspace instead of hyperspace

--- ci/helm/deploy.sh
+++ ci/helm/deploy.sh
@@ -4,4 +4,4 @@ ENVIRONMENT=${1:-ci}
 
 helm repo add fairspace https://fairspace.github.io/helm-repo
 helm repo update
-helm upgrade --install hyperspace-${ENVIRONMENT} fairspace/hyperspace --namespace=hyperspace-${ENVIRONMENT} --version $VERSION -f ./ci/config/${ENVIRONMENT}-values.yaml
+helm upgrade --install ${APPNAME}-${ENVIRONMENT} fairspace/${APPNAME} --namespace=${APPNAME}-${ENVIRONMENT} --version $VERSION -f ./ci/config/${ENVIRONMENT}-values.yaml
