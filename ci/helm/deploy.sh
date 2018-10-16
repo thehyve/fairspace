afd2391b9290eef935790741bc10aeee80f01693
Robert Horlings <robert@isdat.nl>
2018-10-16 13:41:02 +0200

    Update helm repo before deployment

--- ci/helm/deploy.sh
+++ ci/helm/deploy.sh
@@ -2,6 +2,4 @@
 
 ENVIRONMENT=${1:-ci}
 
-helm repo add fairspace https://fairspace.github.io/helm-repo
-helm repo update
 helm upgrade --install ${APPNAME}-${ENVIRONMENT} fairspace/${APPNAME} --namespace=${APPNAME}-${ENVIRONMENT} --version $VERSION -f ./ci/config/${ENVIRONMENT}-values.yaml
