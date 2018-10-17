db84deacd210d467bad966aa20a71c731b98ba6b
Robert Horlings <robert@isdat.nl>
2018-10-17 10:58:09 +0200

    Store menu expansion state in local storage
    This stores the state between refreshes of the page.
    Fixes VRE-172 and VRE-262

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -1,15 +1,25 @@
 import {TOGGLE_MENU} from "../actions/actionTypes";
 
+const LOCAL_STORAGE_MENU_KEY = 'FAIRSPACE_MENU_EXPANDED';
+
+// Set the default value based on the value from local storage
+// Localstorage stores values as strings. As the default is true
+// we only check whether the value is explicitly set to 'false'
+const localStorageValue = localStorage.getItem(LOCAL_STORAGE_MENU_KEY);
 const defaultState = {
-    menuExpanded: true
+    menuExpanded: localStorageValue === 'false' ? false : true
 };
 
 const ui = (state = defaultState, action) => {
     switch (action.type) {
         case TOGGLE_MENU:
+            // Store information in local storage
+            const newMenuExpandedState = !state.menuExpanded;
+            localStorage.setItem(LOCAL_STORAGE_MENU_KEY, newMenuExpandedState);
+
             return {
                 ...state,
-                menuExpanded: !state.menuExpanded
+                menuExpanded: newMenuExpandedState
             }
         default:
             return state;
