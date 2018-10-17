654a5a03be4dd4e0013c85359a14022252522823
Robert Horlings <robert@isdat.nl>
2018-10-17 13:36:53 +0200

    Use external build scripts to DRY

--- ci/nodejs/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# The environment variables are set are due to the version of gcc
-# needing to be at least 4.9 to properly install the
-# jsonld dependency rdf-canonize.
-env CC=/usr/bin/gcc-4.9 CXX=/usr/bin/g++-4.9 yarn install
-CI=true DISPLAY=:99 yarn test --silent
-yarn build
--- ci/nodejs/tag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Required env variables:
-#   $VERSION
-#
-
-# Update version number and repository
-sed -i -e "s/\"version\": \"0.0.0-RELEASEVERSION\"/\"version\": \"$VERSION\"/" package.json
