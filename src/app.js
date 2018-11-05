ed6ab54e0a88dbb4bfe8214bb1518f6ac496ef14
Robert Horlings <robert@isdat.nl>
2018-11-05 13:06:16 +0100

    Use separate configuration file for all parameters

--- src/app.js
+++ src/app.js
@@ -1,23 +1,36 @@
 const app = require('express')();
 const setupTracingMiddleware = require('./config/setupTracingMiddleware');
 const setupWebdavMiddleware = require('./config/setupWebdavMiddleware');
-const setupMetadataMiddleware = require('./config/setupMetadataMiddleware');
 
 // Configuration parameters
-const rootPath = process.env.FILES_FOLDER || '/data';
-const basePath = '/api/storage/webdav';
-const authEnabled = process.env.AUTH_ENABLED !== 'false';
-const tracingEnabled = process.env.TRACING_ENABLED !== 'false';
-const zipkinEndointUrl = process.env.ZIPKIN_URL || 'http://jaeger-collector.jaeger:9411';
-const zipkinSamplingRate = process.env.ZIPKIN_SAMPLING_RATE || 0.01;
-const permissionsEndpointUrl = process.env.PERMISSIONS_URL;
-const metadataEndpointUrl = process.env.METADATA_URL;
+const defaultConfig = {
+    "rootPath": "/data",
+    "basePath":"/api/storage/webdav",
+    "auth": {
+        "enabled": true
+    },
+    "tracing": {
+        "enabled": true,
+        "zipkinUrl": "",
+        "samplingRate": 0.01
+    },
+    "urls":{
+        "collections": ""
+    }
+}
+
+// Read external configuration file
+let configuration;
+if(process.env.CONFIG_FILE) {
+    configuration = {...defaultConfig, ...require(process.env.CONFIG_FILE)}
+} else {
+    configuration = defaultConfig;
+}
 
 // Respond to / anonymously to allow for health checks
 app.get('/', (req, res, next) => req.get('probe') ? res.send('Hi, I\'m Titan!').end() : next());
 
-if(tracingEnabled) setupTracingMiddleware(app, zipkinEndointUrl, zipkinSamplingRate);
-setupWebdavMiddleware(app, rootPath, basePath, authEnabled, permissionsEndpointUrl);
-setupMetadataMiddleware(app, metadataEndpointUrl);
+if(configuration.tracing.enabled) setupTracingMiddleware(app, configuration.tracing);
+setupWebdavMiddleware(app, configuration.rootPath, configuration.basePath, configuration.auth.enabled, configuration.urls.collections);
 
 module.exports = app;
---
+++ test/test-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "rootPath": __dirname + "/fs",
+  "urls": {
+    "collections": "http://fairspace.io/api/collections/permissions?location=%s"
+  }
+}
