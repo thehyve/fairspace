b7417a506d27d93e2f9d99668db660be20fff9ee
Robert Horlings <robert@isdat.nl>
2018-11-09 16:21:57 +0100

    Clean destination header before using it in events

--- src/events/EventEmitter.js
+++ src/events/EventEmitter.js
@@ -18,6 +18,20 @@ module.exports = function EventEmitter(rabbot, collectionApi, fileTypeProvider, 
         return collectionApi.retrieveCollection(paths[1], user)
     }
 
+    const getDestination = req => {
+        const destination = req.headers['destination']
+
+        // Check whether the destination contains a scheme and host/port
+        // If so, remove it
+        const startIndex = destination.indexOf('://');
+        if(startIndex !== -1) {
+            // Remove scheme and the hostname + port
+            return destination.substring(destination.indexOf('/', startIndex + '://'.length))
+        } else {
+            return destination;
+        }
+    }
+
     const readEvent = req => ({
         routingKey: 'read',
         type: "io.fairspace.titan.read",
@@ -57,7 +71,7 @@ module.exports = function EventEmitter(rabbot, collectionApi, fileTypeProvider, 
         routingKey: 'copy',
         type: "io.fairspace.titan.copy",
         body: {
-            destination: req.headers['destination']
+            destination: getDestination(req)
         }
     })
 
@@ -65,7 +79,7 @@ module.exports = function EventEmitter(rabbot, collectionApi, fileTypeProvider, 
         routingKey: 'move',
         type: "io.fairspace.titan.move",
         body: {
-            destination: req.headers['destination']
+            destination: getDestination(req)
         }
     })
 
