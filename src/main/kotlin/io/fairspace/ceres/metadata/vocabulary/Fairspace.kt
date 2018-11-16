540900c7e526729dc02cdb17ce1727d5b07e0f50
Robert Horlings <robert@isdat.nl>
2018-11-16 11:47:57 +0100

    Update entity class names to reflect the vocabulary

--- src/main/kotlin/io/fairspace/ceres/metadata/vocabulary/Fairspace.kt
+++ src/main/kotlin/io/fairspace/ceres/metadata/vocabulary/Fairspace.kt
@@ -2,6 +2,6 @@ package io.fairspace.ceres.metadata.vocabulary
 
 object Fairspace {
     val partOf = "http://fairspace.io/ontology#partOf"
-    val file = "http://fairspace.io/ontology#file"
-    val directory = "http://fairspace.io/ontology#directory"
+    val file = "http://fairspace.io/ontology#File"
+    val directory = "http://fairspace.io/ontology#Directory"
 }
