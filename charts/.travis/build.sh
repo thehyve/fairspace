84dd39437c5129f071f624b24e12fd66d102557a
Robert Horlings <robert@isdat.nl>
2018-11-19 15:08:23 +0100

    Small bash script fix

--- charts/.travis/build.sh
+++ charts/.travis/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 $BUILD_SCRIPTS_DIR/helm/tag.sh
 $BUILD_SCRIPTS_DIR/helm/add_repos.sh
-if [[ $ALLOW_SNAPSHOTS ]]; then $BUILD_SCRIPTS_DIR/helm/allow_snapshot_dependencies.sh; fi'
+if [[ $ALLOW_SNAPSHOTS ]]; then $BUILD_SCRIPTS_DIR/helm/allow_snapshot_dependencies.sh; fi
 $BUILD_SCRIPTS_DIR/helm/build.sh
 
 if [[ $SHOULD_RELEASE ]]; then
