1f0de028e17e9e45f0a1c18af04b73037ae9f4c4
Robert Horlings <robert@isdat.nl>
2018-11-20 10:31:42 +0100

    Install kubectl and azure when releasing

---
+++ .travis/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $SHOULD_RELEASE ]]; then
+  $BUILD_SCRIPTS_DIR/az/install.sh
+  $BUILD_SCRIPTS_DIR/az/login.sh
+  $BUILD_SCRIPTS_DIR/k8s/install_kubectl.sh
+  $BUILD_SCRIPTS_DIR/k8s/config_kubectl.sh
+  $BUILD_SCRIPTS_DIR/k8s/switch_context.sh
+fi
