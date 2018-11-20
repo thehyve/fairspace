284ac77c0803f26145fc730f77abca4366ef70a0
Robert Horlings <robert@isdat.nl>
2018-11-20 10:59:21 +0100

    Add support for [full build] to trigger building all projects

--- .travis/build-condition.sh
+++ .travis/build-condition.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+#
+# This script will check whether a certain directory contains changes
+# and thus should be built.
+#
+# This check can be overridden by adding [full build] to the commit message
+#
+# Usage: build-condition.sh <commitrange> <path>
+#
 
 if [[ -z $1 ]]; then
     echo "Commit range cannot be empty"
+    echo "Usage: build-condition.sh <commitrange> <path>"
     exit 1
 fi
 
 if [[ -z $2 ]]; then
     echo "Change path cannot be empty"
+    echo "Usage: build-condition.sh <commitrange> <path>"
     exit 1
+fi
+
+if [[ $TRAVIS_COMMIT_MESSAGE == *"[full build]"* ]];then
+    # Commit message contains '[full build]' and all
+    # directories should be built
 fi
 
 git diff --name-only $1 | sort -u | uniq | grep $2 > /dev/null
