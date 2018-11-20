da734723c608349602972009d6851ebff64e7b0d
Robert Horlings <robert@isdat.nl>
2018-11-20 10:31:59 +0100

    Login to docker for retrieving base image

--- projects/jupyterhub-singleuser-docker/.travis/build.sh
+++ projects/jupyterhub-singleuser-docker/.travis/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "${DOCKER_PASSWORD}" | docker login ${DOCKER_REPO} -u "${DOCKER_USERNAME}" --password-stdin
 docker build ./images/full --tag "${CONTAINER_NAME}" --tag "${CONTAINER_NAME}-full"
 docker build ./images/simple --tag "${CONTAINER_NAME}-simple"
