5af2a31cd97e138163b31f8d2f88356fe904102c
Robert Horlings <robert@isdat.nl>
2018-11-20 10:40:45 +0100

    Add kubectl to path in travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,4 +6,5 @@ if [[ $SHOULD_RELEASE ]]; then
   $BUILD_SCRIPTS_DIR/k8s/install_kubectl.sh
   $BUILD_SCRIPTS_DIR/k8s/config_kubectl.sh
   $BUILD_SCRIPTS_DIR/k8s/switch_context.sh
+  export PATH="$HOME/downloads/v${KUBECTL_VERSION}:$PATH"
 fi
--- charts/.travis/install.sh
+++ charts/.travis/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 source $BUILD_SCRIPTS_DIR/helm/install_helm.sh
-export PATH="$HOME/downloads/v${KUBECTL_VERSION}:$PATH"
