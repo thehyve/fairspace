4ca11128111c5c6f340dfb73c1f4bfd54ffb8ae5
Robert Horlings <robert@isdat.nl>
2018-11-20 10:33:11 +0100

    Return error code from scripts on failure

--- projects/pluto/.travis/build.sh
+++ projects/pluto/.travis/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-$BUILD_SCRIPTS_DIR/gradle/tag.sh
-$BUILD_SCRIPTS_DIR/gradle/build.sh
+$BUILD_SCRIPTS_DIR/gradle/tag.sh || exit 1
+$BUILD_SCRIPTS_DIR/gradle/build.sh || exit 1
 $BUILD_SCRIPTS_DIR/docker/build.sh
