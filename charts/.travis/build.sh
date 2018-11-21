3f2b5c5a2328b43dc6f4012be1ed650fa4be5cbd
Robert Horlings <robert@isdat.nl>
2018-11-21 11:57:20 +0100

    Fix pod annotations [skip ci]

--- charts/.travis/build.sh
+++ charts/.travis/build.sh
@@ -2,7 +2,6 @@
 $BUILD_SCRIPTS_DIR/helm/tag.sh
 .travis/add_pod_annotations.sh
 $BUILD_SCRIPTS_DIR/helm/add_repos.sh
-$BUILD_SCRIPTS_DIR/helm/add_pod_annotations.sh
 $BUILD_SCRIPTS_DIR/helm/build.sh
 
 if [[ $SHOULD_RELEASE ]]; then
