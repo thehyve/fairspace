7f33a961ad341a599e9869e6a5072b7ecfd3ac1f
bo <bo.gao111@gmail.com>
2018-11-27 11:46:56 +0100

    show lable and comment in metadata entity

--- projects/mercury/src/pages/Metadata/MetadataEntityPage.js
+++ projects/mercury/src/pages/Metadata/MetadataEntityPage.js
@@ -5,28 +5,57 @@ import metadataAPI from "../../services/MetadataAPI/MetadataAPI";
 import Metadata from '../../components/metadata/Metadata';
 import asPage from "../../containers/asPage/asPage";
 import BreadCrumbs from "../../components/generic/BreadCrumbs/BreadCrumbs";
+import connect from 'react-redux/es/connect/connect';
+import {LABEL_URI, COMMENT_URI} from '../../services/MetadataAPI/MetadataAPI';
 
-function MetadataEntityPage(props) {
-    const {match: { params }} = props;
+export class MetadataEntityPage extends React.Component{
 
-    return (
-        <div>
-            <BreadCrumbs />
+    render() {
+        return (
+            <div>
+                <BreadCrumbs />
 
-            <List>
-                <ListItem>Id: {params.id}</ListItem>
-            </List>
+                <List>
+                    <ListItem>Id: {this.props.id}</ListItem>
+                    <ListItem>Id: {this.props.label}</ListItem>
+                    <ListItem>Id: {this.props.comment}</ListItem>
+                </List>
 
-            <Metadata
-                editable={true}
-                metadataAPI={metadataAPI}
-                subject={`${window.location.origin}/iri/${params.type}/${params.id}`}
-            />
-        </div>
-    );
+                <Metadata
+                    editable={true}
+                    metadataAPI={metadataAPI}
+                    subject={this.props.subject}
+                />
+            </div>
+        );
+    }
 }
 
-export default asPage(MetadataEntityPage);
+const mapStateToProps = (state, ownProps) => {
+    console.log('state', state, ', ownProps: ', ownProps);
+    const {match: { params }} = ownProps;
+    const subject = `${window.location.origin}/iri/${params.type}/${params.id}`;
+    const data = state['metadataBySubject'][subject]['data'];
+    const labelProp = data.find(prop => {
+        return prop.key === LABEL_URI;
+    });
+    const label = labelProp && labelProp.values && labelProp.values[0] ?
+        labelProp.values[0] : undefined;
+    const commentProp = data.find(prop => {
+        return prop.key === COMMENT_URI;
+    });
+    const comment = commentProp && commentProp.values && commentProp.values[0] ?
+        commentProp.values[0] : undefined;
+
+    return {
+        subject: subject,
+        id: params.id,
+        label: label,
+        comment: comment
+    };
+}
+
+export default connect(mapStateToProps)(asPage(MetadataEntityPage));
 
 
 
