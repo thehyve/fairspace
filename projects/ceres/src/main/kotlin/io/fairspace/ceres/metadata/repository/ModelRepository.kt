edb8ddcde2740e9194a3df919c88d922e779803c
strangepleasures <pavel@thehyve.nl>
2018-11-29 12:21:53 +0100

    VRE-282 Implement deletion of inferred triples

--- projects/ceres/src/main/kotlin/io/fairspace/ceres/metadata/repository/ModelRepository.kt
+++ projects/ceres/src/main/kotlin/io/fairspace/ceres/metadata/repository/ModelRepository.kt
@@ -6,9 +6,11 @@ import org.apache.jena.query.Query.*
 import org.apache.jena.query.QueryExecutionFactory
 import org.apache.jena.query.QueryFactory
 import org.apache.jena.query.ResultSet
+import org.apache.jena.rdf.model.InfModel
 import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.rdf.model.ModelFactory.createInfModel
+import org.apache.jena.rdf.model.RDFNode
 import org.apache.jena.reasoner.Reasoner
 import org.apache.jena.sparql.resultset.ResultSetMem
 import org.apache.jena.system.Txn.calculateRead
@@ -16,35 +18,37 @@ import org.apache.jena.system.Txn.executeWrite
 import org.springframework.stereotype.Component
 
 @Component
-class ModelRepository(private val dataset: Dataset, reasoner: Reasoner) {
-    private val model = ThreadLocal.withInitial { createInfModel(reasoner, dataset.defaultModel) }
+class ModelRepository(private val dataset: Dataset, private val reasoner: Reasoner) {
+    private val models = ThreadLocal.withInitial { extendedModel(dataset.defaultModel) }
 
     private val log = logger {}
 
-    fun list(subject: String?, predicate: String? = null, obj: String? = null): Model {
+    fun list(subject: String? = null, predicate: String? = null, obj: String? = null): Model {
         log.trace { "Retrieving statements for s: $subject p: $predicate o: $obj" }
-        return read {
-            listStatements(subject?.let(::createResource), predicate?.let(::createProperty), obj?.let(::createResource))
+        return read { model ->
+            model.listStatements(subject?.let(model::createResource), predicate?.let(model::createProperty), obj?.let(model::createResource))
                     .toModel()
         }
     }
 
     fun add(delta: Model) {
         log.trace { "Adding statements: $delta" }
-        write { add(delta) }
+        write { model -> model.add(delta) }
     }
 
-    fun remove(subject: String?, predicate: String? = null) {
+    fun remove(subject: String?, predicate: String? = null, obj: String? = null) {
         log.trace { "Removing statements for s: $subject p: $predicate" }
-        write {
-            removeAll(subject?.let(::createResource), predicate?.let(::createProperty), null)
+        write { model ->
+            extendedModel(list(subject, predicate, obj))
+                    .listStatements()
+                    .forEach { model.remove(it) }
         }
     }
 
     fun query(queryString: String): Any { // ResultSet | Model | Boolean
         log.trace { "Executing SPARQL: ${toSingleLine(queryString)}" }
-        return read {
-            QueryExecutionFactory.create(QueryFactory.create(queryString), this).run {
+        return read { model ->
+            QueryExecutionFactory.create(QueryFactory.create(queryString), model).run {
                 when (query.queryType) {
                     QueryTypeSelect -> execSelect().detach()
                     QueryTypeConstruct -> execConstruct().detach()
@@ -58,21 +62,27 @@ class ModelRepository(private val dataset: Dataset, reasoner: Reasoner) {
 
     fun update(delta: Model) {
         log.trace { "Updating statements: $delta" }
-        write {
-            delta.listStatements().forEach {
-                removeAll(it.subject, it.predicate, null)
-            }
-            add(delta)
+        write { model ->
+            delta.listStatements()
+                    .forEach {
+                        val siblings = model.listStatements(it.subject, it.predicate, null as? RDFNode).toModel()
+                        extendedModel(siblings)
+                                .listStatements()
+                                .forEach { stmt -> model.remove(stmt) }
+                    }
+            model.add(delta)
         }
     }
 
-    private fun <R> read(action: Model.() -> R): R = calculateRead(dataset) { action(model.get()) }
+    private fun <R> read(action: (InfModel) -> R): R = calculateRead(dataset) { action(models.get()) }
 
-    private fun write(action: Model.() -> Unit) = executeWrite(dataset) { action(model.get()) }
+    private fun write(action: (InfModel) -> Unit) = executeWrite(dataset) { action(models.get()) }
 
     private fun ResultSet.detach() = ResultSetMem(this)
 
     private fun Model.detach() = ModelFactory.createDefaultModel().add(this)
+
+    private fun extendedModel(model: Model) = createInfModel(reasoner, model)
 
     private fun toSingleLine(s: String) =
             s.splitToSequence('\n')
