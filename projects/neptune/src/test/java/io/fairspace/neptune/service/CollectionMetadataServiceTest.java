bde80f42b24f48896cd9699d320ccec8b338f477
Robert Horlings <robert@isdat.nl>
2018-12-04 13:43:18 +0100

    Store triple with username for newly created collections. Fixes VRE-327

--- projects/neptune/src/test/java/io/fairspace/neptune/service/CollectionMetadataServiceTest.java
+++ projects/neptune/src/test/java/io/fairspace/neptune/service/CollectionMetadataServiceTest.java
@@ -1,5 +1,6 @@
 package io.fairspace.neptune.service;
 
+import io.fairspace.neptune.config.upstream.AuthorizationContainer;
 import io.fairspace.neptune.model.Collection;
 import io.fairspace.neptune.vocabulary.Fairspace;
 import org.apache.jena.rdf.model.Model;
@@ -20,6 +21,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CollectionMetadataServiceTest {
@@ -32,25 +34,31 @@ public class CollectionMetadataServiceTest {
     private static final String CREATOR = "user1";
     private static final String EXPECTED_USER_URI = BASE_URL + "/iri/users/" + CREATOR;
     private static final ZonedDateTime CREATIONDATETIME = ZonedDateTime.now(ZoneOffset.UTC);
+    public static final String USER_FULLNAME = "User first lastname";
 
     @Mock
     private TripleService tripleService;
+
+    @Mock
+    private AuthorizationContainer authorizationContainer;
 
     private CollectionMetadataService collectionMetadataService;
 
     @Before
     public void setUp() throws Exception {
-        collectionMetadataService = new CollectionMetadataService(tripleService, BASE_URL);
+        collectionMetadataService = new CollectionMetadataService(tripleService, authorizationContainer, BASE_URL);
     }
 
     @Test
     public void collectionsWithAllPropertiesShouldBeAccepted() throws IOException {
+        when(authorizationContainer.getFullname()).thenReturn(USER_FULLNAME);
+
         Collection c = getCollection();
 
         collectionMetadataService.createCollection(c);
 
         verify(tripleService, times(1))
-                .postTriples(argThat(m -> m.size() == 5
+                .postTriples(argThat(m -> m.size() == 6
                         && m.contains(
                         m.createResource(EXPECTED_COLLECTION_URI),
                         RDF.type,
@@ -68,6 +76,10 @@ public class CollectionMetadataServiceTest {
                         Fairspace.creator,
                         m.createResource(EXPECTED_USER_URI))
                         && m.contains(
+                        m.createResource(EXPECTED_USER_URI),
+                        RDFS.label,
+                        m.createLiteral(USER_FULLNAME))
+                        && m.contains(
                         m.createResource(EXPECTED_COLLECTION_URI),
                         Fairspace.dateCreated,
                         m.createTypedLiteral(GregorianCalendar.from(c.getDateCreated())))
@@ -76,6 +88,8 @@ public class CollectionMetadataServiceTest {
 
     @Test
     public void collectionsWithEmptyDescriptionShouldBeAccepted() {
+        when(authorizationContainer.getFullname()).thenReturn(USER_FULLNAME);
+
         Collection c = Collection.builder()
                 .id(COLLECTION_ID)
                 .name(COLLECTION_NAME)
@@ -86,7 +100,7 @@ public class CollectionMetadataServiceTest {
         collectionMetadataService.createCollection(c);
 
         verify(tripleService, times(1))
-                .postTriples(argThat(m -> m.size() == 5
+                .postTriples(argThat(m -> m.size() == 6
                         && m.contains(
                         m.createResource(EXPECTED_COLLECTION_URI),
                         RDF.type,
@@ -103,6 +117,10 @@ public class CollectionMetadataServiceTest {
                         m.createResource(EXPECTED_COLLECTION_URI),
                         Fairspace.creator,
                         m.createResource(EXPECTED_USER_URI))
+                        && m.contains(
+                        m.createResource(EXPECTED_USER_URI),
+                        RDFS.label,
+                        m.createLiteral(USER_FULLNAME))
                         && m.contains(
                         m.createResource(EXPECTED_COLLECTION_URI),
                         Fairspace.dateCreated,
