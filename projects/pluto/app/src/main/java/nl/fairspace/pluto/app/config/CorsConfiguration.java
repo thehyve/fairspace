4e487d797e4fe6f3831f090250711bd0fbcac9ed
Robert Horlings <robert@isdat.nl>
2018-12-05 10:34:10 +0100

    Enable CORS preflight requests from other domains

--- projects/pluto/app/src/main/java/nl/fairspace/pluto/app/config/CorsConfiguration.java
+++ projects/pluto/app/src/main/java/nl/fairspace/pluto/app/config/CorsConfiguration.java
@@ -2,8 +2,10 @@ package nl.fairspace.pluto.app.config;
 
 import nl.fairspace.pluto.app.config.dto.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -28,7 +30,24 @@ public class CorsConfiguration {
 
     @Bean
     CorsFilter corsFilter() {
-        return new CorsFilter(corsConfigurationSource());
+        CorsFilter corsFilter = new CorsFilter(corsConfigurationSource());
+        corsFilter.setCorsProcessor(new AllowWebDavCorsProcessor());
+        return corsFilter;
+    }
+
+    /**
+     * This registration bean makes sure that the corsFilter is added as the first filter
+     * This is needed, because in certain cases another corsFilter (with default settings) is added to
+     * the filter chain. This seems to be happing when bypassing the spring servlet (on the /zuul/* endpoints).
+     * This filter effectively intercepts the preflight requests and responds properly.
+     *
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>(corsFilter());
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registrationBean;
     }
 }
 
