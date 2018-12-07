6fba5c05015e0c217059b3a4ed25b8fdc9ad443b
strangepleasures <pavel@thehyve.nl>
2018-12-07 14:31:10 +0100

    Fix transaction conflicts

--- projects/ceres/src/main/kotlin/io/fairspace/ceres/config/JenaConfiguration.kt
+++ projects/ceres/src/main/kotlin/io/fairspace/ceres/config/JenaConfiguration.kt
@@ -1,7 +1,7 @@
 package io.fairspace.ceres.config
 
 import io.fairspace.ceres.metadata.repository.OwlPropertyInverter
-import org.apache.jena.tdb2.TDB2Factory
+import org.apache.jena.tdb2.TDB2Factory.connectDataset
 import org.apache.jena.util.FileManager
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -10,10 +10,10 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class JenaConfiguration {
     @Value("\${jena.dataset.path}")
-    lateinit var datasetPath: String;
+    lateinit var datasetPath: String
 
     @Bean
-    fun model() = TDB2Factory.connectDataset(datasetPath).defaultModel
+    fun dataset() = connectDataset(datasetPath)
 
     @Bean
     fun enhancer() = OwlPropertyInverter(FileManager.get().loadModel("inference-model.jsonld"))
