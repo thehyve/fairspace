aff92cc10a0b76504353625bfe6321b2b5a14e5b
Robert Horlings <robert@isdat.nl>
2018-12-19 13:32:59 +0100

    Increase e2e timeouts

--- projects/janus/.travis/build.sh
+++ projects/janus/.travis/build.sh
@@ -4,7 +4,7 @@ set -e
 npm ci
 
 export METADATA_PROPAGATION_TIME=1000
-export REQUEST_TIMEOUT=2000
+export REQUEST_TIMEOUT=4000
 
-$(npm bin)/mocha api-tests/*.js --timeout 10000
+$(npm bin)/mocha api-tests/*.js --timeout 12000
 $(npm bin)/cypress run
