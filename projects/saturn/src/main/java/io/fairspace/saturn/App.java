ca57813b390693e7f92e315c8e102cdd20f5d9f6
strangepleasures <pavel@thehyve.nl>
2019-01-24 14:09:38 +0100

    Support local development

--- projects/saturn/src/main/java/io/fairspace/saturn/App.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/App.java
@@ -3,12 +3,19 @@ package io.fairspace.saturn;
 import org.apache.jena.fuseki.main.FusekiServer;
 import org.apache.jena.tdb2.TDB2Factory;
 
+import java.io.File;
+
 public class App {
+    private static final String DEFAULT_DATASET_PATH = "/data/saturn/db";
+    private static final String LOCAL_DATASET_PATH = "db";
+
     public static void main(String[] args) {
         System.out.println("Saturn is starting");
 
+        String datasetPath = new File(DEFAULT_DATASET_PATH).exists() ? DEFAULT_DATASET_PATH : LOCAL_DATASET_PATH;
+
         FusekiServer.create()
-                .add("/rdf", TDB2Factory.connectDataset("/data/saturn/db"))
+                .add("/rdf", TDB2Factory.connectDataset(datasetPath))
                 .port(8080)
                 .build()
                 .start();
