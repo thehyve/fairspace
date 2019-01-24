9dc2f4b98c3bd39c210b15e686620f3da47d9522
strangepleasures <pavel@thehyve.nl>
2019-01-24 13:35:48 +0100

    Trivia

--- projects/saturn/src/main/java/io/fairspace/saturn/App.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/App.java
@@ -5,6 +5,8 @@ import org.apache.jena.tdb2.TDB2Factory;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Saturn is starting");
+
         FusekiServer.create()
                 .add("/rdf", TDB2Factory.connectDataset("/data/saturn/db"))
                 .port(8080)
