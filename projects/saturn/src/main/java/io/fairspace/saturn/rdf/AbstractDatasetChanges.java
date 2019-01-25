4b7c2ffcee32c6321ec4efdf8b7b1ae784526236
strangepleasures <pavel@thehyve.nl>
2019-01-25 16:33:04 +0100

    Add javadoc

--- projects/saturn/src/main/java/io/fairspace/saturn/rdf/AbstractDatasetChanges.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/rdf/AbstractDatasetChanges.java
@@ -2,6 +2,9 @@ package io.fairspace.saturn.rdf;
 
 import org.apache.jena.sparql.core.DatasetChanges;
 
+/**
+ * A class providing default implementations for rarely used methods of DatasetChanges
+ */
 public abstract class AbstractDatasetChanges implements DatasetChanges {
     @Override
     public void start() {
