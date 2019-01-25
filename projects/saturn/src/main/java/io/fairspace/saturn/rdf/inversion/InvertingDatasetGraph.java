aceaabcdee8567ef14436ab4a8bd9c0e00a5f1e0
strangepleasures <pavel@thehyve.nl>
2019-01-25 16:52:29 +0100

    Add comments

--- projects/saturn/src/main/java/io/fairspace/saturn/rdf/inversion/InvertingDatasetGraph.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/rdf/inversion/InvertingDatasetGraph.java
@@ -12,6 +12,10 @@ import java.util.Map;
 import static io.fairspace.saturn.rdf.Vocabulary.VOCABULARY_GRAPH;
 import static org.apache.jena.system.Txn.executeRead;
 
+/**
+ * A graph wrapper performing inference of inverse properties defined in the vocabulary.
+ * It picks up new rules when they are added.
+ */
 public class InvertingDatasetGraph extends DatasetGraphMonitor {
     private static final Node inverseOf = OWL.inverseOf.asNode();
 
@@ -38,6 +42,7 @@ public class InvertingDatasetGraph extends DatasetGraphMonitor {
 
         private Inverter(DatasetGraph dsg) {
             this.dsg = dsg;
+            // Load inversion rules from the dictionary
             executeRead(dsg, () -> dsg.find(VOCABULARY_GRAPH, Node.ANY, inverseOf, Node.ANY)
                     .forEachRemaining(quad -> {
                         propertiesMap.put(quad.getSubject(), quad.getObject());
@@ -49,20 +54,24 @@ public class InvertingDatasetGraph extends DatasetGraphMonitor {
         public void change(QuadAction action, Node graph, Node subject, Node predicate, Node object) {
             switch (action) {
                 case ADD:
+                    // A new inversion rule added?
                     if (graph.equals(VOCABULARY_GRAPH) && predicate.equals(inverseOf)) {
                         propertiesMap.put(subject, object);
                         propertiesMap.put(object, subject);
                     }
+                    // Check if an inverse statement should be added as well
                     Node toAdd = propertiesMap.get(predicate);
                     if (toAdd != null && !dsg.contains(graph, object, toAdd, subject)) {
                         dsg.add(graph, object, toAdd, subject);
                     }
                     break;
                 case DELETE:
+                    // An inversion rule removed?
                     if (graph.equals(VOCABULARY_GRAPH) && predicate.equals(inverseOf)) {
                         propertiesMap.remove(subject);
                         propertiesMap.remove(object);
                     }
+                    // Check if an inverse statement should be removed as well
                     Node toDelete = propertiesMap.get(predicate);
                     if (toDelete != null && dsg.contains(graph, object, toDelete, subject)) {
                         dsg.delete(graph, object, toDelete, subject);
