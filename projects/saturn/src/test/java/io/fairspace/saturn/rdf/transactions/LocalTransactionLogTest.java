77fcd1dc8d89b1d17eab39b7a68265fbd253037e
strangepleasures <pavel@thehyve.nl>
2019-01-29 11:35:33 +0100

    Structured transaction log

---
+++ projects/saturn/src/test/java/io/fairspace/saturn/rdf/transactions/LocalTransactionLogTest.java
@@ -0,0 +1,48 @@
+package io.fairspace.saturn.rdf.transactions;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static java.util.UUID.randomUUID;
+import static org.apache.commons.io.FileUtils.deleteDirectory;
+import static org.apache.commons.io.FileUtils.getTempDirectory;
+import static org.junit.Assert.*;
+
+public class LocalTransactionLogTest {
+
+    private File logDir;
+    private LocalTransactionLog log;
+
+    @Before
+    public void before() {
+        logDir = new File(getTempDirectory(), randomUUID().toString());
+        log = new LocalTransactionLog(logDir, (t, o) -> {});
+    }
+
+    @After
+    public void after() throws IOException {
+        deleteDirectory(logDir);
+    }
+
+    @Test
+    public void logContinuesNumbering() throws IOException {
+        log.log(new TransactionRecord());
+        var newLog = new LocalTransactionLog(logDir, (t, o) -> {});
+        newLog.log(new TransactionRecord());
+
+        assertTrue(new File(new File(new File(logDir, "volume-1"), "chapter-1"), "tx-1").exists());
+        assertTrue(new File(new File(new File(logDir, "volume-1"), "chapter-1"), "tx-2").exists());
+    }
+
+    @Test
+    public void storageSchemaWorksAsExpected() throws IOException {
+        for (int i = 0; i < 1000001; i++) {
+            log.log(new TransactionRecord());
+        }
+        assertTrue(new File(new File(new File(logDir, "volume-2"), "chapter-1001"), "tx-1000001").exists());
+    }
+}
