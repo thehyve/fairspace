cb99791f8ae76ae6f5a3244ea179891fbf8f7d57
AlQuraian <m4design@gmail.com>
2019-01-30 11:37:12 +0100

    Externalise logic into util, action btns always visible, no hovering

--- projects/mercury/src/utils/permissionUtils.js
+++ projects/mercury/src/utils/permissionUtils.js
@@ -1,13 +1,33 @@
+import {compareBy, comparing} from "./comparisionUtils";
+
 const MANAGE = 'Manage';
 const WRITE = 'Write';
 const READ = 'Read';
 
-export default {
-    canRead: (collection) => collection
-        && (collection.access === MANAGE || collection.access === WRITE || collection.access === READ),
+export const canRead = (collection) => collection
+    && (collection.access === MANAGE || collection.access === WRITE || collection.access === READ);
 
-    canWrite: (collection) => collection
-        && (collection.access === MANAGE || collection.access === WRITE),
+export const canWrite = (collection) => collection
+    && (collection.access === MANAGE || collection.access === WRITE);
 
-    canManage: (collection) => collection && collection.access === MANAGE,
+export const canManage = (collection) => collection && collection.access === MANAGE;
+
+const permissionLevel = p => ({Manage: 0, Write: 1, Read: 2}[p.access]);
+
+export const sortPermissions = (permissions) => {
+    if (!permissions) {
+        return [];
+    }
+
+    return permissions.sort(comparing(compareBy(permissionLevel), compareBy('subject')));
+};
+
+/**
+ * Check if collaborator can alter permission. User can alter permission if:
+ * - has manage access to a collection
+ * - permission is not his/hers
+ */
+export const canAlterPermission = (userCanManage, permission, currentLoggedUser) => {
+    const isSomeoneElsePermission = currentLoggedUser.id !== permission.subject;
+    return userCanManage && isSomeoneElsePermission;
 };
