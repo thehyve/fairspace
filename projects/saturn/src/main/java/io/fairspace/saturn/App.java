afc9cd10e0057f1d7f0bde287254abada2f1c65e
strangepleasures <pavel@thehyve.nl>
2019-02-01 12:34:03 +0100

    Refactoring for readability and testability

--- projects/saturn/src/main/java/io/fairspace/saturn/App.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/App.java
@@ -12,18 +12,11 @@ import org.cfg4j.source.classpath.ClasspathConfigurationSource;
 import org.cfg4j.source.compose.FallbackConfigurationSource;
 import org.cfg4j.source.context.filesprovider.ConfigFilesProvider;
 import org.cfg4j.source.files.FilesConfigurationSource;
-import org.eclipse.jetty.security.ConstraintSecurityHandler;
-import org.eclipse.jetty.server.Request;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.nio.file.Paths;
 
 import static io.fairspace.saturn.auth.Security.createAuthenticator;
 import static java.util.Collections.singletonList;
-import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 
 public class App {
     private static ConfigFilesProvider configFilesProvider = () -> singletonList(Paths.get("./application.yaml"));
@@ -52,21 +45,7 @@ public class App {
 
         if (config.authEnabled()) {
             var authenticator = createAuthenticator(config.jwksUrl(), config.jwksAlgorithm());
-            fusekiServerBuilder.securityHandler(new ConstraintSecurityHandler() {
-                @Override
-                public void handle(String pathInContext, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-                    if (!"/health".equals(pathInContext)) {
-                        var userInfo = authenticator.apply(request);
-                        if (userInfo == null) {
-                            response.sendError(SC_UNAUTHORIZED);
-                            baseRequest.setHandled(true);
-                            return;
-                        }
-                        // TODO: Check roles
-                    }
-                    super.handle(pathInContext, baseRequest, request, response);
-                }
-            });
+            fusekiServerBuilder.securityHandler(new SaturnSecurityHandler(authenticator));
         }
 
         fusekiServerBuilder
@@ -78,4 +57,5 @@ public class App {
         System.out.println("Access Metadata at /statements");
         System.out.println("Access Vocabulary API at /vocabulary");
     }
+
 }
