067dbfe26a0bee6b9a6943939d04d8754f269c54
Sietse Snel <sietse@thehyve.nl>
2019-02-06 18:49:58 +0100

    Adapt helm chart install script for GCP

--- charts/.travis/install.sh
+++ charts/.travis/install.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-source $BUILD_SCRIPTS_DIR/helm/install_helm.sh
+# shellcheck disable=SC1090
+source "$BUILD_SCRIPTS_DIR/helm/install_helm.sh"
 
 if [[ $SHOULD_RELEASE ]]; then
-  $BUILD_SCRIPTS_DIR/az/install.sh
-  $BUILD_SCRIPTS_DIR/az/login.sh
-  $BUILD_SCRIPTS_DIR/k8s/install_kubectl.sh
+  if [ "$DEPLOY_PLATFORM" = "GCP" ]
+  then echo "Installing Google Cloud SDK ..."
+       "$BUILD_SCRIPTS_DIR/gcp/install.sh"
+       echo "Configuring Google Cloud SDK ..."
+       "$BUILD_SCRIPTS_DIR/gcp/login.sh"
+  else echo "Installing Azure CLI ..."
+       "$BUILD_SCRIPTS_DIR/az/install.sh"
+       echo "Configuring Azure CLI ..."
+       "$BUILD_SCRIPTS_DIR/az/login.sh"
+  fi
+
+  "$BUILD_SCRIPTS_DIR/k8s/install_kubectl.sh"
   export PATH="$HOME/downloads/v${KUBECTL_VERSION}:$PATH"
-  $BUILD_SCRIPTS_DIR/k8s/config_kubectl.sh
-  $BUILD_SCRIPTS_DIR/k8s/switch_context.sh
+  "$BUILD_SCRIPTS_DIR/k8s/config_kubectl.sh"
+  "$BUILD_SCRIPTS_DIR/k8s/switch_context.sh"
+
 fi
