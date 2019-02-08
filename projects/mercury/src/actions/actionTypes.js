f61cc5a88c3252c0bb644d35a5e4d67b00a1e40c
AlQuraian <m4design@gmail.com>
2019-02-08 14:26:03 +0100

    Remove unused actions

--- projects/mercury/src/actions/actionTypes.js
+++ projects/mercury/src/actions/actionTypes.js
@@ -15,8 +15,6 @@ export const CLIPBOARD_PASTE_FULFILLED              = 'CLIPBOARD_PASTE_FULFILLED
 export const CLIPBOARD_PASTE_REJECTED               = 'CLIPBOARD_PASTE_REJECTED';
 
 // Collection browser
-export const OPEN_INFODRAWER                        = 'OPEN_INFODRAWER';
-export const CLOSE_INFODRAWER                       = 'CLOSE_INFODRAWER';
 export const OPEN_COLLECTION                        = 'OPEN_COLLECTION';
 export const SELECT_COLLECTION                      = 'SELECT_COLLECTION';
 export const OPEN_PATH                              = 'OPEN_PATH';
