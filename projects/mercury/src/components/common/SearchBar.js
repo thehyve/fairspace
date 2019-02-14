853b090692625e5c5c3336b59da5cc1eb8c6f6b6
AlQuraian <m4design@gmail.com>
2019-02-14 16:15:07 +0100

    Extract styles

--- projects/mercury/src/components/common/SearchBar.js
+++ projects/mercury/src/components/common/SearchBar.js
@@ -2,49 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {withRouter} from "react-router-dom";
 import {InputBase, withStyles} from '@material-ui/core';
-import {fade} from '@material-ui/core/styles/colorManipulator';
 import SearchIcon from '@material-ui/icons/Search';
 
+import styles from './SearchBar.styles';
 import {buildSearchUrl, getSearchTypeFromString, getSearchQueryFromString} from '../../utils/searchUtils';
-
-const styles = theme => ({
-    search: {
-        'position': 'relative',
-        'flex': 0.8,
-        'borderRadius': theme.shape.borderRadius,
-        'backgroundColor': fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        'marginLeft': 0,
-        'width': '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing.unit,
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing.unit * 9,
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-        width: '100%',
-    },
-    inputInput: {
-        paddingTop: theme.spacing.unit,
-        paddingRight: theme.spacing.unit,
-        paddingBottom: theme.spacing.unit,
-        paddingLeft: theme.spacing.unit * 10,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-    },
-});
 
 class SearchBar extends React.Component {
     state = {
