fd8aacce6e56a9dca5e9a18efc0cc1e501d4e4dc
AlQuraian <m4design@gmail.com>
2019-02-22 13:54:43 +0100

    Tests for subject by path reducers

---
+++ projects/mercury/src/reducers/cache/subjectByPathReducers.test.js
@@ -0,0 +1,35 @@
+import reducer from './subjectByPathReducers';
+import * as actionTypes from "../../actions/actionTypes";
+
+describe('Subject by path reducers', () => {
+    it('should set pending state for path on pending action', () => {
+        expect(reducer({}, {
+            type: actionTypes.STAT_FILE_PENDING,
+            meta: {
+                path: '/dir'
+            },
+        })).toEqual({
+            "/dir": {
+                pending: true
+            }
+        });
+    });
+
+    it('should update state with correct uri for path', () => {
+        expect(reducer({}, {
+            type: actionTypes.STAT_FILE_FULFILLED,
+            payload: {
+                props: {
+                    iri: 'https://workspace.ci.test.fairdev.app/iri/500'
+                }
+            },
+            meta: {
+                path: '/dir'
+            },
+        })).toEqual({
+            "/dir": {
+                data: "https://workspace.ci.test.fairdev.app/iri/500"
+            }
+        });
+    });
+});
