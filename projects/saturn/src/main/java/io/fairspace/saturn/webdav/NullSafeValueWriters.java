729b076426f7e207861941fc146810e03a899299
strangepleasures <pavel@thehyve.nl>
2019-02-25 11:14:10 +0100

    VRE-603 Don't include empty tags in PROPFIND responses

---
+++ projects/saturn/src/main/java/io/fairspace/saturn/webdav/NullSafeValueWriters.java
@@ -0,0 +1,20 @@
+package io.fairspace.saturn.webdav;
+
+import io.milton.http.XmlWriter;
+import io.milton.http.values.ValueAndType;
+import io.milton.http.values.ValueWriters;
+
+import javax.xml.namespace.QName;
+import java.util.Map;
+
+/**
+ * Ignores null property values, which is needed to make it work with WebDAV-Client npm library
+ */
+public class NullSafeValueWriters extends ValueWriters {
+    @Override
+    public void writeValue(XmlWriter writer, QName qname, String prefix, ValueAndType vat, String href, Map<String, String> nsPrefixes) {
+        if (vat.getValue() != null) {
+            super.writeValue(writer, qname, prefix, vat, href, nsPrefixes);
+        }
+    }
+}
