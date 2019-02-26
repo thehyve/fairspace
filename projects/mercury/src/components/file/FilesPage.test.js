61954e2fcfa3399e69912b583fc116263f6ba81b
Robert Horlings <robert@isdat.nl>
2019-02-26 15:57:09 +0100

    Add tests for uri-changing logic in mercury

---
+++ projects/mercury/src/components/file/FilesPage.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {shallow} from "enzyme";
+import {FilesPage} from "./FilesPage";
+
+describe('FilesPage', () => {
+    describe('handling of changed collection location', () => {
+        const selectCollection = jest.fn();
+        const fetchCollectionsIfNeeded = jest.fn();
+        const openedPath = '/subdirectory/something-else';
+        const openedCollection = {
+            name: 'My collection',
+            description: 'description',
+            location: 'location1'
+        };
+        const newCollection = {
+            name: 'My collection',
+            description: 'description',
+            location: 'new-location'
+        };
+
+        it('updates url after collection location has changed', () => {
+            const history = [];
+            const wrapper = shallow(<FilesPage
+                openedPath={openedPath}
+                openedCollection={openedCollection}
+                history={history}
+                selectCollection={selectCollection}
+                fetchCollectionsIfNeeded={fetchCollectionsIfNeeded}
+            />);
+
+            wrapper.instance().handleCollectionLocationChange(newCollection);
+            expect(history.length).toEqual(1);
+            expect(history[0]).toEqual('/collections/new-location/subdirectory/something-else');
+        });
+
+        it('can handle an empty openedPath', () => {
+            const history = [];
+            const wrapper = shallow(<FilesPage
+                openedCollection={openedCollection}
+                history={history}
+                selectCollection={selectCollection}
+                fetchCollectionsIfNeeded={fetchCollectionsIfNeeded}
+            />);
+
+            wrapper.instance().handleCollectionLocationChange(newCollection);
+            expect(history.length).toEqual(1);
+            expect(history[0]).toEqual('/collections/new-location');
+        });
+    });
+});
