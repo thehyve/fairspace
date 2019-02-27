9745d7045c19b250255b37ca5f8d92d1c969bd88
Pavel Mikhailovskii <pavel@thehyve.nl>
2019-02-28 00:02:29 +0100

    Use Saturn for local development

--- projects/mercury/src/setupProxy.js
+++ projects/mercury/src/setupProxy.js
@@ -1,7 +1,8 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
-    app.use(proxy('/api', {target: 'http://localhost:5000/'}));
-    app.use(proxy('/webdav', {target: 'http://localhost:5000/'}));
+    app.use(proxy('/api/workspace', {target: 'http://localhost:5000/'}));
+    app.use(proxy('/api', {target: 'http://localhost:8080/'}));
+    app.use(proxy('/webdav', {target: 'http://localhost:8080/'}));
     app.use(proxy('/account', {target: 'http://localhost:5000/'}));
 };
