b07498aa0a4af66903f76168c4d01ce241d90842
Pavel Mikhailovskii <pavel@thehyve.nl>
2019-02-28 22:48:55 +0100

    UserService - WIP

--- projects/saturn/src/main/java/io/fairspace/saturn/App.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/App.java
@@ -8,6 +8,7 @@ import io.fairspace.saturn.services.collections.CollectionsApp;
 import io.fairspace.saturn.services.collections.CollectionsService;
 import io.fairspace.saturn.services.health.HealthApp;
 import io.fairspace.saturn.services.metadata.MetadataApp;
+import io.fairspace.saturn.services.users.UserService;
 import io.fairspace.saturn.vfs.SafeFileSystem;
 import io.fairspace.saturn.vfs.managed.LocalBlobStore;
 import io.fairspace.saturn.vfs.managed.ManagedFileSystem;
@@ -34,12 +35,13 @@ public class App {
         setWorkspaceURI(config.jena.baseIRI);
 
         var ds = SaturnDatasetFactory.connect(config.jena);
-        // The RDF connection is supposed to be threadsafe and can
+        // The RDF connection is supposed to be thread-safe and can
         // be reused in all the application
         var rdf = new RDFConnectionLocal(ds);
 
-        var collections = new CollectionsService(rdf, SecurityUtil::userInfo);
-        var fs = new SafeFileSystem(new ManagedFileSystem(rdf, new LocalBlobStore(new File(config.webDAV.blobStorePath)), SecurityUtil::userInfo, collections));
+        var userService = new UserService(rdf, SecurityUtil::userInfo);
+        var collections = new CollectionsService(rdf, userService::getCurrentUserIRI);
+        var fs = new SafeFileSystem(new ManagedFileSystem(rdf, new LocalBlobStore(new File(config.webDAV.blobStorePath)), userService::getCurrentUserIRI, collections));
 
         var fusekiServerBuilder = FusekiServer.create()
                 .add("rdf", ds)
@@ -47,7 +49,6 @@ public class App {
                         new MetadataApp("/api/metadata", rdf, defaultGraphIRI),
                         new MetadataApp("/api/vocabulary", rdf, createURI(config.jena.baseIRI + "vocabulary")),
                         new CollectionsApp(collections),
-                        //    new VocabularyApp(rdf),
                         new HealthApp()))
                 .addServlet("/webdav/*", new MiltonWebDAVServlet("/webdav/", fs))
                 .port(config.port);
---
+++ projects/saturn/src/test/java/io/fairspace/saturn/services/users/UserServiceTest.java
@@ -0,0 +1,26 @@
+package io.fairspace.saturn.services.users;
+
+import io.fairspace.saturn.auth.UserInfo;
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.junit.Before;
+
+import java.util.function.Supplier;
+
+import static io.fairspace.saturn.rdf.SparqlUtils.setWorkspaceURI;
+import static org.apache.jena.query.DatasetFactory.createTxnMem;
+import static org.apache.jena.rdfconnection.RDFConnectionFactory.connect;
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+    private final Supplier<UserInfo> userInfoSupplier = () -> new UserInfo("userId", null, null, null);
+    private RDFConnection rdf;
+    private UserService service;
+
+    @Before
+    public void before() {
+        setWorkspaceURI("http://example.com/iri/");
+        rdf = connect(createTxnMem());
+        service = new UserService(rdf, userInfoSupplier);
+    }
+
+}
