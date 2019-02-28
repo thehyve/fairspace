99d7361f77f82a0186375674e095b781f860d3fa
strangepleasures <pavel@thehyve.nl>
2019-02-28 11:28:49 +0100

    Test httpUtils

--- projects/mercury/src/utils/httpUtils.js
+++ projects/mercury/src/utils/httpUtils.js
@@ -3,7 +3,7 @@ function failOnHttpError(message) {
         if (response && !response.ok) {
             switch (response.status) {
                 case 401:
-                    window.location.href = `/login?redirectUrl=${encodeURI(window.location.href)}`;
+                    window.location.assign(`/login?redirectUrl=${encodeURI(window.location.href)}`);
                     break;
                 default:
                     throw Error(`${message} ${response.error || ''}`);
