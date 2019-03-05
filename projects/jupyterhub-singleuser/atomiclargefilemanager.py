dcf53c39dafb6a064e93a250ae6e86e928e9e771
strangepleasures <pavel@thehyve.nl>
2019-03-05 15:10:18 +0100

    VRE-613 Fix non-upload operations

--- projects/jupyterhub-singleuser/atomiclargefilemanager.py
+++ projects/jupyterhub-singleuser/atomiclargefilemanager.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import tempfile
 
 from tornado import web
@@ -10,18 +11,29 @@ class AtomicLargeFileManager(LargeFileManager):
     """Handle large file upload atomically."""
 
     _temp_dir = tempfile.mkdtemp()
+    _uploading = False
 
     def _get_os_path(self, path):
-        return os.path.join(self._temp_dir, path)
+        if self._uploading:
+            return os.path.join(self._temp_dir, os.path.basename(path))
+        else:
+            return super()._get_os_path(path)
 
     def run_post_save_hook(self, model, os_path):
-        relative_path = os_path[len(self._temp_dir):]
-        target_path = super()._get_os_path(relative_path)
+        target_path = super()._get_os_path(model['path'])
 
         try:
-            os.rename(os_path, target_path)
+            shutil.move(os_path, target_path)
         except Exception as e:
             self.log.error("Error moving an uploaded file from %s to %s", os_path, target_path, exc_info=True)
             raise web.HTTPError(500, u'Unexpected error while moving an uploaded file to its destination: %s' % e)
 
         return super().run_post_save_hook(model=model, os_path=target_path)
+
+    def new(self, model=None, path=''):
+        try:
+            self._uploading = True
+            return super().new(model, path)
+        finally:
+            self._uploading = False
+
