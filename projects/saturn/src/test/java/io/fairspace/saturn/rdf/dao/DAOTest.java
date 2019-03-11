ba244a77c0a58f1dbaab0a6ea3d6b5eb5115d173
Robert Horlings <robert@isdat.nl>
2019-03-11 14:37:16 +0100

    Update projects/saturn/src/test/java/io/fairspace/saturn/rdf/dao/DAOTest.java
    Co-Authored-By: strangepleasures <pavel@thehyve.nl>

--- projects/saturn/src/test/java/io/fairspace/saturn/rdf/dao/DAOTest.java
+++ projects/saturn/src/test/java/io/fairspace/saturn/rdf/dao/DAOTest.java
@@ -233,7 +233,7 @@ public class DAOTest {
         var entity2 = dao.read(basicEntity.getClass(), basicEntity.getIri());
         assertEquals(t1, entity2.getDateCreated());
         assertTrue(entity2.getDateModified().isAfter(t1));
-        ensureResentInstant(entity2.getDateModified());
+        ensureRecentInstant(entity2.getDateModified());
         assertNotNull(entity2.getModifiedBy());
         assertNotEquals(entity2.getCreatedBy(), entity2.getModifiedBy());
         assertNull(entity2.getDeletedBy());
