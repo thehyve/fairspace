bcf71507d8d922eee6057e539b6effd03badc8a6
Robert Horlings <robert@isdat.nl>
2019-03-13 09:34:01 +0100

    Return only workspace users from pluto endpoint
    The call to keycloak previously returned all keycloak users. We would
    want to retrieve all users with a specific role, but keycloak does not
    provide such an endpoint atm.
    
    However, our keycloak setup is such that all users for a workspace are
    member of a certain group. Keycloak does provide an endpoint to return
    all members of a group, which is now used. To retrieve those members,
    we do need the group id, which is not known on installation. For that reason
    the groupId is retrieved from keycloak on every first call to this endpoint
    and cached forever.
    
    Please note that the mockito upgrade is needed to prevent errors on JDK 11.
    
    Fixes VRE-446

---
+++ projects/pluto/app/src/main/java/nl/fairspace/pluto/app/web/KeycloakUserList.java
@@ -0,0 +1,152 @@
+package nl.fairspace.pluto.app.web;
+
+import io.fairspace.oidc_auth.model.OAuthAuthenticationToken;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+/**
+ * List of users in keycloak, to be returned to the frontend
+ *
+ * This class handles keycloak configuration for retrieving the list of users for the
+ * current workspace. Keycloak does not provide a method to return a list of users for
+ * a given role. For that reason, we retrieve the list of users by group.
+ */
+@Slf4j
+@Profile("!noAuth")
+@Component
+public class KeycloakUserList {
+    @Value("${keycloak.usersUriPattern}")
+    String usersUriPattern;
+
+    @Value("${keycloak.groupUri}")
+    String groupUri;
+
+    @Autowired(required = false)
+    OAuthAuthenticationToken token;
+
+    private final RestTemplate restTemplate;
+    private String groupId = null;
+
+    public KeycloakUserList() {
+        this(new RestTemplate() {{
+            getMessageConverters()
+                    .add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+        }});
+    }
+
+    /**
+     * Constructor which enables injecting a custom restTemplate
+     * @param restTemplate
+     */
+    KeycloakUserList(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    /**
+     * Constructor which enables injecting a custom restTemplate and uri patterns
+     * @param restTemplate
+     */
+    KeycloakUserList(RestTemplate restTemplate, String usersUriPattern, String groupUri) {
+        this.restTemplate = restTemplate;
+        this.usersUriPattern = usersUriPattern;
+        this.groupUri = groupUri;
+    }
+
+    /**
+     * Retrieves information on the users for the current workspace
+     *
+     * The call is being forwarded to the keycloak api for retrieving users
+     *
+     * @return a list with information on all users in the group for the current workspace
+     * @see <https://www.keycloak.org/docs-api/4.0/rest-api/index.html#_getmembers>
+     */
+    @GetMapping(value = "/users", produces = "application/json")
+    public ResponseEntity<String> getUsers(String queryString) {
+        // Forward the request, without any headers except for the Authorization
+        // header. Keycloak will not return a valid response if some headers are
+        // forwarded (e.g. Host or X-Forwarded-Host)
+        HttpHeaders headers = new HttpHeaders();
+
+        if(token != null) {
+            log.trace("Adding authorization header for user retrieval: {}",token.getAccessToken());
+            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token.getAccessToken());
+        }
+
+        HttpEntity<Object> request = new HttpEntity<>(headers);
+
+        // Send the request upstream
+        try {
+            // Pass along the full query string
+            String uri = String.format(usersUriPattern, getGroupId()) + "?" + queryString;
+
+            log.trace("Retrieving users from {}", uri);
+            return restTemplate.exchange(uri, HttpMethod.GET, request, String.class);
+        } catch(HttpClientErrorException e) {
+            log.warn("Client error while retrieving list of users from Auth provider", e);
+            return ResponseEntity.status(e.getStatusCode()).build();
+        } catch(Exception e) {
+            log.error("An exception occurred while retrieving list of users from Auth provider", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    /**
+     * Returns the keycloak group identifier
+     * @return
+     */
+    private String getGroupId() {
+        // Retrieve the groupId, if it has not been retrieved before
+        // As it will never change, we will cache it forever
+        if(groupId == null) {
+            log.info("Retrieve group identifier for keycloak user group at {}", groupUri);
+
+            HttpHeaders headers = new HttpHeaders();
+
+            if(token != null) {
+                log.trace("Adding authorization header for user retrieval: {}",token.getAccessToken());
+                headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token.getAccessToken());
+            }
+
+            HttpEntity<Object> request = new HttpEntity<>(headers);
+
+            ParameterizedTypeReference<List<GroupInfo>> parameterizedTypeReference = new ParameterizedTypeReference<List<GroupInfo>>(){};
+            ResponseEntity<List<GroupInfo>> groupResponse = restTemplate.exchange(groupUri, HttpMethod.GET, request, parameterizedTypeReference);
+
+            if(groupResponse.getStatusCode().is2xxSuccessful() && groupResponse.getBody().size() > 0) {
+                groupId = groupResponse.getBody().get(0).getId();
+            } else {
+                log.warn("Could not retrieve group identifier from keycloak on url {}: status {}", groupUri, groupResponse.getStatusCodeValue());
+                throw new IllegalStateException("Could not retrieve group identifier from keycloak");
+            }
+        }
+
+        return groupId;
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    static class GroupInfo {
+        private String id = null;
+        private String name = null;
+    }
+}
---
+++ projects/pluto/app/src/test/java/nl/fairspace/pluto/app/web/KeycloakUserListTest.java
@@ -0,0 +1,107 @@
+package nl.fairspace.pluto.app.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class KeycloakUserListTest {
+    @Mock
+    RestTemplate restTemplate;
+
+    String groupUri = "http://get-groups";
+    String userUriPattern = "http://get-users/%s";
+
+    KeycloakUserList keycloakUserList;
+
+    @Before
+    public void setUp() throws Exception {
+        keycloakUserList = new KeycloakUserList(restTemplate, userUriPattern, groupUri);
+    }
+
+    @Test
+    public void testUserResponse() {
+        String groupId = "group-id";
+        String userUri = String.format(userUriPattern, groupId) + "?";
+        mockGroupResponse(groupId, "name");
+        mockUserResponse(userUri, "users");
+
+        ResponseEntity<String> userListResponse = keycloakUserList.getUsers("");
+
+        // Expect the response from the server to be passed through
+        assertEquals(200, userListResponse.getStatusCodeValue());
+        assertEquals("users", userListResponse.getBody());
+    }
+
+
+    @Test
+    public void testWhetherGroupIdIsRetrievedAndCached() {
+        String groupId = "group-id";
+        String userUri = String.format(userUriPattern, groupId) + "?";
+        mockGroupResponse(groupId, "name");
+        mockUserResponse(userUri, "users");
+
+        keycloakUserList.getUsers("");
+
+        // Expect a single call for the group id
+        verify(restTemplate, times(1)).exchange(eq(groupUri), eq(HttpMethod.GET), any(), any(ParameterizedTypeReference.class));
+
+        // Retrieve the users again
+        keycloakUserList.getUsers("");
+
+        // Expect a single call for the group id, but two for the users
+        verify(restTemplate, times(1)).exchange(eq(groupUri), eq(HttpMethod.GET), any(), any(ParameterizedTypeReference.class));
+        verify(restTemplate, times(2)).exchange(eq(userUri), eq(HttpMethod.GET), any(), any(Class.class));
+    }
+
+    @Test
+    public void testWhetherGroupIdErrorIsReturnedProperly() {
+        doThrow(new RuntimeException("Test-error"))
+                .when(restTemplate).exchange(eq(groupUri), eq(HttpMethod.GET), any(), any(ParameterizedTypeReference.class));
+
+        ResponseEntity<String> userListResponse = keycloakUserList.getUsers("");
+
+        assertEquals(500, userListResponse.getStatusCodeValue());
+    }
+
+    @Test
+    public void testWhetherQueryStringIsForwarded() {
+        String groupId = "group-id";
+        String userUri = String.format(userUriPattern, groupId) + "?first=1&max=4";
+        mockGroupResponse(groupId, "name");
+        mockUserResponse(userUri, "users");
+
+        keycloakUserList.getUsers("first=1&max=4");
+        verify(restTemplate, times(1)).exchange(eq(userUri), eq(HttpMethod.GET), any(), any(Class.class));
+    }
+
+    private void mockUserResponse(String userUri, String users) {
+        ResponseEntity<String> groupResponse = ResponseEntity.ok(users);
+        doReturn(groupResponse).when(restTemplate).exchange(eq(userUri), eq(HttpMethod.GET), any(), any(Class.class));
+    }
+
+    private void mockGroupResponse(String id, String name) {
+        ResponseEntity<List<KeycloakUserList.GroupInfo>> groupResponse = ResponseEntity.ok(
+                Collections.singletonList(new KeycloakUserList.GroupInfo(id, name))
+        );
+        doReturn(groupResponse).when(restTemplate).exchange(eq(groupUri), eq(HttpMethod.GET), any(), any(ParameterizedTypeReference.class));
+
+    }
+}
