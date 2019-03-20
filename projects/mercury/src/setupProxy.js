61f6547449f33160aa1a0f4cabc824772b627331
AlQuraian <m4design@gmail.com>
2019-03-20 14:55:56 +0100

    Two scripts for dev, real backend and mocked

--- projects/mercury/src/setupProxy.js
+++ projects/mercury/src/setupProxy.js
@@ -1,19 +1,30 @@
 const proxy = require('http-proxy-middleware');
+const path = require('path');
+
+const mockDataDir = path.join(__dirname, '../mock-server/mock-data');
 
 module.exports = (app) => {
+    // Might not be the best way, should improve
+    if (process.title.includes('dev:real')) {
+        app.get('/account/user', (req, res) => res.sendFile(`${mockDataDir}/user.json`));
+        app.get('/account/authorizations', (req, res) => res.send(['user-workspace1', 'ROLE_USER']));
+
+        app.get('/api/workspace/users', (req, res) => res.sendFile(`${mockDataDir}/workspace/users.json`));
+        app.get('/api/workspace/config', (req, res) => res.sendFile(`${mockDataDir}/workspace/workspace-config.json`));
+        app.get('/api/workspace/details', (req, res) => res.sendFile(`${mockDataDir}/workspace/workspace-details.json`));
+
+
+        app.use(proxy('/api/search', {
+            target: 'http://localhost:9200/',
+            pathRewrite: {
+                '^/api/search': '' // remove base path
+            }
+        }));
+    } else {
+        app.use(proxy('/api/search', {target: 'http://localhost:5000/'}));
+    }
+
     app.use(proxy('/api/workspace', {target: 'http://localhost:5000/'}));
-    app.use(proxy('/api/search', {target: 'http://localhost:5000/'}));
-
-    // Use this configuration instead of the previous line
-    // to talk to a real ES instance on localhost
-    //
-    // app.use(proxy('/api/search', {
-    //     target: 'http://localhost:9200/',
-    //     pathRewrite: {
-    //         '^/api/search': '' // remove base path
-    //     }
-    // }));
-
     app.use(proxy('/api', {target: 'http://localhost:8080/'}));
     app.use(proxy('/webdav', {target: 'http://localhost:8080/'}));
     app.use(proxy('/account', {target: 'http://localhost:5000/'}));
