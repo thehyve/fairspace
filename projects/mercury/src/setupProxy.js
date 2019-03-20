7e211d3ed6b9a08386df94908c4d4bf526b8260d
AlQuraian <m4design@gmail.com>
2019-03-20 16:14:54 +0100

    Removed mock data and code

--- projects/mercury/src/setupProxy.js
+++ projects/mercury/src/setupProxy.js
@@ -2,7 +2,6 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
     app.use(proxy('/api/workspace', {target: 'http://localhost:5000/'}));
-    // app.use(proxy('/api/search', {target: 'http://localhost:5000/'}));
 
     // Use this configuration instead of the previous line
     // to talk to a real ES instance on localhost
