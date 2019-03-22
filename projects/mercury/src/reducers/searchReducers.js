9d61453135d4477bc36fb0796bd756e113cf11fe
Robert Horlings <robert@isdat.nl>
2019-03-22 15:31:53 +0100

    Specify user-friendly error message for invalid queries

--- projects/mercury/src/reducers/searchReducers.js
+++ projects/mercury/src/reducers/searchReducers.js
@@ -6,6 +6,13 @@ const initialState = {
     error: null
 };
 
+const determineErrorMessage = (payload) => {
+    switch (payload.status) {
+        case 400: return "Oops, we're unable to parse this query. Please only use alphanumeric characters.";
+        default: return "Error retrieving search results";
+    }
+}
+
 const searchReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.PERFORM_SEARCH_PENDING:
@@ -25,7 +32,7 @@ const searchReducer = (state = initialState, action) => {
             return {
                 ...state,
                 pending: false,
-                error: "Error retrieving search results"
+                error: determineErrorMessage(action.payload)
             };
         default:
             return state;
