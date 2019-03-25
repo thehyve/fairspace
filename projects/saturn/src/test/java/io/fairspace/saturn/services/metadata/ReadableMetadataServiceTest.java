243df099df87766815cb1a482065470310b153b6
Robert Horlings <robert@isdat.nl>
2019-03-25 16:45:21 +0100

    Add new SHACL vocabularies to saturn
    The format of the vocabularies has changed according to the SHACL
    standards. See VRE-617 and VRE-624. The updated vocabularies have
    been added to saturn
    
    The vocabulary has been split into a fixed part (system-vocabulary)
    and a part that the user can change (user-vocabulary). Both are exposed
    in separate endpoints, where only the user-vocabulary can be updated.

--- projects/saturn/src/test/java/io/fairspace/saturn/services/metadata/ReadableMetadataServiceTest.java
+++ projects/saturn/src/test/java/io/fairspace/saturn/services/metadata/ReadableMetadataServiceTest.java
@@ -12,47 +12,42 @@ import org.apache.jena.vocabulary.RDFS;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.apache.jena.graph.NodeFactory.createURI;
 import static org.apache.jena.query.DatasetFactory.createTxnMem;
-import static org.apache.jena.rdf.model.ModelFactory.createDefaultModel;
-import static org.apache.jena.rdf.model.ResourceFactory.*;
+import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
+import static org.apache.jena.rdf.model.ResourceFactory.createResource;
+import static org.apache.jena.rdf.model.ResourceFactory.createStatement;
+import static org.apache.jena.rdf.model.ResourceFactory.createStringLiteral;
+import static org.apache.jena.rdf.model.ResourceFactory.createTypedLiteral;
 import static org.apache.jena.system.Txn.executeWrite;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
-public class MetadataServiceTest {
-    private static final String vocabularyURI = "http://fairspace.io/iri/vocabulary";
+public class ReadableMetadataServiceTest {
+    private static final String userVocabularyURI = "http://fairspace.io/iri/user-vocabulary";
     private static final String GRAPH = "http://fairspace.io/iri/graph";
 
     private static final Resource S1 = createResource("http://fairspace.io/iri/S1");
     private static final Resource S2 = createResource("http://fairspace.io/iri/S2");
     private static final Resource S3 = createResource("http://fairspace.io/iri/S3");
     private static final Property P1 = createProperty("http://fairspace.io/ontology/P1");
-    private static final Property P2 = createProperty("http://fairspace.io/ontology/P2");
-
-    private static final Property MACHINE_ONLY_PROPERTY = createProperty("http://fairspace.io/ontology#filePath");
 
     private static final Statement STMT1 = createStatement(S1, P1, S2);
     private static final Statement STMT2 = createStatement(S2, P1, S3);
-    private static final Statement MACHINE_ONLY_STATEMENT = createStatement(S1, MACHINE_ONLY_PROPERTY, S3);
 
     private static final Statement LBL_STMT1 = createStatement(S1, RDFS.label, createStringLiteral("subject1"));
     private static final Statement LBL_STMT2 = createStatement(S2, RDFS.label, createStringLiteral("subject2"));
 
     private Dataset ds;
-    private MetadataService api;
-
-    @Mock
-    private MetadataEntityLifeCycleManager lifeCycleManager;
+    private ReadableMetadataService api;
 
     @Before
     public void setUp() {
         ds = createTxnMem();
-        api = new MetadataService(new RDFConnectionLocal(ds), createURI(GRAPH), lifeCycleManager,null);
+        api = new ReadableMetadataService(new RDFConnectionLocal(ds), createURI(GRAPH));
     }
 
     @Test
@@ -118,107 +113,10 @@ public class MetadataServiceTest {
     }
 
     @Test
-    public void testPutWillAddStatements() {
-        Model delta = createDefaultModel().add(STMT1).add(STMT2);
-
-        api.put(delta);
-
-        Model result = api.get(null, null, null, false);
-        assertTrue(result.contains(STMT1) && result.contains(STMT2));
-    }
-
-    @Test
-    public void testPutHandlesLifecycleForEntitities() {
-        Model delta = createDefaultModel().add(STMT1).add(STMT2);
-        api.put(delta);
-        verify(lifeCycleManager).updateLifecycleMetadata(delta);
-    }
-
-
-    @Test
-    public void testPutWillNotRemoveExistingStatements() {
-        // Prepopulate the model
-        final Statement EXISTING1 = createStatement(S1, P1, S3);
-        final Statement EXISTING2 = createStatement(S2, P2, createPlainLiteral("test"));
-        executeWrite(ds, () -> ds.getNamedModel(GRAPH).add(EXISTING1).add(EXISTING2));
-
-        // Put new statements
-        Model delta = createDefaultModel().add(STMT1).add(STMT2);
-        api.put(delta);
-
-        // Now ensure that the existing triples are still there
-        // and the new ones are added
-        Model model = ds.getNamedModel(GRAPH);
-        assertTrue(model.contains(EXISTING1));
-        assertTrue(model.contains(EXISTING2));
-        assertTrue(model.contains(STMT1));
-        assertTrue(model.contains(STMT2));
-    }
-
-    @Test
-    public void delete() {
-        executeWrite(ds, () -> ds.getNamedModel(GRAPH).add(STMT1).add(STMT2));
-
-        api.delete(S1.getURI(), null, null);
-
-        assertFalse(ds.getNamedModel(GRAPH).contains(STMT1));
-        assertTrue(ds.getNamedModel(GRAPH).contains(STMT2));
-
-        api.delete(null, P1.getURI(), null);
-
-        assertTrue(ds.getNamedModel(GRAPH).isEmpty());
-    }
-
-    @Test
-    public void deleteShouldNotRemoveMachineOnlyTriples() {
-        setMachineOnlyPredicate(MACHINE_ONLY_PROPERTY.getURI());
-
-        executeWrite(ds, () -> ds.getNamedModel(GRAPH).add(STMT1).add(MACHINE_ONLY_STATEMENT));
-
-        api.delete(S1.getURI(), null, null);
-
-        assertFalse(ds.getNamedModel(GRAPH).contains(STMT1));
-        assertTrue(ds.getNamedModel(GRAPH).contains(MACHINE_ONLY_STATEMENT));
-    }
-
-    @Test
-    public void deleteModel() {
-        executeWrite(ds, () -> ds.getNamedModel(GRAPH).add(STMT1).add(STMT2));
-
-        api.delete(createDefaultModel().add(STMT1));
-
-        assertFalse(ds.getNamedModel(GRAPH).contains(STMT1));
-        assertTrue(ds.getNamedModel(GRAPH).contains(STMT2));
-    }
-
-    @Test
-    public void patch() {
-        executeWrite(ds, () -> ds.getNamedModel(GRAPH).add(STMT1).add(STMT2));
-
-        Statement newStmt1 = createStatement(S1, P1, S3);
-        Statement newStmt2 = createStatement(S2, P1, S1);
-        Statement newStmt3 = createStatement(S1, P2, S3);
-
-        api.patch(createDefaultModel().add(newStmt1).add(newStmt2).add(newStmt3));
-        assertTrue(ds.getNamedModel(GRAPH).contains(newStmt1));
-        assertTrue(ds.getNamedModel(GRAPH).contains(newStmt2));
-        assertTrue(ds.getNamedModel(GRAPH).contains(newStmt3));
-        assertFalse(ds.getNamedModel(GRAPH).contains(STMT1));
-        assertFalse(ds.getNamedModel(GRAPH).contains(STMT2));
-    }
-
-    @Test
-    public void testPatchHandlesLifecycleForEntitities() {
-        Model delta = createDefaultModel().add(STMT1).add(STMT2);
-        api.patch(delta);
-        verify(lifeCycleManager).updateLifecycleMetadata(delta);
-    }
-
-    @Test
     public void getByType() {
         Resource personConsent = createResource("http://fairspace.io/ontology#PersonConsent");
         Resource researchProject = createResource("http://fairspace.io/ontology#ResearchProject");
-        Property fairspaceEntity = createProperty("http://fairspace.io/ontology#fairspaceEntity");
+        Property showInCatalog = createProperty("http://fairspace.io/ontology#showInCatalog");
 
         // Setup the model
         executeWrite(ds, () -> {
@@ -230,10 +128,10 @@ public class MetadataServiceTest {
                     .add(createResource("http://example.com/person"), RDF.type, FOAF.Person);
 
             // Mark personConsent and researchProject as fairspace entities
-            ds.getNamedModel(vocabularyURI)
-                    .add(personConsent, fairspaceEntity, createTypedLiteral(true))
-                    .add(researchProject, fairspaceEntity, createTypedLiteral(true))
-                    .add(FOAF.Person, fairspaceEntity, createTypedLiteral(false));
+            ds.getNamedModel(userVocabularyURI)
+                    .add(personConsent, showInCatalog, createTypedLiteral(true))
+                    .add(researchProject, showInCatalog, createTypedLiteral(true))
+                    .add(FOAF.Person, showInCatalog, createTypedLiteral(false));
         });
 
         // Test whether entities of a single type can be retrieved, including the label
@@ -258,15 +156,4 @@ public class MetadataServiceTest {
         assertTrue(m4.isEmpty());
     }
 
-    /**
-     * Store the machine-only predicate in the database
-     */
-    private void setMachineOnlyPredicate(String predicateUri) {
-        Resource predicateResource = createResource(predicateUri);
-
-        // Actually update the database itself, as the delete method depends on it
-        executeWrite(ds, () -> ds.getNamedModel(vocabularyURI)
-                .add(predicateResource, RDF.type, RDF.Property)
-                .add(predicateResource, createProperty("http://fairspace.io/ontology#machineOnly"), createTypedLiteral(true)));
-    }
 }
