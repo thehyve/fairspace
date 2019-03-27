3fa79af1a310f9520c97b04a7a65405ccc5f265b
strangepleasures <pavel@thehyve.nl>
2019-03-27 11:10:27 +0100

    Fix DummyAuthenticator

--- projects/saturn/src/main/java/io/fairspace/saturn/auth/DummyAuthenticator.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/auth/DummyAuthenticator.java
@@ -1,7 +1,6 @@
 package io.fairspace.saturn.auth;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 
@@ -9,20 +8,17 @@ import static java.util.Collections.emptySet;
 
 // For local development only
 public class DummyAuthenticator implements Function<HttpServletRequest, UserInfo> {
-    private static final UserInfo DUMMY_USER =
-            new UserInfo("123", "test-dummy", "John", "user@example.com", emptySet());
-
     @Override
     public UserInfo apply(HttpServletRequest request) {
         // Allow the client to provide some authorities
-            String authoritiesHeader = request.getHeader("x-fairspace-authorities");
+        String authoritiesHeader = request.getHeader("x-fairspace-authorities");
         Set<String> authorities = authoritiesHeader == null
                 ? emptySet()
                 : Set.of(authoritiesHeader.split(","));
 
-        UserInfo dummyUser = new UserInfo("123", "test-dummy", "John", "user@example.com", authorities);
+        UserInfo dummyUser = new UserInfo("6e6cde34-45bc-42d8-8cdb-b6e9faf890d3", "test-dummy", "John", "user@example.com", authorities);
 
         request.setAttribute(SecurityUtil.USER_INFO_REQUEST_ATTRIBUTE, dummyUser);
-        return DUMMY_USER;
+        return dummyUser;
     }
 }
