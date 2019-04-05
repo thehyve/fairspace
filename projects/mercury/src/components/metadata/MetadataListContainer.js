49e194d9309d4f00d93fcce003d21c91d6ee199e
Robert Horlings <robert@isdat.nl>
2019-04-05 12:29:21 +0200

    Extract button for new metadata entities to ListContainer

--- projects/mercury/src/components/metadata/MetadataListContainer.js
+++ projects/mercury/src/components/metadata/MetadataListContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 
+import {Button} from "@material-ui/core";
 import {getLabel, relativeLink} from "../../utils/metadataUtils";
 import * as metadataActions from "../../actions/metadataActions";
 import NewMetadataEntityDialog from "./NewMetadataEntityDialog";
@@ -9,9 +10,25 @@ import {ErrorDialog, ErrorMessage, LoadingInlay, LoadingOverlay} from "../common
 import MetaList from './MetaList';
 
 class MetadataListContainer extends React.Component {
+    state = {
+        creating: false
+    };
+
     componentDidMount() {
         this.props.fetchAllEntitiesIfNeeded();
+        this.props.fetchMetadataVocabularyIfNeeded();
     }
+
+    openDialog = (e) => {
+        e.stopPropagation();
+
+        this.setState({creating: true});
+    };
+
+    closeDialog = (e) => {
+        if (e) e.stopPropagation();
+        this.setState({creating: false});
+    };
 
     handleEntityCreation = (shape, id) => {
         this.props.createMetadataEntity(shape, id)
@@ -20,7 +37,7 @@ class MetadataListContainer extends React.Component {
                 this.props.history.push(relativeLink(res.value));
             })
             .catch(e => ErrorDialog.showError(e, `Error creating a new metadata entity.\n${e.message}`));
-    }
+    };
 
     render() {
         const {loading, creatingMetadataEntity, error, entities} = this.props;
@@ -35,7 +52,23 @@ class MetadataListContainer extends React.Component {
 
         return (
             <>
-                <NewMetadataEntityDialog onCreate={this.handleEntityCreation} />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    aria-label="Add"
+                    title="Create a new Metadata"
+                    onClick={this.openDialog}
+                    style={{margin: '10px 0'}}
+                    disabled={!this.props.shapes}
+                >
+                    Create
+                </Button>
+
+                <NewMetadataEntityDialog
+                    open={this.state.creating}
+                    onCreate={this.handleEntityCreation}
+                    onClose={this.closeDialog}
+                />
                 {entities && entities.length > 0 ? <MetaList items={entities} /> : null}
                 <LoadingOverlay loading={creatingMetadataEntity} />
             </>
@@ -44,6 +77,7 @@ class MetadataListContainer extends React.Component {
 }
 
 const mapStateToProps = ({metadataBySubject, cache: {allEntities, vocabulary}}) => {
+    const pending = !allEntities || allEntities.pending || !vocabulary || vocabulary.pending;
     const allEntitiesData = allEntities && allEntities.data ? allEntities.data : [];
     const vocabularyData = vocabulary ? vocabulary.data : undefined;
     const entities = allEntitiesData.map(e => ({
@@ -54,8 +88,9 @@ const mapStateToProps = ({metadataBySubject, cache: {allEntities, vocabulary}}) 
     }));
 
     return ({
-        loading: allEntities ? allEntities.pending : true,
+        loading: pending,
         error: allEntities ? allEntities.error : false,
+        shapes: vocabularyData && vocabularyData.getFairspaceClasses(),
         entities,
         creatingMetadataEntity: metadataBySubject.creatingMetadataEntity
     });
