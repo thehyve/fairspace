e68f64bd1e6c0ab0f3cd49db683e7e6c759b01df
strangepleasures <pavel@thehyve.nl>
2019-04-15 14:20:54 +0200

    Add comments

--- projects/saturn/src/main/java/io/fairspace/saturn/services/metadata/validation/MetadataAndVocabularyConsistencyValidator.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/services/metadata/validation/MetadataAndVocabularyConsistencyValidator.java
@@ -26,31 +26,35 @@ public class MetadataAndVocabularyConsistencyValidator implements MetadataReques
 
     @Override
     public ValidationResult validate(Model modelToRemove, Model modelToAdd) {
+        // We first determine which shapes were modified and how the updated vocabulary will look like.
         var oldVocabulary = rdf.fetch(VOCABULARY_GRAPH_URI.getURI());
         var newVocabulary = oldVocabulary.difference(modelToRemove).union(modelToAdd);
         var actuallyAdded = newVocabulary.difference(oldVocabulary);
         var actuallyRemoved = oldVocabulary.difference(newVocabulary);
         var modifiedShapes = union(actuallyRemoved.listSubjects().toSet(), actuallyAdded.listSubjects().toSet());
 
+        // And then check, if the model is still valid with the updated vocabulary
         return validateModifiedShapes(modifiedShapes, newVocabulary);
     }
 
     private ValidationResult validateModifiedShapes(Set<Resource> shapes, Model vocabulary) {
         return mergeValidationResults(onResult ->
                 shapes.forEach(shape -> {
-                    validateTargetClass(shape, vocabulary, onResult);
-                    validateProperty(shape, vocabulary, onResult);
+                    validateByTargetClass(shape, vocabulary, onResult);
+                    validateByPropertyPath(shape, vocabulary, onResult);
                 }));
     }
 
-    private void validateTargetClass(Resource shape, Model vocabulary, Consumer<ValidationResult> onResult) {
+    private void validateByTargetClass(Resource shape, Model vocabulary, Consumer<ValidationResult> onResult) {
+        // determine the target class (if any) and validate all the entities belonging to it
         vocabulary.listObjectsOfProperty(shape, SH.targetClass)
                 .forEachRemaining(targetClass -> rdf.querySelect(storedQuery("subjects_by_type", targetClass),
                         row -> validateResource(row.getResource("s"), vocabulary, onResult)));
     }
 
 
-    private void validateProperty(Resource shape, Model vocabulary, Consumer<ValidationResult> onResult) {
+    private void validateByPropertyPath(Resource shape, Model vocabulary, Consumer<ValidationResult> onResult) {
+        // determine the underlying property path (if any) and validate all the entities having it
         vocabulary.listObjectsOfProperty(shape, SH.path)
                 .forEachRemaining(path -> rdf.querySelect(storedQuery("subjects_with_property", path),
                         row -> validateResource(row.getResource("s"), vocabulary, onResult)));
