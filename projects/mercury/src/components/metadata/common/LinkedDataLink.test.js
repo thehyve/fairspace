f5ec73322645067f3c16f179612081812067fcbc
Robert Horlings <robert@isdat.nl>
2019-04-16 09:02:06 +0200

    Add unit test for metadata link
    The added test will verify whether the actual behaviour on localhost
    (server running on a port number, IRI without port number) works as
    expected to prevent future bugs.

--- projects/mercury/src/components/metadata/common/LinkedDataLink.test.js
+++ projects/mercury/src/components/metadata/common/LinkedDataLink.test.js
@@ -3,6 +3,11 @@ import {shallow} from "enzyme";
 import {Link} from "react-router-dom";
 import LinkedDataLink from "./LinkedDataLink";
 
+const setLocation = (location) => {
+    delete global.window.location;
+    global.window.location = location;
+};
+
 describe('MetadataLink', () => {
     // Please note that for tests, the window.location.origin is set to http://localhost
     const pathname = '/uri/test/abc';
@@ -19,6 +24,16 @@ describe('MetadataLink', () => {
     it('should render internal link for uri with same hostname (URI includes port)', () => {
         const wrapper = shallow(<LinkedDataLink uri={`${origin}:8000${pathname}${search}${hash}`} />);
         expect(wrapper.containsMatchingElement(<Link to={{pathname, search, hash}} />)).toBe(true);
+    });
+
+    it('should render internal link for uri with same hostname if server runs on different port', () => {
+        const oldLocation = global.window.location;
+        setLocation(new URL("http://localhost:8080/"));
+
+        const wrapper = shallow(<LinkedDataLink uri={`${origin}${pathname}${search}${hash}`} />);
+        expect(wrapper.containsMatchingElement(<Link to={{pathname, search, hash}} />)).toBe(true);
+
+        setLocation(oldLocation);
     });
 
     it('should render external link for uri with other hostname', () => {
