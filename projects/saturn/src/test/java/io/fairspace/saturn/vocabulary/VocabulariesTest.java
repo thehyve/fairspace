750dd3c43ea9a8ad76eb581a784e072c3c2a9d3c
Robert Horlings <robert@isdat.nl>
2019-04-17 12:12:49 +0200

    Solve validation issues in updated user vocabulary

--- projects/saturn/src/test/java/io/fairspace/saturn/vocabulary/VocabulariesTest.java
+++ projects/saturn/src/test/java/io/fairspace/saturn/vocabulary/VocabulariesTest.java
@@ -3,10 +3,12 @@ package io.fairspace.saturn.vocabulary;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdfconnection.RDFConnectionLocal;
 import org.apache.jena.util.FileManager;
 import org.junit.Test;
 import org.topbraid.shacl.vocabulary.SH;
+import org.topbraid.spin.util.JenaUtil;
 
 import java.util.List;
 
@@ -59,6 +61,17 @@ public class VocabulariesTest {
         var engine = createEngine(dataModel, shapesModel);
         var report = engine.validateAll();
         var violations = createConstraintViolations(report.getModel());
+
+        // Show validation errors on failure
+        for(Resource resource : JenaUtil.getAllInstances(SH.ValidationResult.inModel(report.getModel()))) {
+            Resource focusNode = JenaUtil.getResourceProperty(resource, SH.focusNode);
+            Resource value = JenaUtil.getResourceProperty(resource, SH.value);
+            Resource path = JenaUtil.getResourceProperty(resource, SH.resultPath);
+            String message = JenaUtil.getStringProperty(resource, SH.resultMessage);
+
+            System.out.println(String.format("%s %s %s - %s", focusNode.getURI(), path.getURI(), value != null ? value.getURI() : '-',message));
+        }
+
         assertTrue(violations.isEmpty());
     }
 }
