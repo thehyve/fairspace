9a8d474837d84232af157678f3f23a550915fc10
AlQuraian <m4design@gmail.com>
2019-04-24 12:14:22 +0200

    Add comment

--- projects/mercury/src/components/metadata/common/values/SwitchValue.js
+++ projects/mercury/src/components/metadata/common/values/SwitchValue.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Switch from "@material-ui/core/Switch";
 
+// The error variable here is a boolean for error existiance, the Switch component expects a string instead
 const SwitchValue = ({entry, property, style, onChange, error, ...otherProps}) => (
     <Switch
         {...otherProps}
