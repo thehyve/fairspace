e98d8c32c845b0783ee02350c1db51f2b24e94e7
AlQuraian <m4design@gmail.com>
2019-04-24 12:57:42 +0200

    Word break on table to avoid extended table view

--- projects/mercury/src/components/metadata/common/LinkedDataList.js
+++ projects/mercury/src/components/metadata/common/LinkedDataList.js
@@ -1,11 +1,20 @@
 import React from "react";
-import {Paper, Table, TableBody, TableCell, TableHead, TableRow} from "@material-ui/core";
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    withStyles
+} from "@material-ui/core";
 
 import LinkedDataLink from "./LinkedDataLink";
+import styles from './LinkedDataList.styles';
 
-const linkedDataList = ({items = []}) => (
-    <Paper>
-        <Table>
+const linkedDataList = ({items = [], classes}) => (
+    <Paper className={classes.root}>
+        <Table className={classes.table}>
             <TableHead>
                 <TableRow>
                     <TableCell>Label</TableCell>
@@ -17,13 +26,13 @@ const linkedDataList = ({items = []}) => (
                 {
                     items.map(({id, label, type, typeLabel}) => (
                         <TableRow key={id}>
-                            <TableCell>
+                            <TableCell className={classes.cell}>
                                 {label}
                             </TableCell>
-                            <TableCell>
+                            <TableCell className={classes.cell}>
                                 <a href={type}> {typeLabel} </a>
                             </TableCell>
-                            <TableCell>
+                            <TableCell className={classes.cell}>
                                 <LinkedDataLink uri={id}>
                                     {label}
                                 </LinkedDataLink>
@@ -36,4 +45,4 @@ const linkedDataList = ({items = []}) => (
     </Paper>
 );
 
-export default linkedDataList;
+export default withStyles(styles)(linkedDataList);
