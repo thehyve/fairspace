bf03c8a15f71f3607e9e28752403fe952ec008f5
Robert Horlings <robert@isdat.nl>
2019-05-07 09:20:47 +0200

    Skip building simple JupyterHub image
    Building it causes problems because of updated dependencies. Also
    the simple image is currently not being used and may very well
    be removed in the near future.
    
    See https://travis-ci.com/fairspace/workspace/jobs/197908184
    and VRE-706

--- projects/jupyterhub-singleuser/.travis/release.sh
+++ projects/jupyterhub-singleuser/.travis/release.sh
@@ -2,5 +2,5 @@
 
 echo "${DOCKER_PASSWORD}" | docker login ${DOCKER_REPO} -u "${DOCKER_USERNAME}" --password-stdin
 docker push "${CONTAINER_NAME}"
-docker push "${CONTAINER_NAME}-simple"
+# docker push "${CONTAINER_NAME}-simple"
 docker push "${CONTAINER_NAME}-full"
