95323ace3123b2a2248a72cbccec3164b5550eb8
jochemb <j.bijlard@gmail.com>
2019-05-14 10:55:32 +0200

    store ipynb checkpoints in home dir instead of collection folder

--- projects/jupyterhub-singleuser/config.py
+++ projects/jupyterhub-singleuser/config.py
@@ -1,3 +1,4 @@
 from notebook.services.contents.atomiclargefilemanager import AtomicLargeFileManager
 
 c.NotebookApp.contents_manager_class = AtomicLargeFileManager
+c.FileCheckpoints.checkpoint_dir = '/home/jovyan/.notebookCheckpoints'
