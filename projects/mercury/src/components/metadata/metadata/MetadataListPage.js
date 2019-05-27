2fcf2fd6227fdff11a5bbf473dde9437b02066ba
Robert Horlings <robert@isdat.nl>
2019-05-27 17:01:44 +0200

    Add dropdown to filter on type in metadata catalog

--- projects/mercury/src/components/metadata/metadata/MetadataListPage.js
+++ projects/mercury/src/components/metadata/metadata/MetadataListPage.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import Paper from "@material-ui/core/Paper";
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
+import Paper from "@material-ui/core/Paper";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Checkbox from "@material-ui/core/Checkbox";
+import ListItemText from "@material-ui/core/ListItemText";
+import Input from "@material-ui/core/Input";
+import {withStyles} from "@material-ui/core";
 
 import MetadataBrowserContainer from "./MetadataBrowserContainer";
 import SearchBar from "../../common/SearchBar";
@@ -10,9 +17,37 @@ import {getVocabulary} from "../../../reducers/cache/vocabularyReducers";
 import {getFirstPredicateId} from "../../../utils/linkeddata/jsonLdUtils";
 import * as constants from "../../../constants";
 import {fetchMetadataVocabularyIfNeeded} from "../../../actions/vocabularyActions";
+import {getLabel} from "../../../utils/linkeddata/metadataUtils";
 
-const MetadataListPage = ({fetchVocabulary, vocabulary, targetClasses, searchMetadata: search}) => {
+const styles = theme => ({
+    typeSelect: {
+        paddingLeft: theme.spacing.unit * 10
+    }
+});
+
+const MetadataListPage = ({classes, fetchVocabulary, vocabulary, classesInCatalog, searchMetadata: search}) => {
+    const [selectedTypes, setSelectedTypes] = useState([]);
+    const [query, setQuery] = useState('');
+
     fetchVocabulary();
+
+    const renderTypeClass = typeClass => {
+        const targetClass = getFirstPredicateId(typeClass, constants.SHACL_TARGET_CLASS);
+        const label = getLabel(typeClass);
+
+        return (
+            <MenuItem key={targetClass} value={typeClass}>
+                <Checkbox checked={selectedTypes.indexOf(typeClass) > -1} />
+                <ListItemText primary={label} secondary={targetClass} />
+            </MenuItem>
+        );
+    };
+
+    const toTargetClasses = shapes => shapes.map(c => getFirstPredicateId(c, constants.SHACL_TARGET_CLASS));
+
+    const performSearch = (queryString, types) => {
+        search(queryString, toTargetClasses(types.length === 0 ? classesInCatalog : types));
+    };
 
     return (
         <>
@@ -21,29 +56,45 @@ const MetadataListPage = ({fetchVocabulary, vocabulary, targetClasses, searchMet
                 <SearchBar
                     placeholder="Search"
                     disableUnderline
-                    onSearchChange={(query) => {
-                        search(query, targetClasses);
+                    onSearchChange={q => {
+                        setQuery(q);
+                        performSearch(q, selectedTypes);
                     }}
                 />
+
+                <FormControl className={classes.typeSelect}>
+                    <Select
+                        multiple
+                        displayEmpty
+                        value={selectedTypes}
+                        onChange={e => {
+                            setSelectedTypes(e.target.value);
+                            performSearch(query, e.target.value);
+                        }}
+                        input={<Input id="select-multiple-checkbox" />}
+                        renderValue={selected => (selected.length === 0 ? '[All types]' : selected.map(getLabel).join(', '))}
+                    >
+                        {classesInCatalog.map(renderTypeClass)}
+                    </Select>
+                </FormControl>
             </Paper>
-            {targetClasses && targetClasses.length > 0 && (
+
+            {classesInCatalog && classesInCatalog.length > 0 && (
                 <MetadataBrowserContainer
                     vocabulary={vocabulary}
-                    targetClasses={targetClasses}
+                    targetClasses={toTargetClasses(classesInCatalog)}
                     fetchVocabulary={fetchVocabulary}
                 />
-            )
-            }
+            )}
         </>
     );
 };
 
 const mapStateToProps = (state) => {
     const vocabulary = getVocabulary(state);
-    const targetClasses = vocabulary.getClassesInCatalog()
-        .map(c => getFirstPredicateId(c, constants.SHACL_TARGET_CLASS));
+    const classesInCatalog = vocabulary.getClassesInCatalog();
 
-    return {targetClasses, vocabulary};
+    return {classesInCatalog, vocabulary};
 };
 
 const mapDispatchToProps = {
@@ -51,4 +102,4 @@ const mapDispatchToProps = {
     fetchVocabulary: fetchMetadataVocabularyIfNeeded
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(MetadataListPage);
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(MetadataListPage));
