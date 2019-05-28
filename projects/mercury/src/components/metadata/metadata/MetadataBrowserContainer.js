7669ca261a96cfc81d5df8809af1263b89c08101
Robert Horlings <robert@isdat.nl>
2019-05-28 16:47:07 +0200

    Update projects/mercury/src/components/metadata/metadata/MetadataBrowserContainer.js

--- projects/mercury/src/components/metadata/metadata/MetadataBrowserContainer.js
+++ projects/mercury/src/components/metadata/metadata/MetadataBrowserContainer.js
@@ -21,7 +21,7 @@ const MetadataBrowserContainer = ({entities, hasHighlights, ...otherProps}) => (
             {
                 entities && entities.length > 0
                     ? <MetadataList items={entities} hasHighlights={hasHighlights} />
-                    : <MessageDisplay message="The vocabulary is empty" isError={false} />
+                    : <MessageDisplay message="The metadata layer is empty" isError={false} />
             }
         </LinkedDataCreator>
     </LinkedDataValuesContext.Provider>
