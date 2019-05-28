d7690172d11f9a181b9a0fbe81521f1883cae0ca
AlQuraian <m4design@gmail.com>
2019-05-28 15:11:57 +0200

    Merge wit dev

--- projects/mercury/src/components/metadata/common/LinkedDataListPage.js
+++ projects/mercury/src/components/metadata/common/LinkedDataListPage.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import Paper from "@material-ui/core/Paper";
+import {
+    withStyles, Paper, Select, MenuItem, FormControl,
+    Checkbox, ListItemText, Input
+} from "@material-ui/core";
 
 import SearchBar from "../../common/SearchBar";
 import BreadCrumbs from "../../common/BreadCrumbs";
 
-const LinkedDataListPage = ({listRenderer, onSearchChange}) => (
-    <>
-        <BreadCrumbs />
-        <Paper>
-            <SearchBar
-                placeholder="Search"
-                disableUnderline
-                onSearchChange={onSearchChange}
-            />
-        </Paper>
-        {listRenderer()}
-    </>
-);
+const styles = theme => ({
+    typeSelect: {
+        paddingLeft: theme.spacing.unit * 10
+    }
+});
 
-export default LinkedDataListPage;
+const LinkedDataListPage = ({
+    classes, listRenderer, types, onSearchChange, onTypesChange, selectedTypes
+}) => {
+    const renderTypeClass = ({type, label}) => (
+        <MenuItem key={type} value={type}>
+            <Checkbox checked={selectedTypes.includes(type)} />
+            <ListItemText primary={label} secondary={type} />
+        </MenuItem>
+    );
+
+    return (
+        <>
+            <BreadCrumbs />
+            <Paper>
+                <SearchBar
+                    placeholder="Search"
+                    disableUnderline
+                    onSearchChange={onSearchChange}
+                />
+                <FormControl className={classes.typeSelect}>
+                    <Select
+                        multiple
+                        displayEmpty
+                        value={selectedTypes}
+                        onChange={e => onTypesChange(e.target.value)}
+                        input={<Input id="select-multiple-checkbox" />}
+                        renderValue={selected => (selected.length === 0 ? '[All types]'
+                            : types.filter(({type}) => selected.includes(type)).map(({label}) => label)
+                                .join(', '))}
+                    >
+                        {types.map(renderTypeClass)}
+                    </Select>
+                </FormControl>
+            </Paper>
+            {listRenderer()}
+        </>
+    );
+};
+
+export default withStyles(styles)(LinkedDataListPage);
--- projects/mercury/src/components/metadata/metadata/MetadataListPage.js
+++ projects/mercury/src/components/metadata/metadata/MetadataListPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 
 import LinkedDataListPage from "../common/LinkedDataListPage";
@@ -8,13 +8,42 @@ import {getVocabulary} from "../../../reducers/cache/vocabularyReducers";
 import {getFirstPredicateId} from "../../../utils/linkeddata/jsonLdUtils";
 import * as constants from "../../../constants";
 import {fetchMetadataVocabularyIfNeeded} from "../../../actions/vocabularyActions";
+import {getLabel} from "../../../utils/linkeddata/metadataUtils";
 
-const MetadataListPage = ({fetchVocabulary, vocabulary, search, targetClasses}) => {
+const MetadataListPage = ({fetchVocabulary, vocabulary, classesInCatalog, search}) => {
+    const [selectedTypes, setSelectedTypes] = useState([]);
+    const [query, setQuery] = useState('');
+
     fetchVocabulary();
+
+    const toTargetClasses = shapes => shapes.map(c => getFirstPredicateId(c, constants.SHACL_TARGET_CLASS));
+
+    const performSearch = (queryString, types) => {
+        const shapes = types.length === 0 ? classesInCatalog : classesInCatalog.filter(c => {
+            const targetClass = getFirstPredicateId(c, constants.SHACL_TARGET_CLASS);
+            return types.includes(targetClass);
+        });
+        search(queryString, toTargetClasses(shapes));
+    };
+
+    const targetClasses = toTargetClasses(classesInCatalog);
 
     return (
         <LinkedDataListPage
-            onSearchChange={(query) => search(query, targetClasses)}
+            types={classesInCatalog.map(type => {
+                const targetClass = getFirstPredicateId(type, constants.SHACL_TARGET_CLASS);
+                const label = getLabel(type);
+                return {type: targetClass, label};
+            })}
+            selectedTypes={selectedTypes}
+            onSearchChange={q => {
+                setQuery(q);
+                performSearch(q, selectedTypes);
+            }}
+            onTypesChange={(types) => {
+                setSelectedTypes(types);
+                performSearch(query, types);
+            }}
             listRenderer={() => (
                 targetClasses && targetClasses.length > 0 && (
                     <MetadataBrowserContainer
@@ -29,9 +58,8 @@ const MetadataListPage = ({fetchVocabulary, vocabulary, search, targetClasses}) 
 
 const mapStateToProps = (state) => {
     const vocabulary = getVocabulary(state);
-    const targetClasses = vocabulary.getClassesInCatalog()
-        .map(c => getFirstPredicateId(c, constants.SHACL_TARGET_CLASS));
-    return {vocabulary, targetClasses};
+    const classesInCatalog = vocabulary.getClassesInCatalog();
+    return {vocabulary, classesInCatalog};
 };
 
 const mapDispatchToProps = {
