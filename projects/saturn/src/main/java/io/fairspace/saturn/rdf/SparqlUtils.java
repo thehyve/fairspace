09278568639e8e7853a0422745dafeaba2ca5f77
strangepleasures <pavel@thehyve.nl>
2019-05-31 15:53:42 +0200

    Optimized validations

--- projects/saturn/src/main/java/io/fairspace/saturn/rdf/SparqlUtils.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/rdf/SparqlUtils.java
@@ -3,9 +3,11 @@ package io.fairspace.saturn.rdf;
 import lombok.SneakyThrows;
 import org.apache.commons.io.IOUtils;
 import org.apache.jena.datatypes.xsd.XSDDateTime;
+import org.apache.jena.graph.FrontsNode;
 import org.apache.jena.graph.Node;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.RDFList;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdfconnection.RDFConnection;
 
@@ -20,11 +22,11 @@ import java.util.function.Function;
 import static io.fairspace.saturn.ConfigLoader.CONFIG;
 import static java.lang.String.format;
 import static java.util.UUID.randomUUID;
+import static java.util.stream.Collectors.joining;
 import static org.apache.jena.graph.NodeFactory.createLiteral;
 import static org.apache.jena.graph.NodeFactory.createURI;
 import static org.apache.jena.rdf.model.ResourceFactory.createTypedLiteral;
 import static org.apache.jena.riot.out.NodeFmtLib.str;
-import static org.apache.jena.riot.out.NodeFmtLib.strNodes;
 import static org.apache.jena.riot.system.IRIResolver.validateIRI;
 
 public class SparqlUtils {
@@ -40,12 +42,18 @@ public class SparqlUtils {
             if(arg == null) {
                 param = "?" + i;
             } else if(arg instanceof Collection) {
-                var nodes = ((Collection<Object>) arg).stream()
+                param = ((Collection<?>) arg).stream()
                         .filter(Objects::nonNull)
                         .map(SparqlUtils::toSerializableNode)
-                        .toArray(Node[]::new);
+                        .map(SparqlUtils::toString)
+                        .collect(joining(" "));
+            } else if(arg instanceof RDFList) {
 
-                param = strNodes(nodes);
+                param = ((RDFList) arg).asJavaList()
+                        .stream()
+                        .map(SparqlUtils::toSerializableNode)
+                        .map(SparqlUtils::toString)
+                        .collect(joining(", ", "(", ")"));
             } else {
                 param = toString(toSerializableNode(arg));
             }
@@ -104,6 +112,9 @@ public class SparqlUtils {
     private static Node toNode(Object value) {
         if (value instanceof Node) {
             return (Node) value;
+        }
+        if (value instanceof FrontsNode) {
+            return ((FrontsNode) value).asNode();
         }
         if (value instanceof String) {
             return createLiteral((String) value);
--- projects/saturn/src/main/java/io/fairspace/saturn/services/metadata/validation/MetadataAndVocabularyConsistencyValidator.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/services/metadata/validation/MetadataAndVocabularyConsistencyValidator.java
@@ -4,29 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdfconnection.RDFConnection;
 import org.topbraid.shacl.vocabulary.SH;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-import static com.google.common.collect.Sets.union;
 import static io.fairspace.saturn.rdf.SparqlUtils.storedQuery;
 import static io.fairspace.saturn.services.metadata.validation.ShaclUtil.createEngine;
 import static io.fairspace.saturn.services.metadata.validation.ShaclUtil.getViolations;
 import static io.fairspace.saturn.vocabulary.Vocabularies.VOCABULARY_GRAPH_URI;
+import static java.lang.String.format;
+import static org.topbraid.spin.util.JenaUtil.getIntegerProperty;
+import static org.topbraid.spin.util.JenaUtil.getListProperty;
 
 /**
  * Checks if existing metadata remains valid after changes in the vocabulary
  */
 @AllArgsConstructor
 public class MetadataAndVocabularyConsistencyValidator implements MetadataRequestValidator {
-    static final int MAX_SUBJECTS = 10;
-    // This static exception instance is used to stop further validation
-    private static final TooManyViolationsException TOO_MANY_VIOLATIONS = new TooManyViolationsException();
-
     private final RDFConnection rdf;
 
     @Override
@@ -35,53 +33,144 @@ public class MetadataAndVocabularyConsistencyValidator implements MetadataReques
         var oldVocabulary = rdf.fetch(VOCABULARY_GRAPH_URI.getURI());
         var newVocabulary = oldVocabulary.difference(modelToRemove).union(modelToAdd);
         var actuallyAdded = newVocabulary.difference(oldVocabulary);
-        var actuallyRemoved = oldVocabulary.difference(newVocabulary);
-        var modifiedShapes = union(actuallyRemoved.listSubjects().toSet(), actuallyAdded.listSubjects().toSet());
 
-        // And then check, if the model is still valid with the updated vocabulary
-        var terminatingViolationHandlerWrapper = new ViolationHandler() {
-            private Set<Resource> subjects = new HashSet<>();
+        actuallyAdded.listStatements().forEachRemaining(stmt -> {
+            var subject = stmt.getSubject().inModel(newVocabulary);
 
-            @Override
-            public void onViolation(String message, Resource subject, Property predicate, RDFNode object) {
-                violationHandler.onViolation(message, subject, predicate, object);
-                subjects.add(subject);
-                if (subjects.size() == MAX_SUBJECTS) {
-                    throw TOO_MANY_VIOLATIONS; // Stop validation
+            if (subject.getPropertyResourceValue(SH.targetClass) != null) {
+                var targetClass = subject.getPropertyResourceValue(SH.targetClass);
+
+                if (stmt.getPredicate().equals(SH.property) && stmt.getObject().isResource()) {
+                    var propertyShape = stmt.getObject().asResource().inModel(newVocabulary);
+                    if (propertyShape.hasProperty(SH.path)) {
+                        var property = newVocabulary.createProperty(propertyShape.getPropertyResourceValue(SH.path).getURI());
+                        validateProperty(propertyShape, property, List.of(targetClass), violationHandler);
+                    }
                 }
-            }
-        };
+            } else if (subject.getPropertyResourceValue(SH.path) != null) {
+                    var property = newVocabulary.createProperty(subject.getPropertyResourceValue(SH.path).getURI());
 
-        try {
-            validateModifiedShapes(modifiedShapes, newVocabulary, terminatingViolationHandlerWrapper);
-        } catch (TooManyViolationsException ignore) {
+                    if (stmt.getPredicate().equals(SH.datatype)) {
+                        validateDataType(subject, property, getClassesWithProperty(subject, newVocabulary), violationHandler);
+                    } else if (stmt.getPredicate().equals(SH.class_)) {
+                        validateClass(subject, property, getClassesWithProperty(subject, newVocabulary), violationHandler);
+                    } else if (stmt.getPredicate().equals(SH.minCount)) {
+                        var newMinCount = getIntegerProperty(subject, SH.minCount);
+                        var oldMinCount = getIntegerProperty(subject.inModel(oldVocabulary), SH.minCount);
+                        if (newMinCount != null && (oldMinCount == null || oldMinCount < newMinCount)) {
+                            validateMinCount(subject, property, getClassesWithProperty(subject, newVocabulary), violationHandler);
+                        }
+                    } else if (stmt.getPredicate().equals(SH.maxCount)) {
+                        var newMaxCount = getIntegerProperty(subject, SH.maxCount);
+                        var oldMaxCount = getIntegerProperty(subject.inModel(oldVocabulary), SH.maxCount);
+                        if (newMaxCount != null && (oldMaxCount == null || oldMaxCount > newMaxCount)) {
+                            validateMaxCount(subject, property, getClassesWithProperty(subject, newVocabulary), violationHandler);
+                        }
+                    } else if (stmt.getPredicate().equals(SH.maxLength)) {
+                        var newMaxLength = getIntegerProperty(subject, SH.maxLength);
+                        var oldMaxLength = getIntegerProperty(subject.inModel(oldVocabulary), SH.maxLength);
+                        if (newMaxLength != null && (oldMaxLength == null || oldMaxLength > newMaxLength)) {
+                            validateMaxLength(subject, property, getClassesWithProperty(subject, newVocabulary), violationHandler);
+                        }
+                    } else if (stmt.getPredicate().equals(SH.minLength)) {
+                        var newMinLength = getIntegerProperty(subject, SH.minLength);
+                        var oldMinLength = getIntegerProperty(subject.inModel(oldVocabulary), SH.minLength);
+                        if (newMinLength != null && (oldMinLength == null || oldMinLength < newMinLength)) {
+                           validateMinLength(subject, property, getClassesWithProperty(subject, newVocabulary), violationHandler);
+                        }
+                    } else if (stmt.getPredicate().equals(SH.in)) {
+                        var newIn = getListProperty(subject, SH.in);
+                        var oldIn = getListProperty(subject.inModel(oldVocabulary), SH.in);
+                        if (newIn != null && (oldIn == null || !newIn.asJavaList().containsAll(oldIn.asJavaList()))) {
+                            validateIn(subject, property, getClassesWithProperty(subject, newVocabulary), violationHandler);
+                        }
+                    }
+                }
+        });
+    }
+
+    private static List<Resource> getClassesWithProperty(Resource propertyShape, Model vocabulary) {
+        var classes = new ArrayList<Resource>();
+        var propertyResource = propertyShape.getPropertyResourceValue(SH.path);
+        if (propertyResource != null) {
+            vocabulary.listSubjectsWithProperty(SH.property, propertyShape)
+                    .forEachRemaining(classShape -> {
+                                var subjectClass = classShape.getPropertyResourceValue(SH.targetClass);
+                                if (subjectClass != null) {
+                                    classes.add(subjectClass);
+                                }
+                            }
+                    );
+        }
+        return classes;
+    }
+
+    private void validateProperty(Resource propertyShape, Property property, Collection<Resource> subjectClasses, ViolationHandler violationHandler) {
+        validateDataType(propertyShape, property, subjectClasses, violationHandler);
+        validateClass(propertyShape, property, subjectClasses, violationHandler);
+        validateMaxLength(propertyShape, property, subjectClasses, violationHandler);
+        validateMinLength(propertyShape, property, subjectClasses, violationHandler);
+        validateMinCount(propertyShape, property, subjectClasses, violationHandler);
+        validateMaxCount(propertyShape, property, subjectClasses, violationHandler);
+        validateIn(propertyShape, property, subjectClasses, violationHandler);
+    }
+
+    private void validateDataType(Resource propertyShape, Property property, Collection<Resource> subjectClasses, ViolationHandler violationHandler) {
+        var dataType = propertyShape.getPropertyResourceValue(SH.datatype);
+        if (dataType != null) {
+            rdf.querySelect(storedQuery("find_wrong_data_type", property, subjectClasses, dataType), row ->
+                    violationHandler.onViolation("Invalid object type", row.getResource("subject"), property, row.get("object")));
         }
     }
 
-    private void validateModifiedShapes(Set<Resource> shapes, Model vocabulary, ViolationHandler violationHandler) {
-        for (var shape : shapes) {
-            validateByTargetClass(shape, vocabulary, violationHandler);
-            validateByPropertyPath(shape, vocabulary, violationHandler);
+    private void validateClass(Resource propertyShape, Property property, Collection<Resource> subjectClasses, ViolationHandler violationHandler) {
+        var theClass = propertyShape.getPropertyResourceValue(SH.class_);
+        if (theClass != null) {
+            rdf.querySelect(storedQuery("find_wrong_class", property, subjectClasses, theClass), row ->
+                    violationHandler.onViolation("Invalid object class", row.getResource("subject"), property, row.get("object")));
         }
     }
 
-    private void validateByTargetClass(Resource shape, Model vocabulary, ViolationHandler violationHandler) {
-        // determine the target class (if any) and validate all the entities belonging to it
-        var targetClass = shape.inModel(vocabulary).getPropertyResourceValue(SH.targetClass);
-
-        if (targetClass != null && targetClass.isURIResource()) {
-            rdf.querySelect(
-                    storedQuery("subjects_by_type", targetClass),
-                    row -> validateResource(row.getResource("s"), vocabulary, violationHandler)
-            );
+    private void validateMaxLength(Resource propertyShape, Property property, Collection<Resource> subjectClasses, ViolationHandler violationHandler) {
+        var maxLength = getIntegerProperty(propertyShape, SH.maxLength);
+        if (maxLength != null) {
+            rdf.querySelect(storedQuery("find_too_long", property, subjectClasses, maxLength), row ->
+                    violationHandler.onViolation(format("Value has more than %d characters", maxLength), row.getResource("subject"), property, row.get("object")));
         }
     }
 
-    private void validateByPropertyPath(Resource shape, Model vocabulary, ViolationHandler violationHandler) {
-        // determine the affected classes for a a property shape
-        vocabulary.listSubjectsWithProperty(SH.property, shape)
-                .forEachRemaining(classShape ->
-                        validateByTargetClass(classShape, vocabulary, violationHandler));
+    private void validateMinLength(Resource propertyShape, Property property, Collection<Resource> subjectClasses, ViolationHandler violationHandler) {
+        var minLength = getIntegerProperty(propertyShape, SH.minLength);
+        if (minLength != null) {
+            rdf.querySelect(storedQuery("find_too_short", property, subjectClasses, minLength), row ->
+                    violationHandler.onViolation(format("Value has less than %d characters", minLength), row.getResource("subject"), property, row.get("object")));
+        }
+    }
+
+
+    private void validateMinCount(Resource propertyShape, Property property, Collection<Resource> subjectClasses, ViolationHandler violationHandler) {
+        var minCount = getIntegerProperty(propertyShape, SH.minCount);
+        if (minCount != null) {
+            rdf.querySelect(storedQuery("find_too_few", property, subjectClasses, minCount), row ->
+                    violationHandler.onViolation("Too few values", row.getResource("subject"), property, null));
+        }
+    }
+
+    private void validateMaxCount(Resource propertyShape, Property property, Collection<Resource> subjectClasses, ViolationHandler violationHandler) {
+        var maxCount = getIntegerProperty(propertyShape, SH.maxCount);
+        if (maxCount != null) {
+            rdf.querySelect(storedQuery("find_too_many", property, subjectClasses, maxCount), row ->
+                    violationHandler.onViolation("Too many values", row.getResource("subject"), property, null));
+        }
+    }
+
+    private void validateIn(Resource propertyShape, Property property, Collection<Resource> subjectClasses, ViolationHandler violationHandler) {
+        var values = getListProperty(propertyShape, SH.in);
+
+        if (values != null) {
+            rdf.querySelect(storedQuery("find_not_in", property, subjectClasses, values), row ->
+                    violationHandler.onViolation("Invalid value", row.getResource("subject"), property, row.get("object")));
+        }
     }
 
     @SneakyThrows
@@ -91,6 +180,4 @@ public class MetadataAndVocabularyConsistencyValidator implements MetadataReques
         validationEngine.validateNode(resource.asNode());
         getViolations(validationEngine, violationHandler);
     }
-
-    private static class TooManyViolationsException extends RuntimeException {}
 }
