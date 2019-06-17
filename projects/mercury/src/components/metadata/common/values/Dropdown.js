82d136e648b975b2e370fff1b4553731259e3958
AlQuraian <m4design@gmail.com>
2019-06-17 11:42:49 +0200

    Clean code

--- projects/mercury/src/components/metadata/common/values/Dropdown.js
+++ projects/mercury/src/components/metadata/common/values/Dropdown.js
@@ -4,12 +4,11 @@ import PropTypes from 'prop-types';
 import MaterialReactSelect from "../../../common/MaterialReactSelect";
 import {compareBy} from "../../../../utils/genericUtils";
 
-const Dropdown = ({options, onChange = () => {}, ...otherProps}) => (
+const Dropdown = ({options, ...otherProps}) => (
     <MaterialReactSelect
         style={{width: '100%'}}
         {...otherProps}
         options={options.sort(compareBy('disabled'))}
-        onChange={onChange}
     />
 );
 
