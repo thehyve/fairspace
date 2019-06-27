df7554db82175649e9bdecd800dd4a50c82734be
strangepleasures <pavel@thehyve.nl>
2019-06-28 00:14:36 +0200

    Simplify TextIndexESBulk implementation

--- projects/saturn/src/main/java/io/fairspace/saturn/rdf/search/TextIndexESBulk.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/rdf/search/TextIndexESBulk.java
@@ -9,12 +9,13 @@ import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.script.Script;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
 import static com.google.common.collect.Iterables.partition;
@@ -23,19 +24,14 @@ import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 /**
  * Bulk Elastic Search Implementation of {@link TextIndex}
- *
  */
 public class TextIndexESBulk extends TextIndexES {
-    private static final String DASH = "-";
-
-    private static final String UNDERSCORE = "_";
-
     /**
      * ES Script for adding/updating the document in the index.
      * The main reason to use scripts is because we want to modify the values of the fields that contains an array of values
      */
     private static final String ADD_UPDATE_SCRIPT = "if((ctx._source == null) || (ctx._source[params.fieldName] == null) || (ctx._source[params.fieldName].empty == true)) " +
-            "{ctx._source[params.fieldName]=[params.fieldValue] } else {ctx._source[params.fieldName].add(params.fieldValue)}";
+            "{ctx._source[params.fieldName]=[params.fieldValue]} else {ctx._source[params.fieldName].add(params.fieldValue)}";
 
     /**
      * ES Script for deleting a specific value in the field for the given document in the index.
@@ -45,7 +41,7 @@ public class TextIndexESBulk extends TextIndexES {
             "&& (ctx._source[params.fieldToRemove].indexOf(params.valueToRemove) >= 0)) " +
             "{ctx._source[params.fieldToRemove].remove(ctx._source[params.fieldToRemove].indexOf(params.valueToRemove))}";
 
-    private static final Logger LOGGER      = LoggerFactory.getLogger(TextIndexESBulk.class) ;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TextIndexESBulk.class);
     private static final int BULK_SIZE = 1000;
 
     private final Client client;
@@ -58,6 +54,7 @@ public class TextIndexESBulk extends TextIndexES {
 
     /**
      * Constructor used mainly for performing Integration tests
+     *
      * @param config an instance of {@link TextIndexConfig}
      * @param client an instance of {@link TransportClient}. The client should already have been initialized with an index
      */
@@ -110,6 +107,7 @@ public class TextIndexESBulk extends TextIndexES {
     /**
      * Update an Entity. Since we are doing Upserts in add entity anyway, we simply call {@link #addEntity(Entity)}
      * method that takes care of updating the Entity as well.
+     *
      * @param entity the entity to update.
      */
     @Override
@@ -125,55 +123,29 @@ public class TextIndexESBulk extends TextIndexES {
      * The entity will be added as a new document in ES, if it does not already exists.
      * If the Entity exists, then the entity will simply be updated.
      * The entity will never be replaced.
+     *
      * @param entity the entity to add
      */
     @Override
     public void addEntity(Entity entity) {
         LOGGER.trace("Adding/Updating the entity {} in ES", entity.getId());
 
-        //The field that has a not null value in the current Entity instance.
-        //Required, mainly for building a script for the update command.
-        String fieldToAdd = null;
-        String fieldValueToAdd = null;
         try {
-            XContentBuilder builder = jsonBuilder()
-                    .startObject();
-
-            for(String field: getDocDef().fields()) {
-                if(entity.get(field) != null) {
-                    if(entity.getLanguage() != null && !entity.getLanguage().isEmpty()) {
-                        //We make sure that the field name contains all underscore and no dash (for eg. when the lang value is en-GB)
-                        //The reason to do this is because the script fails with exception in case we have "-" in field name.
-                        fieldToAdd = normalizeFieldName(field, entity.getLanguage());
-                    } else {
-                        fieldToAdd = field;
-                    }
-
-                    fieldValueToAdd = (String) entity.get(field);
-                    builder = builder.field(fieldToAdd, Arrays.asList(fieldValueToAdd));
-                    break;
-                } else {
-                    //We are making sure that the field is at-least added to the index.
-                    //This will help us tremendously when we are appending the data later in an already indexed document.
-                    builder = builder.field(field, Collections.emptyList());
-                }
-            }
-
-            builder = builder.endObject();
-            IndexRequest indexRequest = new IndexRequest(indexName, getDocDef().getEntityField(), entity.getId())
+            var entry = entity.getMap().entrySet().iterator().next();
+            var builder = jsonBuilder()
+                    .startObject()
+                    .field(entry.getKey(), List.of(entry.getValue()))
+                    .endObject();
+            var indexRequest = new IndexRequest(indexName, getDocDef().getEntityField(), entity.getId())
                     .source(builder);
+            var params = Map.of("fieldName", entry.getKey(), "fieldValue", entry.getValue());
 
-            Map<String, Object> params = new HashMap<>();
-            params.put("fieldName", fieldToAdd);
-            params.put("fieldValue", fieldValueToAdd);
-
-            UpdateRequest upReq = new UpdateRequest(indexName, getDocDef().getEntityField(), entity.getId())
+            var upReq = new UpdateRequest(indexName, getDocDef().getEntityField(), entity.getId())
                     .script(new Script(Script.DEFAULT_SCRIPT_TYPE, Script.DEFAULT_SCRIPT_LANG, ADD_UPDATE_SCRIPT, params))
                     .upsert(indexRequest);
 
             updates.add(upReq);
-
-        } catch(Exception e) {
+        } catch (Exception e) {
             LOGGER.error("Unable to Index the Entity in ElasticSearch.", e);
         }
     }
@@ -181,41 +153,17 @@ public class TextIndexESBulk extends TextIndexES {
     /**
      * Delete the value of the entity from the existing document, if any.
      * The document itself will never get deleted. Only the value will get deleted.
+     *
      * @param entity entity whose value needs to be deleted
      */
     @Override
     public void deleteEntity(Entity entity) {
+        var entry = entity.getMap().entrySet().iterator().next();
+        var params = Map.of("fieldToRemove", entry.getKey(), "valueToRemove", entry.getValue());
 
-        String fieldToRemove = null;
-        String valueToRemove = null;
-        for(String field : getDocDef().fields()) {
-            if(entity.get(field) != null) {
-                fieldToRemove = field;
-                if(entity.getLanguage()!= null && !entity.getLanguage().isEmpty()) {
-                    fieldToRemove = normalizeFieldName(fieldToRemove, entity.getLanguage());
-                }
-                valueToRemove = (String)entity.get(field);
-                break;
-            }
-        }
+        var updateRequest = new UpdateRequest(indexName, getDocDef().getEntityField(), entity.getId())
+                .script(new Script(Script.DEFAULT_SCRIPT_TYPE, Script.DEFAULT_SCRIPT_LANG, DELETE_SCRIPT, params));
 
-        if(fieldToRemove != null && valueToRemove != null) {
-            LOGGER.trace("deleting content related to entity {}", entity.getId());
-            Map<String,Object> params = new HashMap<>();
-            params.put("fieldToRemove", fieldToRemove);
-            params.put("valueToRemove", valueToRemove);
-
-            UpdateRequest updateRequest = new UpdateRequest(indexName, getDocDef().getEntityField(), entity.getId())
-                    .script(new Script(Script.DEFAULT_SCRIPT_TYPE, Script.DEFAULT_SCRIPT_LANG, DELETE_SCRIPT, params));
-
-            updates.add(updateRequest);
-        }
+        updates.add(updateRequest);
     }
-
-    private String normalizeFieldName(String fieldName, String lang) {
-        //We know that the lang field is not null already
-        StringBuilder sb = new StringBuilder(fieldName);
-        return sb.append(UNDERSCORE).append(lang.replaceAll(DASH,UNDERSCORE)).toString();
-    }
-
 }
