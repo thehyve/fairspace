29ef6949857787ade21632780952c77fee0ff6a8
Robert Horlings <robert@isdat.nl>
2019-07-22 14:47:46 +0200

    Implement unit tests for sorting and pagination

---
+++ projects/mercury/src/components/common/__tests__/useSorting.js
@@ -0,0 +1,12 @@
+describe('useSorting custom hook', () => {
+    it('should initialize by sorting the first column', () => {});
+
+    describe('toggleSort', () => {
+        it('should reverse direction when toggling the selected column', () => {});
+        it('should sort ascending if a new column is selected', () => {});
+
+    });
+
+    it('should sort the items based on the selected column', () => {});
+    it('should sort the items in the original order if the selected column has only one value', () => {});
+});
