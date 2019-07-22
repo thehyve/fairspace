592b11705f5e2e229a1a03385d2fc4cc523e0182
Robert Horlings <robert@isdat.nl>
2019-07-22 12:36:19 +0200

    Implement sorting for files and collections

--- projects/mercury/src/components/collections/__tests__/CollectionList.js
+++ projects/mercury/src/components/collections/__tests__/CollectionList.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {TableCell} from "@material-ui/core";
-import {createShallow} from '@material-ui/core/test-utils';
+import {TableBody, TableCell, TableHead} from "@material-ui/core";
 import CollectionList from "../CollectionList";
-
-const shallow = createShallow({dive: true});
+import {mount} from "enzyme";
 
 describe('CollectionList', () => {
+    const dateCreated = new Date();
+    const collections = [{
+        name: 'My Collection',
+        creatorObj: {
+            firstName: 'Mariah',
+            lastName: 'Carey'
+        },
+        dateCreated: dateCreated.toUTCString(),
+        iri: 'http://example.com/0'
+    }];
+
     it('renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<CollectionList />, div);
@@ -19,20 +28,17 @@ describe('CollectionList', () => {
         ReactDOM.unmountComponentAtNode(div);
     });
 
-    it('renders Creator column', () => {
-        const collections = [{creatorObj: {firstName: 'Mariah', lastName: 'Carey'}, iri: 'http://example.com/0'}];
-        const wrapper = shallow(<CollectionList collections={collections} />);
-        const cells = wrapper.dive().find(TableCell);
-        expect(cells.at(2).childAt(0).text()).toEqual('Creator');
-        expect(cells.at(5).childAt(0).text()).toEqual('Mariah Carey');
+    it('renders correct header columns', () => {
+        const wrapper = mount(<CollectionList collections={collections} />);
+        const headerCells = wrapper.find(TableHead).find(TableCell);
+
+        expect(headerCells.map(c => c.text())).toEqual(expect.arrayContaining(['Name', 'Creator', 'Created']));
     });
 
-    it('renders Created column', () => {
-        const date = new Date();
-        const collections = [{dateCreated: date.toUTCString(), iri: 'http://example.com/0'}];
-        const wrapper = shallow(<CollectionList collections={collections} />);
-        const cells = wrapper.dive().find(TableCell);
-        expect(cells.at(1).childAt(0).text()).toEqual('Created');
-        expect(cells.at(4).childAt(0).html()).toContain('M');
+    it('renders correct values columns', () => {
+        const wrapper = mount(<CollectionList collections={collections} />);
+        const bodyCells = wrapper.find(TableBody).find(TableCell);
+
+        expect(bodyCells.map(c => c.text())).toEqual(expect.arrayContaining(['My Collection', 'Mariah Carey']));
     });
 });
