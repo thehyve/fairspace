3bcfbf921f7b23d00ed26cb686bb5cb3e6abae18
AlQuraian <m4design@gmail.com>
2019-08-05 14:24:26 +0200

    Don't render copy button if copying not supported

--- projects/mercury/src/components/common/CopyButton.js
+++ projects/mercury/src/components/common/CopyButton.js
@@ -5,9 +5,11 @@ import {AssignmentOutlined, AssignmentTurnedInOutlined} from '@material-ui/icons
 import useIsMounted from "../../utils/useIsMounted";
 
 const DEFAULT_TIMEOUT = 1000;
+const clipboardSupported = 'clipboard' in navigator;
 
 /**
  * This button allows the user to copy a value to the clipboard.
+ * It will only be rendered if the browser supports the clipboard API
  * If the value is copied, the icon changes and a popup is shown for a short time.
  *
  * @param value
@@ -23,24 +25,23 @@ const CopyButton = ({
     const isMounted = useIsMounted();
 
     const handleCopy = () => {
-        if ('clipboard' in navigator) {
-            setJustCopied(true);
-            navigator.clipboard.writeText(value);
-            setTimeout(() => isMounted() && setJustCopied(false), timeout);
-        }
+        setJustCopied(true);
+        navigator.clipboard.writeText(value);
+        setTimeout(() => isMounted() && setJustCopied(false), timeout);
     };
 
     return (
-        <Tooltip
-            title={justCopied ? labelAfterCopy : labelPreCopy}
-            onClick={handleCopy}
-            style={{...style, cursor: 'pointer'}}
-        >
-            <span>
-                {justCopied ? <AssignmentTurnedInOutlined color="action" /> : <AssignmentOutlined color="action" />}
-            </span>
-        </Tooltip>
-    );
+        clipboardSupported && (
+            <Tooltip
+                title={justCopied ? labelAfterCopy : labelPreCopy}
+                onClick={handleCopy}
+                style={{...style, cursor: 'pointer'}}
+            >
+                <span>
+                    {justCopied ? <AssignmentTurnedInOutlined color="action" /> : <AssignmentOutlined color="action" />}
+                </span>
+            </Tooltip>
+        ));
 };
 
 export default CopyButton;
