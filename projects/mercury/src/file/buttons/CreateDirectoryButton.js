4d432a43e6fe1b105eaaf1d6b91aed1b6d630f3c
Robert Horlings <robert@isdat.nl>
2019-08-26 12:33:11 +0200

    Convert buttons into functional components and check mounted state. Fixes VRE-963

--- projects/mercury/src/file/buttons/CreateDirectoryButton.js
+++ projects/mercury/src/file/buttons/CreateDirectoryButton.js
@@ -1,69 +1,62 @@
-import React from 'react';
-import {
-    Dialog, DialogTitle, DialogContent,
-    DialogActions, Button, TextField
-} from "@material-ui/core";
+import React, {useState} from 'react';
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} from "@material-ui/core";
+import useIsMounted from "react-is-mounted-hook";
 
-class CreateDirectoryButton extends React.Component {
-    state = {
-        creating: false,
-        name: ''
-    };
+const CreateDirectoryButton = ({children, disabled, onCreate}) => {
+    const [opened, setOpened] = useState(false);
+    const [name, setName] = useState('');
+    const isMounted = useIsMounted();
 
-    openDialog = (e) => {
-        e.stopPropagation();
-        this.setState({creating: true, name: ''});
-    };
-
-    closeDialog = (e) => {
+    const openDialog = (e) => {
         if (e) e.stopPropagation();
-        this.setState({creating: false});
+        setName('');
+        setOpened(true);
     };
 
-    createDirectory = (e) => {
-        e.stopPropagation();
-
-        this.props.onCreate(this.state.name)
-            .then(shouldClose => shouldClose && this.closeDialog());
+    const closeDialog = (e) => {
+        if (e) e.stopPropagation();
+        setOpened(false);
     };
 
-    render() {
-        const {children, disabled} = this.props;
-        const {creating, name} = this.state;
+    const createDirectory = (e) => {
+        if (e) e.stopPropagation();
 
-        return (
-            <>
-                <span style={{display: 'inherit'}} onClick={e => !disabled && this.openDialog(e)}>
-                    {children}
-                </span>
+        onCreate(name)
+            .then(shouldClose => isMounted() && shouldClose && closeDialog());
+    };
 
-                <Dialog
-                    open={creating}
-                    onClick={e => e.stopPropagation()}
-                    onClose={this.closeDialog}
-                    aria-labelledby="form-dialog-title"
-                >
-                    <DialogTitle id="form-dialog-title">Create new directory</DialogTitle>
-                    <DialogContent>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Name"
-                            value={name}
-                            name="name"
-                            onChange={event => this.setState({name: event.target.value})}
-                            fullWidth
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={this.closeDialog} color="secondary">Close</Button>
-                        <Button onClick={this.createDirectory} color="primary" disabled={!name}>Create</Button>
-                    </DialogActions>
-                </Dialog>
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <span style={{display: 'inherit'}} onClick={e => !disabled && openDialog(e)}>
+                {children}
+            </span>
+
+            <Dialog
+                open={opened}
+                onClick={e => e.stopPropagation()}
+                onClose={closeDialog}
+                aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle id="form-dialog-title">Create new directory</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Name"
+                        value={name}
+                        name="name"
+                        onChange={e => setName(e.target.value)}
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={closeDialog} color="secondary">Close</Button>
+                    <Button onClick={createDirectory} color="primary" disabled={!name}>Create</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+};
 
 export default CreateDirectoryButton;
--- projects/mercury/src/file/buttons/RenameButton.js
+++ projects/mercury/src/file/buttons/RenameButton.js
@@ -1,95 +1,89 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
-import {
-    Dialog, DialogTitle, DialogContent,
-    DialogActions, Button, TextField
-} from "@material-ui/core";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} from "@material-ui/core";
+import useIsMounted from "react-is-mounted-hook";
 
-class RenameButton extends React.Component {
-    state = {
-        name: '',
-        renaming: false,
+const RenameButton = ({disabled, currentName, onRename, children}) => {
+    const [opened, setOpened] = useState(false);
+    const [name, setName] = useState('');
+    const isMounted = useIsMounted();
+
+    // Set name whenever it is changed in the props
+    useEffect(() => {
+        if (!opened) {
+            setName(currentName);
+        }
+    }, [opened, currentName]);
+
+    const openDialog = (e) => {
+        if (e) e.stopPropagation();
+        if (!disabled) {
+            setOpened(true);
+        }
     };
 
-    componentWillReceiveProps(props) {
-        if (!this.state.renaming) {
-            this.setState({name: props.currentName});
-        }
-    }
-
-    openDialog = (e) => {
+    const closeDialog = e => {
         if (e) e.stopPropagation();
-        if (!this.props.disabled) {
-            this.setState({renaming: true});
-        }
-    }
+        setOpened(false);
+    };
 
-    closeDialog = (e) => {
-        if (e) e.stopPropagation();
-        this.setState({renaming: false});
-    }
-
-    handleRename = (e) => {
+    const handleRename = (e) => {
         e.stopPropagation();
-        this.props.onRename(this.state.name)
-            .then(shouldClose => shouldClose && this.closeDialog());
-    }
+        onRename(name)
+            .then(shouldClose => isMounted() && shouldClose && setOpened(false));
+    };
 
-    render() {
-        const {children, currentName, disabled} = this.props;
-        const {renaming, name} = this.state;
-        const dialog = !disabled && renaming ? (
-            <Dialog
-                open
-                onClick={e => e.stopPropagation()}
-                onClose={this.closeDialog}
-                aria-labelledby="form-dialog-title"
+    const dialog = !disabled && opened ? (
+        <Dialog
+            open
+            onClick={e => e.stopPropagation()}
+            onClose={closeDialog}
+            aria-labelledby="form-dialog-title"
+        >
+            <DialogTitle
+                id="form-dialog-title"
             >
-                <DialogTitle
-                    id="form-dialog-title"
+                {`Rename ${currentName}`}
+            </DialogTitle>
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Name"
+                    value={name}
+                    name="name"
+                    onChange={e => setName(e.target.value)}
+                    fullWidth
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    onClick={closeDialog}
+                    color="secondary"
                 >
-                    {`Rename ${currentName}`}
-                </DialogTitle>
-                <DialogContent>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Name"
-                        value={name}
-                        name="name"
-                        onChange={event => this.setState({name: event.target.value})}
-                        fullWidth
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button
-                        onClick={this.closeDialog}
-                        color="secondary"
-                    >
-                        Close
-                    </Button>
-                    <Button
-                        onClick={this.handleRename}
-                        color="primary"
-                        disabled={disabled || !name || name === currentName}
-                    >
-                        Rename
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        ) : null;
+                    Close
+                </Button>
+                <Button
+                    onClick={handleRename}
+                    color="primary"
+                    disabled={disabled || !name || name === currentName}
+                >
+                    Rename
+                </Button>
+            </DialogActions>
+        </Dialog>
+    ) : null;
 
-        return (
-            <>
-                <span onClick={this.openDialog}>
-                    {children}
-                </span>
-                {dialog}
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <span onClick={openDialog}>
+                {children}
+            </span>
+            {dialog}
+        </>
+    );
+};
 
 RenameButton.propTypes = {
     onRename: PropTypes.func.isRequired,
