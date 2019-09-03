c70e5bd8833ee7e50d0964318bf6fc694d65dc55
strangepleasures <pavel@thehyve.nl>
2019-09-03 11:11:30 +0200

    VRE-985 Add human readable name for workspaces

--- projects/mercury/src/common/hooks/UsePageTitleUpdater.js
+++ projects/mercury/src/common/hooks/UsePageTitleUpdater.js
@@ -9,14 +9,14 @@ const separator = '-';
 const UsePageTitleUpdater = (segments) => {
     const subject = useSubject();
     const iri = useNamespacedIri(subject);
-    const {name: workspaceName} = useContext(VersionContext);
+    const {description: workspaceDescription} = useContext(VersionContext);
 
     useEffect(() => {
         const labels = segments ? segments.map(({label}) => label).join('/') : '';
         const iriToShow = iri ? `${iri} ${separator} ` : '';
 
-        document.title = `${iriToShow}${labels} ${separator} ${workspaceName}`;
-    }, [iri, workspaceName, segments]);
+        document.title = `${iriToShow}${labels} ${separator} ${workspaceDescription}`;
+    }, [iri, workspaceDescription, segments]);
 };
 
 export default UsePageTitleUpdater;
