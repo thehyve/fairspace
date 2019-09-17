1a048d77e6a310230303f5356bf266471c73c54e
Robert Horlings <robert@isdat.nl>
2019-09-17 10:16:33 +0200

    Use shared-library layout components

--- projects/mercury/src/common/components/index.js
+++ projects/mercury/src/common/components/index.js
@@ -1,4 +1,3 @@
-export {default as AuthorizationCheck} from "./AuthorizationCheck";
 export {default as LoadingOverlay} from './LoadingOverlay';
 export {default as SearchBar} from './SearchBar';
 export {default as ProgressButton} from './ProgressButton';
