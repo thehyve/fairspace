9f8139a92ec698ae6f2a5cf12f845820ce70d2a3
Mohammad <m4design@gmail.com>
2019-09-17 15:50:35 +0200

    Search components more generic (#698)

--- projects/mercury/src/search/__tests__/SearchPage.js
+++ projects/mercury/src/search/__tests__/SearchPage.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import {MessageDisplay} from '@fairspace/shared-frontend';
 
 import {SearchPage} from '../SearchPage';
-import SearchResults from '../SearchResults';
 
 describe('<SearchPage />', () => {
     let wrapper;
 
     beforeEach(() => {
-        wrapper = shallow(<SearchPage fetchVocabularyIfNeeded={() => {}} location={{search: ''}} performSearch={() => {}} />);
+        wrapper = shallow(<SearchPage
+            results={{total: 1, items: []}}
+            fetchVocabularyIfNeeded={() => {}}
+            location={{search: ''}}
+            performSearch={() => {}}
+        />);
     });
 
-    it('should render <SearchResults /> when receiving results', () => {
-        expect(wrapper.find(SearchResults)).toHaveLength(1);
+    it('should render table when receiving results', () => {
+        expect(wrapper.find('[data-testid="results-table"]')).toHaveLength(1);
     });
 
     it('should render error component on error', () => {
@@ -27,18 +31,18 @@ describe('<SearchPage />', () => {
 
     it('should perform search on component first mount', () => {
         const search = jest.fn();
-        shallow(<SearchPage fetchVocabularyIfNeeded={() => {}} location={{search: ''}} performSearch={search} />);
+        mount(<SearchPage fetchVocabularyIfNeeded={() => {}} location={{search: ''}} performSearch={search} />);
         expect(search.mock.calls.length).toEqual(1);
     });
 
     it('should fetch vocabulary on component first mount', () => {
         const fetchVocabularyIfNeeded = jest.fn();
-        shallow(<SearchPage fetchVocabularyIfNeeded={fetchVocabularyIfNeeded} location={{search: ''}} performSearch={() => {}} />);
+        mount(<SearchPage fetchVocabularyIfNeeded={fetchVocabularyIfNeeded} location={{search: ''}} performSearch={() => {}} />);
         expect(fetchVocabularyIfNeeded.mock.calls.length).toEqual(1);
     });
 
     it('should perform search after search query change', () => {
-        const localWrapper = shallow(<SearchPage fetchVocabularyIfNeeded={() => {}} location={{search: ''}} performSearch={() => {}} />);
+        const localWrapper = mount(<SearchPage fetchVocabularyIfNeeded={() => {}} location={{search: ''}} performSearch={() => {}} />);
         const search = jest.fn();
         localWrapper.setProps(
             {
