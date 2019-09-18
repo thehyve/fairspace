20c862765b1e26b5ea743ca064665ae16db3b429
Robert Horlings <robert@isdat.nl>
2019-09-18 15:52:00 +0200

    Provide anonymous component with a name
    Without this change, the search results would be refreshed every time the
    user hovers over the menu.

--- projects/mercury/src/metadata/MetadataOverviewPage.js
+++ projects/mercury/src/metadata/MetadataOverviewPage.js
@@ -3,4 +3,6 @@ import MetadataSearchResultList from "./MetadataSearchResultList";
 import LinkedDataOverviewPage from "./common/LinkedDataOverviewPage";
 import LinkedDataSearchResultsList from "./common/LinkedDataSearchResultsList";
 
-export default () => <LinkedDataOverviewPage title="Metadata" resultsComponent={props => LinkedDataSearchResultsList({...props, listComponent: MetadataSearchResultList})} />;
+const ResultsComponent = props => <LinkedDataSearchResultsList {...props} listComponent={MetadataSearchResultList} />;
+
+export default () => <LinkedDataOverviewPage title="Metadata" resultsComponent={ResultsComponent} />;
