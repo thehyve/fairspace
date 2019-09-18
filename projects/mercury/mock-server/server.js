1ac61ab65b6b54313b1b1c0eb8a5b292dfeb5e80
AlQuraian <m4design@gmail.com>
2019-09-18 11:23:28 +0200

    Fix mocked ends urls

--- projects/mercury/mock-server/server.js
+++ projects/mercury/mock-server/server.js
@@ -22,8 +22,12 @@ app.get('/config/version.json', (req, res) => res.sendFile(`${mockDataDir}/works
 // Account API
 app.get('/api/v1/account', (req, res) => res.sendFile(`${mockDataDir}/user.json`));
 
-// Workspace API
-app.get('/api/keycloak/users', (req, res) => res.sendFile(`${mockDataDir}/keycloak-users.json`));
+const MOCKED_USERS_LOCATION = `${mockDataDir}/keycloak-users.json`;
 
+// Users API
+app.get('/api/v1/users', (req, res) => res.sendFile(MOCKED_USERS_LOCATION));
+
+// Users search
+app.get('/api/keycloak/users', (req, res) => res.sendFile(MOCKED_USERS_LOCATION));
 
 app.listen(port);
--- projects/mercury/src/setupProxy.js
+++ projects/mercury/src/setupProxy.js
@@ -1,18 +1,23 @@
 const proxy = require('http-proxy-middleware');
 
+const MOCKED_SERVER_URL = 'http://localhost:5000/';
+const SEARCH_URL = 'http://localhost:9200/';
+const BACKEND_URL = 'http://localhost:8080/';
+
 module.exports = (app) => {
-    app.use(proxy('/config', {target: 'http://localhost:5000/'}));
-    app.use(proxy('/api/v1/account', {target: 'http://localhost:5000/'}));
-    app.use(proxy('/api/keycloak/users', {target: 'http://localhost:5000/'}));
-    
+    app.use(proxy('/config', {target: MOCKED_SERVER_URL}));
+    app.use(proxy('/api/v1/account', {target: MOCKED_SERVER_URL}));
+    app.use(proxy('/api/v1/users', {target: MOCKED_SERVER_URL}));
+    app.use(proxy('/api/keycloak/users', {target: MOCKED_SERVER_URL}));
+
     // to talk to a real ES instance on localhost
     app.use(proxy('/api/v1/search', {
-        target: 'http://localhost:9200/',
+        target: SEARCH_URL,
         pathRewrite: {
             '^/api/v1/search': '' // remove base path
         }
     }));
 
-    app.use(proxy('/api/v1', {target: 'http://localhost:8080/'}));
-    app.use(proxy('/webdav/v1', {target: 'http://localhost:8080/'}));
+    app.use(proxy('/api/v1', {target: BACKEND_URL}));
+    app.use(proxy('/webdav/v1', {target: BACKEND_URL}));
 };
