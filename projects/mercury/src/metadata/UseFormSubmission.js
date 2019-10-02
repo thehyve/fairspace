72e972cc1b473dac788a45a46cf03f397ac4a3e8
AlQuraian <m4design@gmail.com>
2019-10-02 11:50:21 +0200

    Show namespaced iris in validation errors

--- projects/mercury/src/metadata/UseFormSubmission.js
+++ projects/mercury/src/metadata/UseFormSubmission.js
@@ -1,21 +1,36 @@
 import {useState} from "react";
+import {useSelector} from "react-redux";
 import useIsMounted from "react-is-mounted-hook";
 import {ErrorDialog} from "@fairspace/shared-frontend";
 
 import ValidationErrorsDisplay from './common/ValidationErrorsDisplay';
-import {partitionErrors} from "../common/utils/linkeddata/metadataUtils";
-
-const onFormSubmissionError = (e, id) => {
-    if (e.details) {
-        ErrorDialog.renderError(ValidationErrorsDisplay, partitionErrors(e.details, id), e.message);
-    } else {
-        ErrorDialog.showError(e, `Error saving entity.\n${e.message}`);
-    }
-};
+import {partitionErrors, getNamespacedIri} from "../common/utils/linkeddata/metadataUtils";
+import {getVocabulary} from "../common/redux/reducers/cache/vocabularyReducers";
 
 const useFormSubmission = (submitFunc, subject) => {
     const [isUpdating, setUpdating] = useState(false);
     const isMounted = useIsMounted();
+    const namespaces = useSelector(state => getVocabulary(state).getNamespaces());
+
+    const toNamespaced = iri => !!iri && getNamespacedIri(iri, namespaces);
+
+    const withNamespacedProperties = (error) => ({
+        ...error,
+        subject: toNamespaced(error.subject),
+        predicate: toNamespaced(error.predicate)
+    });
+
+    const onFormSubmissionError = (error, id) => {
+        if (error.details) {
+            const partitionedErrors = partitionErrors(error.details, id);
+            const entityErrors = partitionedErrors.entityErrors.map(withNamespacedProperties);
+            const otherErrors = partitionedErrors.otherErrors.map(withNamespacedProperties);
+
+            ErrorDialog.renderError(ValidationErrorsDisplay, {otherErrors, entityErrors}, error.message);
+        } else {
+            ErrorDialog.showError(error, `Error saving entity.\n${error.message}`);
+        }
+    };
 
     const submitForm = () => {
         setUpdating(true);
