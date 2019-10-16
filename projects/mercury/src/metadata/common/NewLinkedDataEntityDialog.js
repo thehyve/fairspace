d72d42d16c9129357d59d09db5360cb06ff2beaf
AlQuraian <m4design@gmail.com>
2019-10-16 13:01:38 +0200

    Fix sending type for unknown subjects

--- projects/mercury/src/metadata/common/NewLinkedDataEntityDialog.js
+++ projects/mercury/src/metadata/common/NewLinkedDataEntityDialog.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useContext, useState} from "react";
 import PropTypes from 'prop-types';
 import {
     Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Typography
@@ -35,7 +35,12 @@ const NewLinkedDataEntityDialog = ({shape, requireIdentifier = true, onClose, on
     const {shapes, extendProperties, createLinkedDataEntity} = useContext(LinkedDataContext);
     const properties = shapes.getPropertiesForNodeShape(shape);
     const type = getFirstPredicateId(shape, consts.SHACL_TARGET_CLASS);
-    const values = {};
+    // The type could be required for unknown subjects which may not be known by the backend
+    const values = {
+        '@type': [{
+            id: type
+        }]
+    };
 
     // Apply context-specific logic to the properties and filter on visibility
     const extendedProperties = extendProperties({properties, isEntityEditable: true});
@@ -46,13 +51,6 @@ const NewLinkedDataEntityDialog = ({shape, requireIdentifier = true, onClose, on
 
         validateAll, validationErrors, isValid
     } = useFormData(values);
-
-    // Store the type to create in the form to ensure it is known
-    // and will be stored
-    useEffect(() => {
-        addValue('@type', type);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
 
     const {isUpdating, submitForm} = useFormSubmission(
         () => createLinkedDataEntity(getIdentifier(), getUpdates(), type)
@@ -140,6 +138,7 @@ const NewLinkedDataEntityDialog = ({shape, requireIdentifier = true, onClose, on
                     Cancel
                 </Button>
                 <Button
+                    data-testid="submit-button"
                     type="submit"
                     onClick={createEntity}
                     color="primary"
