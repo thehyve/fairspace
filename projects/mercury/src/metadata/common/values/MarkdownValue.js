60dcc0dc78c97848a7c1370896c6d9432959580a
Robert Horlings <robert@isdat.nl>
2019-10-29 09:50:12 +0100

    Rename handleChange function as suggested in PR #741

--- projects/mercury/src/metadata/common/values/MarkdownValue.js
+++ projects/mercury/src/metadata/common/values/MarkdownValue.js
@@ -4,20 +4,20 @@ import ReactMarkdown from "react-markdown";
 import BaseInputValue from "./BaseInputValue";
 
 const MarkdownValue = (props) => {
-    const {entry: {value}, onChange: originalOnChange} = props;
+    const {entry: {value}, onChange} = props;
 
     // Show the editor if the user chose to edit
     // or if there is no value yet
     const [showEdit, setShowEdit] = useState(!value);
 
     // After a change, the markdown value should be rendered again
-    const onChange = e => {
+    const handleChange = e => {
         setShowEdit(false);
-        return originalOnChange(e);
+        return onChange(e);
     };
 
     if (showEdit) {
-        return <BaseInputValue {...props} onChange={onChange} type="text" />;
+        return <BaseInputValue {...props} onChange={handleChange} type="text" />;
     }
 
     return <div onClick={() => setShowEdit(true)}><ReactMarkdown source={value} /></div>;
