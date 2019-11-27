d90e8b9b7b3a3e0761de0e8e55f9c629002f359b
AlQuraian <m4design@gmail.com>
2019-11-27 14:47:40 +0100

    Add explicit edit button

--- projects/mercury/src/home/WorkspaceInfo.js
+++ projects/mercury/src/home/WorkspaceInfo.js
@@ -1,10 +1,11 @@
-import React, {useContext} from 'react';
-import {Paper} from '@material-ui/core';
+import React, {useContext, useState} from 'react';
+import {Paper, IconButton, Menu, MenuItem, Grid} from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 import LinkedDataMetadataProvider from '../metadata/LinkedDataMetadataProvider';
 import LinkedDataEntityForm from '../metadata/common/LinkedDataEntityForm';
 import useLinkedData from '../metadata/UseLinkedData';
-import {WORKSPACE_INFO} from '../constants';
+import {WORKSPACE_INFO_URI} from '../constants';
 import LinkedDataEntityFormContainer from '../metadata/common/LinkedDataEntityFormContainer';
 import LinkedDataContext from '../metadata/LinkedDataContext';
 
@@ -17,20 +18,55 @@ const WorkspaceInfoWithProvider = () => (
 
 const WorkspaceInfo = () => {
     const {isCoordinator} = useContext(LinkedDataContext);
-    const {properties, values, linkedDataLoading, linkedDataError, updateLinkedData} = useLinkedData(WORKSPACE_INFO);
+    const {properties, values, linkedDataLoading, linkedDataError, updateLinkedData} = useLinkedData(WORKSPACE_INFO_URI);
+    const [anchorEl, setAnchorEl] = useState();
+    const [editingEnabled, setEditingEnabled] = useState(false);
 
     return (
         <Paper style={{maxWidth: 800, padding: 20}}>
             {isCoordinator ? (
-                <LinkedDataEntityFormContainer
-                    subject={WORKSPACE_INFO}
-                    editable
-                    properties={properties}
-                    values={values}
-                    linkedDataLoading={linkedDataLoading}
-                    linkedDataError={linkedDataError}
-                    updateLinkedData={updateLinkedData}
-                />
+                <Grid container direction="row">
+                    <Grid item xs="11">
+                        <LinkedDataEntityFormContainer
+                            subject={WORKSPACE_INFO_URI}
+                            editable={editingEnabled}
+                            properties={properties}
+                            values={values}
+                            linkedDataLoading={linkedDataLoading}
+                            linkedDataError={linkedDataError}
+                            updateLinkedData={updateLinkedData}
+                        />
+                    </Grid>
+                    {
+                        !editingEnabled && (
+                            <Grid item>
+                                <IconButton
+                                    aria-label="More"
+                                    aria-owns={anchorEl ? 'long-menu' : undefined}
+                                    aria-haspopup="true"
+                                    onClick={(e) => setAnchorEl(e.currentTarget)}
+                                >
+                                    <MoreVertIcon />
+                                </IconButton>
+                                <Menu
+                                    id="simple-menu"
+                                    anchorEl={anchorEl}
+                                    open={Boolean(anchorEl)}
+                                    onClose={() => setAnchorEl(null)}
+                                >
+                                    <MenuItem
+                                        onClick={() => {
+                                            setEditingEnabled(true);
+                                            setAnchorEl(null);
+                                        }}
+                                    >
+                                        Edit Information
+                                    </MenuItem>
+                                </Menu>
+                            </Grid>
+                        )
+                    }
+                </Grid>
             ) : (
                 <LinkedDataEntityForm
                     editable={false}
