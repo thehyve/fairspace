290df4790657cfe52184d3822e567f532ac4560a
strangepleasures <pavel@thehyve.nl>
2019-12-27 11:54:31 +0100

    Dynamic proxy

--- projects/mercury/server/server.js
+++ projects/mercury/server/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 const configPath = path.join(__dirname, 'config', 'config.yaml');
 const devConfig = {workspaces: ['http://localhost:8080'], elasticsearch: 'http://localhost:9200'};
-const config = fs.existsSync(configPath) ? YAML.parse(fs.readFileSync(configPath), 'utf8') : devConfig;
+const config = fs.existsSync(configPath) ? YAML.parse(fs.readFileSync(configPath, 'utf8')) : devConfig;
 const {workspaces} = config;
 
 const allProjects = () => Promise.all(workspaces.map(url => fetch(url + '/api/v1/projects/')
@@ -46,7 +46,7 @@ app.use(proxy('/api/v1/search', {
     pathRewrite: {'^/api/v1/search/fairspace': '/workspace-ci'}
 }));
 
-app.use(proxy('/api/v1', {target: 'http://workspace-ci-saturn.workspace-ci.svc.cluster.local'}));
+app.use(proxy('/api/v1', {router: req => workspaceByPath(req.path)}));
 
 const clientDir = path.join(path.dirname(__dirname), 'client');
 
