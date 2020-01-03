daa7c9e9179a47d665343ce7b93b4a46df15856c
strangepleasures <pavel@thehyve.nl>
2020-01-03 10:37:55 +0100

    Fix urls

--- projects/mercury/src/constants.js
+++ projects/mercury/src/constants.js
@@ -8,9 +8,9 @@ export const LEFT_MENU_EXPANSION_DELAY = 500;
 export const MAIN_CONTENT_WIDTH = '55%';
 export const SIDE_PANEL_WIDTH = '45%';
 export const MAIN_CONTENT_MAX_HEIGHT = 'calc(100vh - 156px)';
-export const COLLECTIONS_PATH = '/collections';
-export const METADATA_PATH = '/metadata';
-export const VOCABULARY_PATH = '/vocabulary';
+export const COLLECTIONS_PATH = 'collections';
+export const METADATA_PATH = 'metadata';
+export const VOCABULARY_PATH = 'vocabulary';
 export const TOOLTIP_ENTER_DELAY = 350;
 export const DATE_FORMAT = 'dd/MM/yyyy';
 
