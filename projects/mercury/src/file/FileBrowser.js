46e3c84cfbb9ef3e78d1adc6b20b688dbfc516a0
strangepleasures <pavel@thehyve.nl>
2020-01-04 00:38:44 +0100

    Fix the file browser

--- projects/mercury/src/file/FileBrowser.js
+++ projects/mercury/src/file/FileBrowser.js
@@ -11,6 +11,7 @@ import UploadList from "./UploadList";
 import useUploads from "./UseUploads";
 import {UPLOAD_STATUS_INITIAL} from "../common/contexts/UploadsContext";
 import {useFiles} from "./UseFiles";
+import {projectPrefix} from "../projects/projects";
 
 const TAB_FILES = 'FILES';
 const TAB_UPLOAD = 'UPLOAD';
@@ -60,7 +61,7 @@ export const DisconnectedFileBrowser = ({
 
     const handlePathDoubleClick = (path) => {
         if (path.type === 'directory') {
-            history.push(`/collections${path.filename}`);
+            history.push(`${projectPrefix()}/collections${path.filename}`);
         } else {
             FileAPI.open(path.filename);
         }
