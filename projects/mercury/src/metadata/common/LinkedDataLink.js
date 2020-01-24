bbe84ed496fdb11127b7d9321ca9ff667108caa3
Gijs Kant <gijs@thehyve.nl>
2020-01-24 12:05:55 +0100

    Fix metadata links in metadata table and metadata search results.

--- projects/mercury/src/metadata/common/LinkedDataLink.js
+++ projects/mercury/src/metadata/common/LinkedDataLink.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import * as PropTypes from "prop-types";
+import {projectPrefix} from '../../projects/projects';
 
 /**
  * Renders a link to a the editor for the given entity.
@@ -11,7 +12,7 @@ import * as PropTypes from "prop-types";
  * @param props
  * @constructor
  */
-const LinkedDataLink = ({uri, editorPath, children}) => <Link to={{pathname: editorPath, search: "?iri=" + encodeURIComponent(uri)}}>{children}</Link>;
+const LinkedDataLink = ({uri, editorPath, children}) => <Link to={{pathname: `${projectPrefix()}${editorPath}`, search: "?iri=" + encodeURIComponent(uri)}}>{children}</Link>;
 
 LinkedDataLink.propTypes = {
     uri: PropTypes.string.isRequired,
--- projects/mercury/src/metadata/common/LinkedDataRelationTable.js
+++ projects/mercury/src/metadata/common/LinkedDataRelationTable.js
@@ -10,6 +10,7 @@ import LinkedDataValuesTable from "./LinkedDataValuesTable";
 import IriTooltip from "../../common/components/IriTooltip";
 import Iri from "../../common/components/Iri";
 import {isGenericIriResource, isRelationShape} from "../../common/utils/linkeddata/vocabularyUtils";
+import {projectPrefix} from '../../projects/projects';
 
 const IDENTIFIER_COLUMN = {id: '@id', label: 'Uri', getValue: entry => entry['@id']};
 
@@ -48,7 +49,7 @@ export const LinkedDataRelationTable = ({property, values, onDelete, onAdd, canA
     }
 
     const rowDecorator = (entry, children) => <IriTooltip key={entry.id} enterDelay={TOOLTIP_ENTER_DELAY} title={<Iri iri={entry.id} />}>{children}</IriTooltip>;
-    const onOpen = entry => history.push(`${editorPath}?iri=` + encodeURIComponent(entry.id));
+    const onOpen = entry => history.push(`${projectPrefix()}${editorPath}?iri=` + encodeURIComponent(entry.id));
 
     return (
         <LinkedDataValuesTable
