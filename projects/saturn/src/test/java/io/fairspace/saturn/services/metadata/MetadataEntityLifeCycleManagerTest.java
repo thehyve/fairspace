b51cc792b2c1f78b32b097662e944994f6983e2f
strangepleasures <pavel@thehyve.nl>
2020-02-17 15:39:13 +0100

    Fix tests

--- projects/saturn/src/test/java/io/fairspace/saturn/services/metadata/MetadataEntityLifeCycleManagerTest.java
+++ projects/saturn/src/test/java/io/fairspace/saturn/services/metadata/MetadataEntityLifeCycleManagerTest.java
@@ -31,8 +31,7 @@ import static org.apache.jena.rdf.model.ResourceFactory.createResource;
 import static org.apache.jena.rdf.model.ResourceFactory.createStringLiteral;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MetadataEntityLifeCycleManagerTest {
