149c336a12e9e75eead33a14e14d9cfbfb0801d8
ewelinagr <ewelina@thehyve.nl>
2020-03-25 15:48:58 +0100

    Use user's username if firstName and lastName not specified

--- projects/saturn/src/main/java/io/fairspace/saturn/services/users/UserService.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/services/users/UserService.java
@@ -17,6 +17,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static io.fairspace.saturn.rdf.SparqlUtils.generateMetadataIri;
 import static java.lang.System.getenv;
@@ -70,9 +71,12 @@ public class UserService {
                 .stream()
                 .map(keycloakUser -> {
                     var user = new User();
+                    var name = (isNotEmpty(keycloakUser.getFirstName()) || isNotEmpty(keycloakUser.getLastName()))
+                            ? (keycloakUser.getFirstName() + " " + keycloakUser.getLastName()).trim()
+                            : keycloakUser.getUsername();
                     user.setId(keycloakUser.getId());
                     user.setIri(generateMetadataIri(keycloakUser.getId()));
-                    user.setName((keycloakUser.getFirstName() + " " + keycloakUser.getLastName()).trim());
+                    user.setName(name);
                     user.setEmail(keycloakUser.getEmail());
                     return user;
                 })
