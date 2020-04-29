6736d94791313109d2f9e1048c79f62df0d3ea09
strangepleasures <pavel@thehyve.nl>
2020-04-29 03:04:37 +0200

    Simplify SharingContext

--- projects/mercury/src/common/contexts/SharingContext.js
+++ projects/mercury/src/common/contexts/SharingContext.js
@@ -1,46 +1,18 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useState} from 'react';
 
 import PermissionAPI from "../../permissions/PermissionAPI";
 import WorkspaceContext from "../../workspaces/WorkspaceContext";
+import useAsync from "../hooks/UseAsync";
 
 const SharingContext = React.createContext({});
 
 export const SharingProvider = ({iri, children, getPermissions = PermissionAPI.getPermissions}) => {
     const {workspaces, refreshWorkspaces} = useContext(WorkspaceContext);
-    const [permissions, setPermissions] = useState([]);
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
     const [altering, setAltering] = useState(false);
 
     const extendWithNames = rawPermissions => rawPermissions.map(permission => ({...permission, name: workspaces.find(ws => permission.user === ws.iri).name}));
 
-    const refreshPermissions = () => {
-        let didCancel = false;
-
-        const fetchData = async () => {
-            setLoading(true);
-
-            try {
-                const fetchedPermissions = await getPermissions(iri);
-                if (!didCancel) {
-                    setPermissions(fetchedPermissions);
-                    setLoading(false);
-                }
-            } catch (e) {
-                if (!didCancel) {
-                    setError(e);
-                    setLoading(false);
-                }
-            }
-        };
-
-        fetchData();
-
-        return () => {didCancel = true;};
-    };
-
-    // Refresh the permissions whenever the iri changes
-    useEffect(refreshPermissions, [iri]);
+    const {data: permissions = [], loading, error, refresh: refreshPermissions} = useAsync(() => getPermissions(iri), [iri]);
 
     const alterPermission = (userIri, resourceIri, access) => {
         setAltering(true);
