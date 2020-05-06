73691461a6ca1ba0ce5553a382005f26e6841606
strangepleasures <pavel@thehyve.nl>
2020-05-06 13:06:44 +0200

    Simplify WebDAVServlet

--- projects/saturn/src/main/java/io/fairspace/saturn/webdav/WebDAVServlet.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/webdav/WebDAVServlet.java
@@ -2,12 +2,9 @@ package io.fairspace.saturn.webdav;
 
 import io.fairspace.saturn.config.Services;
 import io.milton.config.HttpManagerBuilder;
-import io.milton.http.Auth;
 import io.milton.http.AuthenticationService;
 import io.milton.http.HttpManager;
-import io.milton.http.Response;
 import io.milton.http.http11.DefaultHttp11ResponseHandler;
-import io.milton.resource.Resource;
 import io.milton.servlet.ServletRequest;
 
 import javax.servlet.ServletException;
@@ -15,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
 
 import static io.milton.servlet.MiltonServlet.clearThreadlocals;
 import static io.milton.servlet.MiltonServlet.setThreadlocals;
@@ -25,24 +21,13 @@ public class WebDAVServlet extends HttpServlet {
     private final HttpManager httpManager;
 
     public WebDAVServlet(Services svc) {
-        httpManager =  new HttpManagerBuilder() {{
+        httpManager = new HttpManagerBuilder() {{
             setResourceFactory(new VfsBackedMiltonResourceFactory(svc.getFileSystem()));
             setMultiNamespaceCustomPropertySourceEnabled(true);
             setAuthenticationService(new AuthenticationService(singletonList(new SaturnAuthenticationHandler())));
             setValueWriters(new NullSafeValueWriters());
 
-            setHttp11ResponseHandler(new DefaultHttp11ResponseHandler(getAuthenticationService(), geteTagGenerator(), getContentGenerator()) {
-                // Doesn't send Accept-Ranges header
-                @Override
-                protected void setRespondCommonHeaders(Response response, Resource resource, Response.Status status, Auth auth) {
-                    response.setStatus(status);
-                    response.setDateHeader(new Date());
-                    var etag = generateEtag(resource);
-                    if (etag != null) {
-                        response.setEtag(etag);
-                    }
-                }
-            });
+            setHttp11ResponseHandler(new DefaultHttp11ResponseHandler(getAuthenticationService(), geteTagGenerator(), getContentGenerator()));
         }}.buildHttpManager();
     }
 
