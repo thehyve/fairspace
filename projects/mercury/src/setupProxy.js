ca84f00a4db3840fac39215d464c0b959444b728
strangepleasures <pavel@thehyve.nl>
2020-05-08 18:38:12 +0200

    Enable debugging and live code updates at http://localhost:300/dev

--- projects/mercury/src/setupProxy.js
+++ projects/mercury/src/setupProxy.js
@@ -1,9 +1,43 @@
 const proxy = require('http-proxy-middleware');
-
-const BACKEND_URL = 'http://localhost:8080/';
+const Keycloak = require('keycloak-connect');
+const session = require('express-session');
 
 module.exports = (app) => {
+    const store = new session.MemoryStore();
+
+    app.use(session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+        // Uncomment to limit the session lifetime to 1 minute
+        // cookie: {maxAge: 60 * 1000 },
+        store
+    }));
+
+
+    const keycloak = new Keycloak(
+        {
+            store
+        },
+        {
+            authServerUrl: 'http://localhost:5100/auth',
+            realm: 'fairspace',
+            clientId: 'workspace-client',
+            secret: '**********'
+        }
+    );
+
+
+    // Return 401 Unauthorized for API requests
+    keycloak.redirectToLogin = (request) => !request.baseUrl.startsWith('/api/');
+
+    app.use(keycloak.middleware({logout: '/logout'}));
+    app.use('/dev', keycloak.protect());
+
+    const addToken = (proxyReq, req) => proxyReq.setHeader('Authorization', `Bearer ${req.kauth.grant.access_token.token}`);
+
     app.use(proxy('/api/v1', {
-        target: BACKEND_URL
+        target: 'http://localhost:8080/',
+        onProxyReq: addToken
     }));
 };
