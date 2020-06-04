7fa5ba4f7ed7b42ce3d942e5302fadf874557b58
ewelinagr <ewelina@thehyve.nl>
2020-06-04 15:23:18 +0200

    Add tests for "show deleted" mode

--- projects/mercury/src/collections/__tests__/CollectionAPI.js
+++ projects/mercury/src/collections/__tests__/CollectionAPI.js
@@ -2,6 +2,10 @@ import mockAxios from 'axios';
 
 import CollectionAPI from "../CollectionAPI";
 
+beforeEach(() => {
+    mockAxios.get.mockClear();
+});
+
 describe('CollectionAPI', () => {
     it('retrieves data for collections', async () => {
         mockAxios.get.mockImplementationOnce(() => Promise.resolve({
@@ -14,6 +18,22 @@ describe('CollectionAPI', () => {
         expect(collections).toEqual([{name: 'collection1'}]);
         expect(mockAxios.get).toHaveBeenCalledTimes(1);
         expect(mockAxios.get).toHaveBeenCalledWith('/api/v1/collections/', {headers: {Accept: 'application/json'}});
+    });
+
+    it('retrieves data for collections including deleted', async () => {
+        mockAxios.get.mockImplementationOnce(() => Promise.resolve({
+            data: [{name: 'collection1'}],
+            headers: {'content-type': 'application/json'}
+        }));
+
+        const collectionsWithDeleted = await CollectionAPI.getCollections(true);
+
+        expect(collectionsWithDeleted).toEqual([{name: 'collection1'}]);
+        expect(mockAxios.get).toHaveBeenCalledTimes(1);
+        expect(mockAxios.get).toHaveBeenCalledWith(
+            '/api/v1/collections/',
+            {headers: {'Accept': 'application/json', 'Show-Deleted': 'on'}}
+        );
     });
 
     it('makes a proper call to add a collection', async () => {
--- projects/mercury/src/collections/__tests__/CollectionList.js
+++ projects/mercury/src/collections/__tests__/CollectionList.js
@@ -22,15 +22,44 @@ describe('CollectionList', () => {
             ownerWorkspace: 'http://example.com/ws1'
         }];
 
+        const {queryByText} = render(
+            <WorkspaceContext.Provider value={{workspaces: [{iri: 'http://example.com/ws1', name: 'ws1'}]}}>
+                <CollectionList collections={collections} showDeleted={false} />
+            </WorkspaceContext.Provider>
+        );
+
+        expect(queryByText('Name')).toBeInTheDocument();
+        expect(queryByText('Creator')).toBeInTheDocument();
+        expect(queryByText('Created')).toBeInTheDocument();
+        expect(queryByText('Deleted')).not.toBeInTheDocument();
+        expect(queryByText('My Collection')).toBeInTheDocument();
+        expect(queryByText('Mariah Carey')).toBeInTheDocument();
+        expect(queryByText('Workspace')).toBeInTheDocument();
+        expect(queryByText('ws1')).toBeInTheDocument();
+    });
+
+    it('renders correct header and values columns in "show deleted" mode', () => {
+        const collections = [{
+            name: 'My Collection',
+            creatorObj: {
+                name: 'Mariah Carey'
+            },
+            dateCreated: new Date().toUTCString(),
+            iri: 'http://example.com/0',
+            ownerWorkspace: 'http://example.com/ws1',
+            dateDeleted: new Date().toUTCString()
+        }];
+
         const {getByText} = render(
             <WorkspaceContext.Provider value={{workspaces: [{iri: 'http://example.com/ws1', name: 'ws1'}]}}>
-                <CollectionList collections={collections} />
+                <CollectionList collections={collections} showDeleted />
             </WorkspaceContext.Provider>
         );
 
         expect(getByText('Name')).toBeInTheDocument();
         expect(getByText('Creator')).toBeInTheDocument();
         expect(getByText('Created')).toBeInTheDocument();
+        expect(getByText('Deleted')).toBeInTheDocument();
         expect(getByText('My Collection')).toBeInTheDocument();
         expect(getByText('Mariah Carey')).toBeInTheDocument();
         expect(getByText('Workspace')).toBeInTheDocument();
