44f2865e83e521b7163fda1974afa747b095fdf1
ewelinagr <ewelina@thehyve.nl>
2020-07-03 15:03:03 +0200

    Use permission property instead of permission API for collections
    Fixes renaming collection issue

--- projects/mercury/src/collections/collectionUtils.js
+++ projects/mercury/src/collections/collectionUtils.js
@@ -1,12 +1,33 @@
 import {buildSearchUrl} from "../search/searchUtils";
 import {COMMENT_URI, LABEL_URI} from "../constants";
-import type {Collection} from "./CollectionAPI";
+import type {Collection, CollectionPermissions} from "./CollectionAPI";
 
 export const getCollectionAbsolutePath = (location) => `/collections/${location}`;
 
 export const handleCollectionSearchRedirect = (history, value) => {
     const searchUrl = value ? buildSearchUrl(value) : '';
     history.push(`/collections${searchUrl}`);
+};
+
+export const mapCollectionPermissions: CollectionPermissions = (access) => {
+    const defaultAccess = {
+        canManage: false,
+        canWrite: false,
+        canRead: false
+    };
+    if (access === 'Manage') {
+        defaultAccess.canManage = true;
+        defaultAccess.canWrite = true;
+        defaultAccess.canRead = true;
+    }
+    if (access === 'Write') {
+        defaultAccess.canWrite = true;
+        defaultAccess.canRead = true;
+    }
+    if (access === 'Read') {
+        defaultAccess.canRead = true;
+    }
+    return defaultAccess;
 };
 
 export const mapCollectionNameAndDescriptionToMetadata = (name, description) => ({
@@ -22,5 +43,6 @@ export const mapFilePropertiesToCollection: Collection = (properties) => ({
     description: properties.comment,
     dateCreated: properties.creationdate,
     createdBy: properties.createdBy,
-    dateModified: properties.lastmod
+    dateModified: properties.lastmod,
+    ...mapCollectionPermissions(properties.access)
 });
