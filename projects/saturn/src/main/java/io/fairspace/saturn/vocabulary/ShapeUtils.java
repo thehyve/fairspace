1e54ce39d212eaf6196b406a0c0a5684edaa97ae
strangepleasures <pavel@thehyve.nl>
2020-08-19 13:12:52 +0200

    More checks in getClassShapeForClass

--- projects/saturn/src/main/java/io/fairspace/saturn/vocabulary/ShapeUtils.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/vocabulary/ShapeUtils.java
@@ -4,6 +4,7 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.shacl.vocabulary.SHACLM;
 import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,7 +20,8 @@ public class ShapeUtils {
     public static Optional<Resource> getClassShapeForClass(Resource type, Model vocabulary) {
         return vocabulary.listSubjectsWithProperty(SHACLM.targetClass, type)
                 .nextOptional()
-                .or(() -> Optional.of(type.inModel(vocabulary)));
+                .or(() -> Optional.of(type.inModel(vocabulary))
+                        .filter(shape -> shape.hasProperty(RDF.type, SHACLM.NodeShape) && shape.hasProperty(RDF.type, RDFS.Class)));
     }
 
     public static List<Resource> getPropertyShapesForResource(Resource resource, Model vocabulary) {
