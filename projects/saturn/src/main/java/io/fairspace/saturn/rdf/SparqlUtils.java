706b8720e545b4937a2d2d294720dc0bf9647f67
strangepleasures <pavel@thehyve.nl>
2020-08-19 11:28:04 +0200

    Get rid of stored queries

--- projects/saturn/src/main/java/io/fairspace/saturn/rdf/SparqlUtils.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/rdf/SparqlUtils.java
@@ -1,34 +1,23 @@
 package io.fairspace.saturn.rdf;
 
-import lombok.NonNull;
-import lombok.SneakyThrows;
-import org.apache.commons.io.IOUtils;
 import org.apache.jena.datatypes.xsd.XSDDateTime;
 import org.apache.jena.graph.FrontsNode;
 import org.apache.jena.graph.Node;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.RDFList;
-import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.update.UpdateExecutionFactory;
 import org.apache.jena.update.UpdateFactory;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 import static io.fairspace.saturn.config.ConfigLoader.CONFIG;
-import static io.fairspace.saturn.util.ValidationUtils.validateIRI;
-import static java.lang.String.format;
 import static java.util.UUID.randomUUID;
-import static java.util.stream.Collectors.joining;
 import static org.apache.jena.datatypes.xsd.XSDDatatype.XSDlong;
 import static org.apache.jena.graph.NodeFactory.createLiteral;
 import static org.apache.jena.graph.NodeFactory.createURI;
@@ -39,61 +28,12 @@ import static org.apache.jena.system.Txn.*;
 
 
 public class SparqlUtils {
-    private static final ConcurrentHashMap<String, String> storedQueries = new ConcurrentHashMap<>();
-
-    public static String storedQuery(String name, Object... args) {
-        var template = storedQueries.computeIfAbsent(name, SparqlUtils::load);
-        var params = new Object[args.length];
-        for (int i = 0; i < args.length; i++) {
-            var arg = args[i];
-            String param;
-
-            if (arg == null) {
-                param = "?" + i;
-            } else if (arg instanceof Collection) {
-                param = ((Collection<?>) arg).stream()
-                        .filter(Objects::nonNull)
-                        .map(SparqlUtils::toSerializableNode)
-                        .map(SparqlUtils::toString)
-                        .collect(joining(" "));
-            } else if (arg instanceof RDFList) {
-                param = toString((RDFList) arg);
-            } else {
-                param = toString(toSerializableNode(arg));
-            }
-
-            params[i] = param;
-        }
-        return format(template, (Object[]) params);
-    }
-
-    private static Node toSerializableNode(Object value) {
-        if (value instanceof Resource) {
-            value = ((Resource) value).asNode();
-        }
-        if (value instanceof Node && ((Node) value).isURI()) {
-            validateIRI(((Node) value).getURI());
-        }
-
-        return toNode(value);
-    }
-
-    @SneakyThrows(IOException.class)
-    private static String load(String name) {
-        return "PREFIX ws: " + str(createURI(CONFIG.jena.metadataBaseIRI)) + '\n' +
-                IOUtils.toString(SparqlUtils.class.getResourceAsStream("/sparql/" + name + ".sparql"), StandardCharsets.UTF_8);
-    }
-
     public static Node generateMetadataIri() {
         return generateMetadataIri(randomUUID().toString());
     }
 
     public static Node generateMetadataIri(String id) {
         return createURI(CONFIG.jena.metadataBaseIRI + id);
-    }
-
-    public static String extractIdFromIri(@NonNull Node iri) {
-        return iri.getURI().replace(CONFIG.jena.metadataBaseIRI, "");
     }
 
     public static Instant parseXSDDateTimeLiteral(Literal literal) {
@@ -146,14 +86,6 @@ public class SparqlUtils {
             throw new IllegalStateException("Too many values: " + values.size());
         }
         return values.stream().findFirst();
-    }
-
-    public static String toString(RDFList values) {
-        return values.asJavaList()
-                .stream()
-                .map(SparqlUtils::toSerializableNode)
-                .map(SparqlUtils::toString)
-                .collect(joining(", ", "(", ")"));
     }
 
     /**
