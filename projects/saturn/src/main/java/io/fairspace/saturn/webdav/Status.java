a5e50cc3344e9634c05b99a9bc543b92e56da17e
Gijs Kant <gijs@thehyve.nl>
2020-08-20 16:58:50 +0200

    Fix available access modes; display modes in order.
    - Fix the set of access modes that the current user can
    transfer a given collection to.
    - Maintain the defined order of access modes and statuses
    in the UI, instead of using the arbitrary order in which
    available modes and statuses are serialised.

--- projects/saturn/src/main/java/io/fairspace/saturn/webdav/Status.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/webdav/Status.java
@@ -1,7 +1,16 @@
 package io.fairspace.saturn.webdav;
 
 public enum Status {
+    /**
+     * Editing of collection contents is enabled.
+     */
     Active,
+    /**
+     * The contents of the collection is read-only.
+     */
     Archived,
+    /**
+     * The contents of the collection is not accessible.
+     */
     Closed
 }
