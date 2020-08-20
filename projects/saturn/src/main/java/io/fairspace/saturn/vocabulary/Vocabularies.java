159e83fee507963bd3a09130c6517b86fa00f6d7
strangepleasures <pavel@thehyve.nl>
2020-08-20 15:02:26 +0200

    Add vocabulary.ttl to Saturn's configmap

--- projects/saturn/src/main/java/io/fairspace/saturn/vocabulary/Vocabularies.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/vocabulary/Vocabularies.java
@@ -6,6 +6,6 @@ import static org.apache.jena.riot.RDFDataMgr.loadModel;
 
 public class Vocabularies {
     public static final Model SYSTEM_VOCABULARY = loadModel("system-vocabulary.ttl");
-    public static final Model USER_VOCABULARY = loadModel("user-vocabulary.ttl");
+    public static final Model USER_VOCABULARY = loadModel("vocabulary.ttl");
     public static final Model VOCABULARY = SYSTEM_VOCABULARY.union(USER_VOCABULARY);
 }
