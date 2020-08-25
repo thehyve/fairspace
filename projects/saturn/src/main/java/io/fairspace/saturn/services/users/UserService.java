177d652dc0db1b1244f94ffcbe5565851b7d3344
strangepleasures <pavel@thehyve.nl>
2020-08-25 16:29:26 +0200

    Better caching in UserService

--- projects/saturn/src/main/java/io/fairspace/saturn/services/users/UserService.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/services/users/UserService.java
@@ -5,17 +5,20 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import io.fairspace.saturn.config.Config;
 import io.fairspace.saturn.rdf.dao.DAO;
+import io.fairspace.saturn.rdf.dao.PersistentEntity;
 import io.fairspace.saturn.rdf.transactions.Transactions;
 import io.fairspace.saturn.services.AccessDeniedException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.jena.graph.Node;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.KeycloakBuilder;
-import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.admin.client.resource.UsersResource;
 
 import javax.servlet.ServletException;
 import javax.ws.rs.NotFoundException;
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -24,21 +27,22 @@ import static io.fairspace.saturn.auth.RequestContext.getCurrentRequest;
 import static io.fairspace.saturn.auth.RequestContext.getUserURI;
 import static io.fairspace.saturn.rdf.SparqlUtils.generateMetadataIri;
 import static java.lang.System.getenv;
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 @Slf4j
 public class UserService {
-    private final LoadingCache<Boolean, List<UserRepresentation>> keycloakUsersCache;
+    private final LoadingCache<Boolean, Map<Node, User>> usersCache;
     private final Transactions transactions;
     private final Config.Auth config;
+    private final UsersResource usersResource;
 
 
     public UserService(Config.Auth config, Transactions transactions) {
         this.config = config;
         this.transactions = transactions;
 
-        var usersResource = KeycloakBuilder.builder()
+        this.usersResource = KeycloakBuilder.builder()
                 .serverUrl(config.authServerUrl)
                 .realm(config.realm)
                 .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
@@ -50,77 +54,79 @@ public class UserService {
                 .realm(config.realm)
                 .users();
 
-        keycloakUsersCache = CacheBuilder.newBuilder()
-                .expireAfterAccess(30, TimeUnit.SECONDS)
+        usersCache = CacheBuilder.newBuilder()
+                .refreshAfterWrite(30, TimeUnit.SECONDS)
                 .build(new CacheLoader<>() {
                     @Override
-                    public List<UserRepresentation> load(Boolean key) {
-                        return usersResource.list();
+                    public Map<Node, User> load(Boolean key) {
+                        return fetchUsers();
                     }
                 });
     }
 
-    private List<UserRepresentation> getKeycloakUsers() throws ExecutionException {
-        return keycloakUsersCache.get(Boolean.FALSE);
+    public Collection<User> getUsers() {
+        return getUsersMap().values();
     }
 
     public User currentUser() {
-        var iri = getUserURI();
-        var user = getUsers().stream().filter(u -> u.getIri().equals(iri)).findFirst().orElse(null);
-        return user;
+        return getUsersMap().get(getUserURI());
     }
 
-    public List<User> getUsers() {
+    public Map<Node, User> getUsersMap() {
         try {
-            var keycloakUsers = getKeycloakUsers();
-            var updated = new HashSet<User>();
-            var users = transactions.calculateRead(model -> {
-                var dao = new DAO(model);
-                return keycloakUsers.stream()
-                .map(ku -> {
-                    var iri = generateMetadataIri(ku.getId());
-                    var user = dao.read(User.class, iri);
-                    if (user == null) {
-                        user = new User();
-                        user.setIri(iri);
-                        user.setId(ku.getId());
-
-                        if (config.superAdminUser.equals(ku.getUsername())) {
-                            user.setSuperadmin(true);
-                            user.setAdmin(true);
-                            user.setCanViewPublicMetadata(true);
-                            user.setCanViewPublicData(true);
-                            user.setCanAddSharedMetadata(true);
-                        }
-
-                        updated.add(user);
-                    }
-
-                    var name = (isNotEmpty(ku.getFirstName()) || isNotEmpty(ku.getLastName()))
-                            ? (ku.getFirstName() + " " + ku.getLastName()).trim()
-                            : ku.getUsername();
-
-                    if (!Objects.equals(user.getName(), name) || !Objects.equals(user.getEmail(), ku.getEmail())) {
-                        user.setEmail(ku.getEmail());
-                        user.setName(name);
-                        updated.add(user);
-                    }
-
-                    return user;
-                }).collect(toList());
-            });
-
-            if (!updated.isEmpty()) {
-                transactions.executeWrite(model -> {
-                    var dao = new DAO(model);
-                    updated.forEach(dao::write);
-                });
-            }
-
-            return users;
+            return usersCache.get(Boolean.FALSE);
         } catch (ExecutionException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private Map<Node, User> fetchUsers() {
+        var keycloakUsers = usersResource.list();
+        var updated = new HashSet<User>();
+        var users = transactions.calculateRead(model -> {
+            var dao = new DAO(model);
+            return keycloakUsers.stream()
+                    .map(ku -> {
+                        var iri = generateMetadataIri(ku.getId());
+                        var user = dao.read(User.class, iri);
+                        if (user == null) {
+                            user = new User();
+                            user.setIri(iri);
+                            user.setId(ku.getId());
+
+                            if (config.superAdminUser.equals(ku.getUsername())) {
+                                user.setSuperadmin(true);
+                                user.setAdmin(true);
+                                user.setCanViewPublicMetadata(true);
+                                user.setCanViewPublicData(true);
+                                user.setCanAddSharedMetadata(true);
+                            }
+
+                            updated.add(user);
+                        }
+
+                        var name = (isNotEmpty(ku.getFirstName()) || isNotEmpty(ku.getLastName()))
+                                ? (ku.getFirstName() + " " + ku.getLastName()).trim()
+                                : ku.getUsername();
+
+                        if (!Objects.equals(user.getName(), name) || !Objects.equals(user.getEmail(), ku.getEmail())) {
+                            user.setEmail(ku.getEmail());
+                            user.setName(name);
+                            updated.add(user);
+                        }
+
+                        return user;
+                    }).collect(toMap(PersistentEntity::getIri, u -> u));
+        });
+
+        if (!updated.isEmpty()) {
+            transactions.executeWrite(model -> {
+                var dao = new DAO(model);
+                updated.forEach(dao::write);
+            });
+        }
+
+        return users;
     }
 
     public void logoutCurrent() {
@@ -132,7 +138,7 @@ public class UserService {
     }
 
     public void update(UserRolesUpdate roles) {
-        if (!currentUser().isAdmin() && !currentUser().isSuperadmin()) {
+        if (!currentUser().isAdmin()) {
             throw new AccessDeniedException();
         }
         transactions.executeWrite(model -> {
@@ -171,5 +177,7 @@ public class UserService {
 
             dao.write(user);
         });
+
+        usersCache.invalidateAll();
     }
 }
