0b8f1fa7683ed6984857fe8defe2f56d4716fa68
Gijs Kant <gijs@thehyve.nl>
2020-09-14 23:31:24 +0200

    Clean up unused code.

--- projects/saturn/src/main/java/io/fairspace/saturn/rdf/SparqlUtils.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/rdf/SparqlUtils.java
@@ -1,11 +1,9 @@
 package io.fairspace.saturn.rdf;
 
 import org.apache.jena.datatypes.xsd.XSDDateTime;
-import org.apache.jena.graph.FrontsNode;
 import org.apache.jena.graph.Node;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Literal;
-import org.apache.jena.rdf.model.Model;
 import org.apache.jena.update.UpdateExecutionFactory;
 import org.apache.jena.update.UpdateFactory;
 
@@ -18,12 +16,8 @@ import java.util.function.Function;
 
 import static io.fairspace.saturn.config.ConfigLoader.CONFIG;
 import static java.util.UUID.randomUUID;
-import static org.apache.jena.datatypes.xsd.XSDDatatype.XSDlong;
-import static org.apache.jena.graph.NodeFactory.createLiteral;
 import static org.apache.jena.graph.NodeFactory.createURI;
-import static org.apache.jena.rdf.model.ModelFactory.createDefaultModel;
 import static org.apache.jena.rdf.model.ResourceFactory.createTypedLiteral;
-import static org.apache.jena.riot.out.NodeFmtLib.str;
 import static org.apache.jena.system.Txn.*;
 
 
@@ -42,30 +36,6 @@ public class SparqlUtils {
 
     public static Literal toXSDDateTimeLiteral(Instant instant) {
         return createTypedLiteral(GregorianCalendar.from(ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())));
-    }
-
-    private static String toString(Object value) {
-        return str(toNode(value));
-    }
-
-    private static Node toNode(Object value) {
-        if (value instanceof Node) {
-            return (Node) value;
-        }
-        if (value instanceof FrontsNode) {
-            return ((FrontsNode) value).asNode();
-        }
-        if (value instanceof String) {
-            return createLiteral((String) value);
-        }
-        if (value instanceof Long) {
-            return createTypedLiteral(value.toString(), XSDlong).asNode();
-        }
-        return createTypedLiteral(value instanceof Instant ? toCalendar((Instant) value) : value).asNode();
-    }
-
-    private static Calendar toCalendar(Instant value) {
-        return GregorianCalendar.from(ZonedDateTime.ofInstant(value, ZoneId.systemDefault()));
     }
 
     public static <T> List<T> select(Dataset dataset, String query, Function<QuerySolution, T> valueExtractor) {
@@ -89,22 +59,6 @@ public class SparqlUtils {
     }
 
     /**
-     * Execute a SELECT query and process the ResultSet with the handler code.
-     *
-     * @param query
-     * @param resultSetAction
-     */
-    public static void queryResultSet(Dataset dataset, String query, Consumer<ResultSet> resultSetAction) {
-        executeRead(dataset, () -> {
-            try (var qExec = query(dataset, query)) {
-                ResultSet rs = qExec.execSelect();
-                resultSetAction.accept(rs);
-            }
-        });
-    }
-
-
-    /**
      * Execute a SELECT query and process the rows of the results with the handler code.
      *
      * @param query
@@ -118,49 +72,11 @@ public class SparqlUtils {
         });
     }
 
-    /**
-     * Execute a CONSTRUCT query and return as a Model
-     */
-    public static Model queryConstruct(Dataset dataset, String query) {
-        return calculateRead(dataset, () -> {
-            try (var qExec = query(dataset, query)) {
-                return detachModel(qExec.execConstruct());
-            }
-        });
-    }
-
-    /**
-     * Execute a DESCRIBE query and return as a Model
-     */
-    public static Model queryDescribe(Dataset dataset, String query) {
-        return calculateRead(dataset, () -> {
-            try (var qExec = query(dataset, query)) {
-                return detachModel(qExec.execDescribe());
-            }
-        });
-    }
-
-    /**
-     * Execute a ASK query and return a boolean
-     */
-    public static boolean queryAsk(Dataset dataset, String query) {
-        return calculateRead(dataset, () -> {
-            try (var qExec = query(dataset, query)) {
-                return qExec.execAsk();
-            }
-        });
-    }
-
     public static void update(Dataset dataset, String updateString) {
         executeWrite(dataset, () -> UpdateExecutionFactory.create(UpdateFactory.create(updateString), dataset).execute());
-    }
-
-    public static Model detachModel(Model m) {
-        return createDefaultModel().add(m);
     }
 
     private static QueryExecution query(Dataset dataset, String query) {
         return QueryExecutionFactory.create(QueryFactory.create(query), dataset);
     }
-
 }
