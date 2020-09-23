443e3b018ff1d62a8e50ac9f81a5db116be553d8
strangepleasures <pavel@thehyve.nl>
2020-09-23 15:30:18 +0200

    Revert undesired changes

--- projects/mercury/src/metadata/common/values/MarkdownValue.js
+++ projects/mercury/src/metadata/common/values/MarkdownValue.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
 import ReactMarkdown from "react-markdown";
 
-import Tooltip from "@material-ui/core/Tooltip";
-import Link from "@material-ui/core/Link";
 import BaseInputValue from "./BaseInputValue";
 
 const MarkdownValue = (props) => {
@@ -11,29 +9,16 @@ const MarkdownValue = (props) => {
     const [showEdit, setShowEdit] = useState(!props.entry.value);
 
     return (
-        <Tooltip
-            interactive
-            title={(
-                <div>
-                    {'This is a '}
-                    <Link href="https://www.markdownguide.org/" target="_blank" rel="noreferrer">Markdown</Link>
-                    {' field'}
-                </div>
-            )}
-        >
-            <div onClick={() => setShowEdit(true)}>
-                {showEdit || !props.entry.value || !props.entry.value.trim() ? (
-
-                    <BaseInputValue
-                        {...props}
-                        autoFocus={showEdit && !!props.entry.value}
-                        onBlur={() => setShowEdit(false)}
-                        type="text"
-                    />
-
-                ) : (<ReactMarkdown source={props.entry.value} />)}
-            </div>
-        </Tooltip>
+        <div onClick={() => setShowEdit(true)}>
+            {showEdit || !props.entry.value || !props.entry.value.trim() ? (
+                <BaseInputValue
+                    {...props}
+                    autoFocus={showEdit && !!props.entry.value}
+                    onBlur={() => setShowEdit(false)}
+                    type="text"
+                />
+            ) : (<ReactMarkdown source={props.entry.value} />)}
+        </div>
     );
 };
 
