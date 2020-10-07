1c3523b88a96a72a2dc553179c7344a614ff2ed0
ewelinagr <ewelina@thehyve.nl>
2020-10-07 13:47:02 +0200

    Add set_label action to CollectionResource
    To prevent editing of the collection label property on collections by
    users without Manage access

--- projects/saturn/src/main/java/io/fairspace/saturn/webdav/CollectionResource.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/webdav/CollectionResource.java
@@ -53,6 +53,15 @@ class CollectionResource extends DirectoryResource implements DisplayNameResourc
         subject.removeAll(RDFS.label).addProperty(RDFS.label, s);
     }
 
+    private void setLabel(String l) throws NotAuthorizedException, ConflictException {
+        if (!canManage()) {
+            throw new NotAuthorizedException(this);
+        }
+        var trimmedLabel = l.trim();
+        ensureNameIsAvailable(trimmedLabel);
+        setDisplayName(trimmedLabel);
+    }
+
     private void validateTargetName(String name) throws ConflictException, BadRequestException {
         validateCollectionName(name);
         var existing = factory.root.findExistingCollectionWithNameIgnoreCase(name);
@@ -294,6 +303,7 @@ class CollectionResource extends DirectoryResource implements DisplayNameResourc
             case "set_status" -> setStatus(getEnumParameter(parameters, "status", Status.class));
             case "set_permission" -> setPermission(getResourceParameter(parameters, "principal"), getEnumParameter(parameters, "access", Access.class));
             case "set_owned_by" -> setOwnedBy(getResourceParameter(parameters, "owner"));
+            case "set_label" -> setLabel(parameters.get("label"));
             case "unpublish" -> unpublish();
             default -> super.performAction(action, parameters, files);
         }
