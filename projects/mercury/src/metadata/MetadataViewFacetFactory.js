89d38921e2a362beb5a8c341bb3256a009e642a7
ewelinagr <ewelina@thehyve.nl>
2020-11-18 17:09:07 +0100

    Styling improvements

--- projects/mercury/src/metadata/MetadataViewFacetFactory.js
+++ projects/mercury/src/metadata/MetadataViewFacetFactory.js
@@ -26,6 +26,7 @@ export type MetadataViewFacetProperties = {
 const useStyles = makeStyles({
     root: {
         width: 275,
+        height: 215,
         maxHeight: 215,
         overflowY: 'auto',
         boxShadow: '0px 1px 1px -1px rgba(0,0,0,0.2), 0px 0px 0px 0px rgba(0,0,0,0.14), 0px 1px 1px 0px rgba(0,0,0,0.12)'
