49fed268f44ac22545c152ec3f7134dd70b1a91c
strangepleasures <pavel@thehyve.nl>
2020-11-25 13:16:19 +0100

    Remove ViewRequest.view

--- projects/saturn/src/main/java/io/fairspace/saturn/services/views/ViewRequest.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/services/views/ViewRequest.java
@@ -4,12 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 
 @Getter @Setter
 public class ViewRequest extends CountRequest {
-    @NotBlank
-    private String view;
     @Min(1)
     private Integer page;
     @Min(1)
