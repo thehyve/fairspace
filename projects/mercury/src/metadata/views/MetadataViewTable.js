77b6c7fd4d46100530f46bd595a379a9c9f49de5
strangepleasures <pavel@thehyve.nl>
2020-12-23 21:13:37 +0100

    Simplify rendering logic

--- projects/mercury/src/metadata/views/MetadataViewTable.js
+++ projects/mercury/src/metadata/views/MetadataViewTable.js
@@ -23,53 +23,13 @@ const useStyles = makeStyles(() => ({
     }
 }));
 
-const mergeRows = (rows: any[], idColumn: string, valueSetColumns: string[], termSetColumns: string[]): any[] => {
-    const ids = [];
-    const result = [];
-    rows.forEach(row => {
-        const id = row[idColumn];
-        if (ids.includes(id)) {
-            const index = ids.indexOf(id);
-            valueSetColumns.forEach(column => {
-                const val = row[column];
-                if (val && !result[index][column].includes(val)) {
-                    result[index][column].push(val);
-                }
-            });
-            termSetColumns.forEach(column => {
-                const labelColumn = `${column}.label`;
-                const val = row[column];
-                if (val && !result[index][column].includes(val)) {
-                    result[index][column].push(val);
-                    result[index][labelColumn].push(row[labelColumn]);
-                }
-            });
-        } else {
-            ids.push(id);
-            const rowValues = {...row};
-            valueSetColumns.forEach(column => {
-                rowValues[column] = [row[column]];
-            });
-            termSetColumns.forEach(column => {
-                const labelColumn = `${column}.label`;
-                rowValues[column] = [row[column]];
-                rowValues[labelColumn] = [row[labelColumn]];
-            });
-            result.push(rowValues);
-        }
-    });
-    return result;
-};
-
 export const MetadataViewTable = (props: MetadataViewTableProperties) => {
     const {columns, visibleColumnNames, data, toggleRow, selected, view, idColumn, history} = props;
     const classes = useStyles();
     const visibleColumns = columns.filter(column => visibleColumnNames.includes(column.name));
     const isCollectionViewTable = isFilesView(view);
-    const valueSetColumns = columns.filter(c => c.type === 'Set').map((c) => c.name);
-    const termSetColumns = columns.filter(c => c.type === 'TermSet').map((c) => c.name);
     const dataLinkColumn = columns.find(c => c.type === 'dataLink');
-    const rows = idColumn && mergeRows(data.rows, idColumn.name, valueSetColumns, termSetColumns);
+    const {rows} = data;
 
     const handleResultSingleClick = (itemIri, itemLabel, linkedFiles) => {
         if (selected && selected.iri === itemIri) {
