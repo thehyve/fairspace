4724235f792b725d5dd4f27d10d6140e7e235ab6
ewelinagr <ewelina@thehyve.nl>
2021-01-07 15:07:21 +0100

    Fix missing import.

--- projects/mercury/src/metadata/views/MetadataView.js
+++ projects/mercury/src/metadata/views/MetadataView.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Button, Grid, withStyles} from '@material-ui/core';
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
