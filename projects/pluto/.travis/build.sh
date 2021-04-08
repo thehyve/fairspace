52d450af91e7d712fc72260f995148bb514530ee
ewelinagr <ewelina@thehyve.nl>
2021-04-08 09:41:19 +0200

    Move Mercury's travis build to Pluto.

--- projects/pluto/.travis/build.sh
+++ projects/pluto/.travis/build.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 set -e
 
+root="$PWD"
+
+echo 'Building the front-end'
+cd "$root"
+cd "../mercury"
+$BUILD_SCRIPTS_DIR/yarn/tag.sh || exit 1
+$BUILD_SCRIPTS_DIR/yarn/build.sh || exit 1
+
 echo 'Building the gateway'
+cd "$root"
 $BUILD_SCRIPTS_DIR/gradle/tag.sh
 ./gradlew clean assemble jacocoTestReport $GRADLE_OPTIONS
 
+test -e build/mercury && rm -r build/mercury
+cp -r ../mercury/build build/mercury
+
 echo 'Building the pluto image'
 $BUILD_SCRIPTS_DIR/docker/build.sh
+
+cd "$root"
--- projects/saturn/.travis/build.sh
+++ projects/saturn/.travis/build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -e
 
-root="$PWD"
-
-echo 'Building the front-end'
-cd "$root"
-cd "../mercury"
-$BUILD_SCRIPTS_DIR/yarn/tag.sh || exit 1
-$BUILD_SCRIPTS_DIR/yarn/build.sh || exit 1
-
 echo 'Building the back-end'
-cd "$root"
 $BUILD_SCRIPTS_DIR/gradle/tag.sh
 ./gradlew clean build test jacocoTestReport $GRADLE_OPTIONS
 
@@ -32,13 +23,5 @@ if [[ "$TRAVIS_BRANCH" = "$RELEASE_BRANCH" ]]; then
   ./gradlew dependencyCheckAnalyze
 fi
 
-test -e build/mercury && rm -r build/mercury
-cp -r ../mercury/build build/mercury
-
 echo 'Building the saturn image'
 $BUILD_SCRIPTS_DIR/docker/build.sh
-
-test -e build/mercury && rm -r build/mercury
-cp -r ../mercury/build build/mercury
-
-cd "$root"
