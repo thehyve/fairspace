c8e897b78b2ed63f6965e0100d91172f47e74bb9
ewelinagr <ewelina@thehyve.nl>
2021-06-29 08:55:57 +0200

    Pluto cleanup.

--- projects/pluto/src/main/java/nl/fairspace/pluto/config/Urls.java
+++ projects/pluto/src/main/java/nl/fairspace/pluto/config/Urls.java
@@ -2,7 +2,5 @@ package nl.fairspace.pluto.config;
 
 public final class Urls {
     private static final String PLUTO_API_PREFIX = "/api";
-    public static final String USERINFO_PATH = PLUTO_API_PREFIX + "/account/";
-    public static final String EXCHANGE_TOKENS_PATH = PLUTO_API_PREFIX + "/account/tokens/";
     public static final String STORAGES_PATH = PLUTO_API_PREFIX + "/storages/";
 }
