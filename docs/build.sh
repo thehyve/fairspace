bf01b6b1c538f010b09984379ec6860ee6fb545a
Gijs Kant <gijs@thehyve.nl>
2021-09-02 16:54:03 +0200

    Add application version to the documentation.

--- docs/build.sh
+++ docs/build.sh
@@ -11,22 +11,24 @@ PROJECT_FILES=(
 
 here=$(dirname "${0}")
 pushd "${here}"
-
+version=$(cat ../VERSION)
 mkdir -p build/docs
+cp ../README.adoc build/
+sed -i -e "s/VERSION/${version}/" build/README.adoc
 cp -r images build/docs/
 for f in ${PROJECT_FILES[*]}; do
   mkdir -p "build/$(dirname "$f")"
   cp "../$f" "build/"$(dirname "$f")""
 done
-asciidoctor-pdf -a pdf-theme=pdf-theme.yml -o build/Fairspace.pdf ../README.adoc || {
+asciidoctor-pdf -a pdf-theme=pdf-theme.yml -o build/Fairspace.pdf build/README.adoc || {
   echo "Error building PDF"
   popd
   exit 1
 }
-asciidoctor -a toc=left -D build/ -o index.html ../README.adoc || {
+asciidoctor -a toc=left -D build/ -o index.html build/README.adoc || {
   echo "Error building site"
   popd
   exit 1
 }
-
+rm build/README.adoc
 popd
