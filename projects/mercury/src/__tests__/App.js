fd421890e6b1b584f309e9e30b9f68dac781ae4d
Frank <29271979+FrankyHollywood@users.noreply.github.com>
2023-06-06 09:56:59 +0200

    test adjustments for Node 18

--- projects/mercury/src/__tests__/App.js
+++ projects/mercury/src/__tests__/App.js
@@ -4,10 +4,6 @@ import App from '../App';
 import {LocalFileAPI} from "../file/FileAPI";
 
 beforeEach(() => {
-    window.localStorage = {
-        getItem: jest.fn(() => null),
-        setItem: jest.fn()
-    };
     const getDirectoryContents = jest.fn(() => Promise.resolve(
         {data: []}
     ));
