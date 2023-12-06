9f05e5414501d7b065949b73b75d01d7b8976082
ewelinagr <ewelina@thehyve.nl>
2023-12-06 15:59:45 +0100

    Enable importing certificates into the Java keystore.

---
+++ projects/saturn/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+CERTS_PATH="/opt/extra_certs.pem"
+
+[[ -f "${CERTS_PATH}" ]] && \
+{ keytool -delete -alias certificate-alias -keystore "${JAVA_HOME}/lib/security/cacerts" -storepass changeit || true; \
+  keytool -import -trustcacerts -file "${CERTS_PATH}" -alias certificate-alias -keystore "${JAVA_HOME}/lib/security/cacerts" -storepass changeit -noprompt; }
+
+
+/opt/saturn-*/bin/saturn
