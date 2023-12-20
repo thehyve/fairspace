c5da6fb9ab5c58071a57a55616763d4324e1fc7a
Frank <29271979+FrankyHollywood@users.noreply.github.com>
2023-12-20 13:10:50 +0100

    Dialog for metadata details

--- projects/mercury/src/metadata/common/LinkedDataLink.js
+++ projects/mercury/src/metadata/common/LinkedDataLink.js
@@ -1,28 +1,65 @@
+/* eslint-disable */
 import React, {useContext} from 'react';
-import {Link as RouterLink} from "react-router-dom";
 import * as PropTypes from "prop-types";
-import {Link} from '@mui/material';
-import {METADATA_PATH} from "../../constants";
+import {Box, Modal, Tooltip} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import LinkedDataEntityPage from "./LinkedDataEntityPage";
+import {MetadataWrapper} from '../LinkedDataWrapper';
 import UserContext from '../../users/UserContext';
-
 /**
  * Renders a link to the metadata editor.
  *
  * @param props
  * @constructor
  */
+
+const styleModalDialog = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+const styleCloseButton = {
+    float: 'right'
+}
+
+const getModal = (open, handleClose, uri) => (
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+    >
+        <Box sx={styleModalDialog}>
+            <Tooltip title="Close - click or press 'Esc'">
+                <CloseIcon onClick={handleClose} sx={styleCloseButton}></CloseIcon>
+            </Tooltip>
+            <MetadataWrapper>
+                <LinkedDataEntityPage title="Metadata" subject={uri} />
+            </MetadataWrapper>
+        </Box>
+    </Modal>
+)
+
 const LinkedDataLink = ({uri, children}) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     const {currentUser} = useContext(UserContext);
     if (currentUser && currentUser.canViewPublicMetadata) {
         return (
-            <Link
-                component={RouterLink}
-                to={{pathname: METADATA_PATH, search: "?iri=" + encodeURIComponent(uri)}}
-                color="inherit"
-                underline="hover"
-            >
-                {children}
-            </Link>
+            <div>
+                <div onClick={handleOpen}>
+                    {children}
+                </div>
+                {getModal(open, handleClose, uri)}
+            </div>
         );
     }
     return children;
