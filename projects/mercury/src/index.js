13c37a5eeb0c9b07b89c2833cc21d90e4c5c0804
ewelinagr <ewelina@thehyve.nl>
2024-02-09 08:22:11 +0100

    Revert default withCredentials axios flag.

--- projects/mercury/src/index.js
+++ projects/mercury/src/index.js
@@ -8,7 +8,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 axios.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest";
-axios.defaults.withCredentials = true;
 
 const container = document.getElementById('root');
 const root = createRoot(container);
