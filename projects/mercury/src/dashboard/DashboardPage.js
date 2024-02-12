be51fe6dedad8566635cff9e54db1a2b9955ea06
Frank <29271979+FrankyHollywood@users.noreply.github.com>
2024-02-12 13:04:05 +0100

    Fixed eslint errors

--- projects/mercury/src/dashboard/DashboardPage.js
+++ projects/mercury/src/dashboard/DashboardPage.js
@@ -1,15 +1,14 @@
-/* eslint-disable */
 import React, {useContext} from "react";
 
 import withStyles from "@mui/styles/withStyles";
-import {APPLICATION_NAME, METADATA_VIEW_MENU_LABEL} from "../constants";
+import {Grid, Link, Typography} from "@mui/material";
 import BreadCrumbs from "../common/components/BreadCrumbs";
 import styles from "./DashboardPage.styles";
 import MetadataViewContext from "../metadata/views/MetadataViewContext";
 import ExternalMetadataSourceContext from "../metadata/external-sources/ExternalMetadataSourceContext";
 import UserContext from "../users/UserContext";
-import {Grid, Link, Typography} from "@mui/material";
 import DomainInfo from "./DomainInfo";
+import {APPLICATION_NAME, METADATA_VIEW_MENU_LABEL} from "../constants";
 
 const DashboardPage = (props) => {
     const {currentUser, classes} = props;
@@ -19,47 +18,47 @@ const DashboardPage = (props) => {
 
     return (
         <Grid container justifyContent="center" spacing="5">
-            {<BreadCrumbs />}
+            <BreadCrumbs />
             <div className={classes.mainPage}>
                 <Grid container justifyContent="center" spacing="5">
                     <Grid container justifyContent="center" spacing="5">
-                        <Typography variant="h3" paragraph={true}>
+                        <Typography variant="h3" paragraph>
                             {APPLICATION_NAME}
                         </Typography>
                     </Grid>
                     <Grid container justifyContent="center" spacing="5" className={classes.header}>
-                        <Typography variant="h5" paragraph={true}>
+                        <Typography variant="h5" paragraph>
                             research data management
                         </Typography>
                     </Grid>
                     <Grid container justifyContent="left" spacing="5">
-                    <Grid item xs={6} md={4}>
-                        <Typography variant="body1" paragraph={true}>
-                            {APPLICATION_NAME} contains your research metadata. Click on one of the domains and start exploring.
-                        </Typography>
-                        <Typography variant="body1" paragraph={true}>
-                            For more details on how to use Fairspace, e.g. how to query the API, please refer the {" "}
+                        <Grid item xs={6} md={4}>
+                            <Typography variant="body1" paragraph>
+                                {APPLICATION_NAME} contains your research metadata. Click on one of the domains and start exploring.
+                            </Typography>
+                            <Typography variant="body1" paragraph>
+                                For more details on how to use Fairspace, e.g. how to query the API, please refer the {" "}
                                 <Link href="https://docs.fairway.app/">
                                     user manual
                                 </Link>.
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={6} md={8}>
-                        <Grid container justifyContent="center" spacing={1}>
-                            {canViewMetadata && (
-                                <DomainInfo
-                                    domainName={METADATA_VIEW_MENU_LABEL}
-                                    domainLink="/metadata-views"
-                                />)
-                            }
-                            {canViewMetadata && externalMetadataSources.map((source) => (
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} md={8}>
+                            <Grid container justifyContent="center" spacing={1}>
+                                {canViewMetadata && (
+                                    <DomainInfo
+                                        domainName={METADATA_VIEW_MENU_LABEL}
+                                        domainLink="/metadata-views"
+                                    />
+                                )}
+                                {canViewMetadata && externalMetadataSources.map((source) => (
                                     <DomainInfo
                                         domainName={source.label}
                                         domainLink={source.name}
                                     />
                                 ))}
+                            </Grid>
                         </Grid>
-                    </Grid>
                     </Grid>
                 </Grid>
             </div>
