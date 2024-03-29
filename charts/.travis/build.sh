b81c477d3f44965ad6dc55bbf3d47081bf533f0f
anton <anton@thehyve.nl>
2024-03-29 23:09:31 +0100

    Removed Travis scripts

--- charts/.travis/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-$BUILD_SCRIPTS_DIR/helm/tag.sh
-.travis/add_pod_annotations.sh
-source $BUILD_SCRIPTS_DIR/helm/add_repos.sh
-$BUILD_SCRIPTS_DIR/helm/build.sh
-
-if [[ $SHOULD_RELEASE ]]; then
-  $BUILD_SCRIPTS_DIR/helm/release.sh
-fi
--- docs/.travis/install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-gem install asciidoctor asciidoctor-pdf rouge
