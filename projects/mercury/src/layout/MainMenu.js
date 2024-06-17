c88be467ecfccfb09c3c8fb64f12c906db37c1b2
Frank <29271979+FrankyHollywood@users.noreply.github.com>
2024-06-17 10:31:47 +0200

    Small refactor

--- projects/mercury/src/layout/MainMenu.js
+++ projects/mercury/src/layout/MainMenu.js
@@ -16,7 +16,7 @@ import ExternalMetadataSourceContext from '../metadata/metadata-sources/External
 import {getExternalStoragePathPrefix} from '../external-storage/externalStorageUtils';
 import {getExternalMetadataSourcePathPrefix} from '../metadata/external-views/externalMetadataSourceUtils';
 import InternalMetadataSourceContext from '../metadata/metadata-sources/InternalMetadataSourceContext';
-import {COLORS, scrollbarStyles} from '../App.theme';
+import {COLORS} from '../App.theme';
 
 const styles = theme => ({
     buttonStack: {
