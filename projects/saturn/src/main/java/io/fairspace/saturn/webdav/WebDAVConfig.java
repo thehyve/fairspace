0d9d964a142b0962dad9300341d717f8e8f295d6
anton <anton@thehyve.nl>
2024-08-23 21:58:12 +0200

    FAIRSPC-82: fixed webdav endpoints

--- projects/saturn/src/main/java/io/fairspace/saturn/webdav/WebDAVConfig.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/webdav/WebDAVConfig.java
@@ -1,10 +1,12 @@
 package io.fairspace.saturn.webdav;
 
+import io.fairspace.saturn.config.Services;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
-import io.fairspace.saturn.config.Services;
+import java.util.Arrays;
 
 @Configuration
 public class WebDAVConfig {
@@ -15,5 +17,36 @@ public class WebDAVConfig {
                 new ServletRegistrationBean<>(svcs.getDavServlet(), "/api/webdav/*");
         bean.setLoadOnStartup(1);
         return bean;
+    }
+
+    /**
+     * Configure the firewall to allow WebDAV methods.
+     * By default, Spring Security blocks all methods except GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, and TRACE.
+     *
+     * @return the firewall
+     */
+    @Bean
+    public StrictHttpFirewall webDavFilterFirewall() {
+        final StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowedHttpMethods(Arrays.stream(ExtendedHttpMethod.values()).map(Enum::name).toList());
+        return firewall;
+    }
+
+    private enum ExtendedHttpMethod {
+        GET,
+        HEAD,
+        POST,
+        PUT,
+        PATCH,
+        DELETE,
+        OPTIONS,
+        TRACE,
+        COPY,
+        LOCK,
+        MKCOL,
+        MOVE,
+        PROPFIND,
+        PROPPATCH,
+        UNLOCK
     }
 }
