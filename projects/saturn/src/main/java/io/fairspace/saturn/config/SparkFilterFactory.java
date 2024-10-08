e34489160b828af42bb888b810c0d2f6f58f34dd
anton <anton@thehyve.nl>
2024-10-08 13:14:23 +0200

    FAIRSPC-81: migrated user endpoints to spring mvc

--- projects/saturn/src/main/java/io/fairspace/saturn/config/SparkFilterFactory.java
+++ projects/saturn/src/main/java/io/fairspace/saturn/config/SparkFilterFactory.java
@@ -3,14 +3,12 @@ package io.fairspace.saturn.config;
 import io.fairspace.saturn.config.properties.KeycloakClientProperties;
 import io.fairspace.saturn.services.search.SearchApp;
 import io.fairspace.saturn.services.users.LogoutApp;
-import io.fairspace.saturn.services.users.UserApp;
 
 public class SparkFilterFactory {
     public static SaturnSparkFilter createSparkFilter(
             String apiPathPrefix, Services svc, KeycloakClientProperties keycloakClientProperties, String publicUrl) {
         return new SaturnSparkFilter(
                 new SearchApp(apiPathPrefix + "/search", svc.getSearchService(), svc.getFileSearchService()),
-                new UserApp(apiPathPrefix + "/users", svc.getUserService()),
                 new LogoutApp("/logout", svc.getUserService(), keycloakClientProperties, publicUrl));
     }
 }
